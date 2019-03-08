8f7eb2a1640cc8afd8033be8dafb8b9f93f24040
ce <zhuxiujia@qq.com>
2019-03-08 16:12:47 +0800

    初步概念

---
+++ src/ast/NodeString.rs
@@ -0,0 +1,35 @@
+//use super::{Node::Node,NodeType::NodeType};
+
+use crate::ast::Node::Node;
+use crate::ast::NodeType::NodeType;
+use std::collections::HashMap;
+
+pub struct NodeString {
+    pub buf: String,
+}
+
+impl Node for NodeString {
+    fn Type(&self) -> NodeType {
+        return NodeType::String;
+    }
+
+    fn Eval(&self, arg: &HashMap<String, String>) -> String {
+        //  self.buf.replace()
+
+//          for (k,v)in arg{
+//          }
+
+        let mut s = String::new();
+        s.push_str(&"sadf");
+        return s;
+    }
+}
+
+impl NodeString {
+    pub fn ToString(&self) -> String {
+        let mut s = String::new();
+        s.push_str("NodeString");
+        return s;
+    }
+}
+
