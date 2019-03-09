77230baa1a2f18a921d828d67bcad0d241fca2a6
zhuxiujia <zhuxiujia@qq.com>
2019-03-10 01:08:40 +0800

    add xml loader

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,40 @@ use std::io::{Read, BufReader};
 use xml::EventReader;
 use xml::reader::XmlEvent;
 
-//fn main() {
-//    let node_string = NodeString { buf: "".to_string() };
-//
-//    let mut m = HashMap::new();
-//    m.insert("v",String::from("dsa"));
-//
-//
-//    let s = node_string.Eval(&m);
-//    println!("{}", s);
-//
-//    println!("{}", m["v"].as_str());
-//
-//
-//    TimeUtil::Count_Time(1, Local::now());
-//}
+fn main() {
+    let node_string = NodeString { buf: "".to_string() };
 
-////load a xml file
-//fn main() {
-//    // --snip--
-//    let filename="D:/RustProject/RustMybatis/src/example/Example_ActivityMapper.xml";
-//    println!("In file {}", filename);
-//
-//    let mut f = File::open(filename).expect("file not found");
-//
-//    let mut contents= String::new();
-//    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-//
-//    println!("With text:/n{}", contents);
-//}
+    let mut m = HashMap::new();
+    m.insert("v",String::from("dsa"));
+
+
+    let s = node_string.Eval(&m);
+    println!("{}", s);
+
+    println!("{}", m["v"].as_str());
+
+
+    TimeUtil::Count_Time(1, Local::now());
+}
+
+//load a xml file
+#[test]
+fn Test_load_file() {
+    // --snip--
+    let filename="D:/RustProject/RustMybatis/src/example/Example_ActivityMapper.xml";
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents= String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    println!("With text:/n{}", contents);
+}
 
 //load xml
 #[test]
-fn Test(){
+fn Test_load_xml(){
     println!(">>>>>>>>>>>>>>>>>>>>>>start load >>>>>>>>>>>>>>>>>>>>>>>");
     utils::XmlLoader::LoadXml(String::from("D:/RustProject/RustMybatis/src/example/Example_ActivityMapper.xml"));
 }
