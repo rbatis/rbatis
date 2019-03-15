47487a81ed39e840ce1224f05a9e51df52ed4da7
ce <zhuxiujia@qq.com>
2019-03-15 09:52:38 +0800

    添加引擎接口

--- src/engines/ExpressionEngine.rs
+++ src/engines/ExpressionEngine.rs
@@ -4,8 +4,8 @@ use std::collections::HashMap;
 * 表达式引擎，T:lexer类型,R 返回类型
 **/
 pub trait ExpressionEngine<T, R> {
-    fn Name() -> String;
-    fn Lexer(lexerArg: String) -> (T, String);
-    fn Eval(lexerResult: T, arg: HashMap<&str, &str>) -> (R, String);
-    fn LexerAndEval(lexerArg: String, arg: HashMap<&str, &str>) -> (R, String);
+    fn Name(&self) -> String;
+    fn Lexer(&self,lexerArg: String) -> (T, String);
+    fn Eval(&self, lexerResult: T, arg: HashMap<&str, &str>) -> (R, String);
+    fn LexerAndEval(&self,lexerArg: String, arg: HashMap<&str, &str>) -> (R, String);
 }
--- src/engines/ExpressionEngineDefault.rs
+++ src/engines/ExpressionEngineDefault.rs
@@ -1,25 +1,40 @@
-use crate::engines::ExpressionEngine::ExpressionEngine;
+use engines::ExpressionEngine::ExpressionEngine;
 use std::collections::HashMap;
 use std::collections::hash_map::RandomState;
 
-struct ExpressionEngineDefault {
-
+pub struct ExpressionEngineDefault<R> {
+    result: R,
 }
 
-impl <T,R>ExpressionEngine<T,R> for ExpressionEngineDefault{
-    fn Name() -> String {
-       return  String::from("ExpressionEngineDefault");
+impl<R> ExpressionEngine<String, R> for ExpressionEngineDefault<R> {
+    fn Name(&self) -> String {
+        return String::from("ExpressionEngineDefault");
     }
 
-    fn Lexer(lexerArg: String) -> (T, String) {
+    fn Lexer(&self, lexerArg: String) -> (String, String) {
         unimplemented!()
     }
 
-    fn Eval(lexerResult: T, arg: HashMap<&str, &str, RandomState>) -> (R, String) {
+    fn Eval(&self, lexerResult: String, arg: HashMap<&str, &str, RandomState>) -> (R, String) {
         unimplemented!()
     }
 
-    fn LexerAndEval(lexerArg: String, arg: HashMap<&str, &str, RandomState>) -> (R, String) {
+    fn LexerAndEval(&self, lexerArg: String, arg: HashMap<&str, &str, RandomState>) -> (R, String) {
         unimplemented!()
     }
+}
+
+
+
+
+
+
+
+
+#[test]
+fn TestExpressionEngineDefault() {
+    let engine = ExpressionEngineDefault {
+        result: false,
+    };
+    println!("{}", engine.Name())
 }
