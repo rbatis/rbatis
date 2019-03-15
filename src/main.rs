60b2d2d1f26d4d6fe502a8bad25ef8d68f87de40
ce <zhuxiujia@qq.com>
2019-03-15 09:52:38 +0800

    添加引擎接口

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use self::ast::NodeString::NodeString;
 //use utils::TimeUtil;
 use chrono::Local;
 use std::collections::HashMap;
-use crate::ast::Node::Node;
+use ast::Node::Node;
 use std::fs::File;
 use std::io::{Read, BufReader};
 use xml::EventReader;
@@ -16,6 +16,9 @@ use std::fs;
 
 fn main() {
     utils::print_util::print_rust_mybatis();
+    let now=Local::now();
 
-    time_util::count_time(1, Local::now());
+
+
+    time_util::count_time(1, now);
 }
