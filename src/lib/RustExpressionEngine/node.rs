da7911e7c9c6c6b570a5ab68fdf0c3ecc4a219aa
zhuxiujia <zhuxiujia@qq.com>
2019-03-18 03:04:53 +0800

    add arg node

--- src/lib/RustExpressionEngine/node.rs
+++ src/lib/RustExpressionEngine/node.rs
@@ -21,13 +21,28 @@ pub enum NodeType {
 //抽象语法树节点
 pub trait Node {
     fn Type(&self) -> NodeType;
-    fn Eval(&self, env: Value) -> (Value, String);
+    fn Eval(&self, env: &Value) -> (Value, &str);
 }
 
 //参数节点
 pub struct ArgNode {
     pub value: String,
+    params: Vec<&'static str>,
+    paramsLen:usize,
     pub t: NodeType,
+}
+
+impl ArgNode {
+   pub fn new(v: &'static str) -> Self {
+        let pars:Vec<&'static str>=v.split('.').collect();
+        let len=pars.len();
+        return Self {
+            value: v.to_string(),
+            t: NArg,
+            params:pars,
+            paramsLen:len,
+        };
+    }
 }
 
 impl Node for ArgNode {
@@ -35,18 +50,20 @@ impl Node for ArgNode {
         return NArg;
     }
 
-    fn Eval(&self, env: Value) -> (Value, String) {
-        let params: Vec<_> = self.value.split('.').collect();
-        let paramsLen = params.len();
-        //: Value::Object(Map<String, Value>)
-        let mut result=&env;
-        for i in 0..paramsLen {
-            result = &result[params[i]];
-            if i == (paramsLen - 1) {
-                return (result.clone(), String::from(""));
+    fn Eval(&self, env: &Value) -> (Value, &str) {
+        if self.params.len() == 1 {
+            return (env[self.value.as_str()].clone(), "");
+        } else {
+            let paramsLen = self.params.len();
+            let mut result = env;
+            for i in 0..paramsLen {
+                result = &result[self.params[i]];
+                if i == (paramsLen - 1) {
+                    return (result.clone(), "");
+                }
             }
+            return (Value::Null, "");
         }
-        return (Value::Null, String::from(""));
     }
 }
 
@@ -62,7 +79,7 @@ impl Node for StringNode {
         return NString;
     }
 
-    fn Eval(&self, env: Value) -> (Value, String) {
-        return (Value::String(self.value.to_string()), String::from(""));
+    fn Eval(&self, env: &Value) -> (Value, &str) {
+        return (Value::String(self.value.to_string()), "");
     }
 }
