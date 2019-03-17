9614eac0dad542b8b81445be876fa5e5134d40c3
zhuxiujia <zhuxiujia@qq.com>
2019-03-17 23:47:54 +0800

    fix bug

--- src/lib/RustExpressionEngine/parser.rs
+++ src/lib/RustExpressionEngine/parser.rs
@@ -1,24 +1,5 @@
-extern crate serde_json;
-
-use serde_json::json;
-use serde_json::Value;
 
 pub fn Parser() -> String {
     return String::from("ds");
 }
 
-#[test]
-fn TestParser() {
-    let john = json!({
-        "name": "John Doe",
-        "age": Value::Null,
-        "phones": [
-            "+44 1234567",
-            "+44 2345678"
-        ]
-    });
-
-
-    let age = &john["age"];
-    println!("{}", *age);
-}
