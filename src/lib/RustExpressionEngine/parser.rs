739f9ec3e0cba9ab13b2680073b5927a71c7396b
ce <zhuxiujia@qq.com>
2019-03-18 11:06:21 +0800

    add opt map

--- src/lib/RustExpressionEngine/parser.rs
+++ src/lib/RustExpressionEngine/parser.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+pub struct  OptMap<'a> {
+    pub OpsMap:HashMap< &'a str,bool>
+}
+
+impl <'a>OptMap<'a>{
+    pub fn new()->Self{
+        let mut defMap=HashMap::new();
+        defMap.insert("+",true);
+        defMap.insert("-",true);
+        defMap.insert("*",true);
+        defMap.insert("/",true);
+
+        defMap.insert("==",true);
+        defMap.insert("!=",true);
+        defMap.insert(">=",true);
+        defMap.insert("<=",true);
+
+        Self{
+            OpsMap:defMap
+        }
+    }
+}
 
 pub fn Parser() -> String {
     return String::from("ds");
