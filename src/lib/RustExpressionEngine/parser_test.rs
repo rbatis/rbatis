8c18916b5f93924483ba986e70d34e3f6e7c0b57
zhuxiujia <zhuxiujia@qq.com>
2019-03-18 21:47:52 +0800

    add Parser

--- src/lib/RustExpressionEngine/parser_test.rs
+++ src/lib/RustExpressionEngine/parser_test.rs
@@ -1,10 +1,24 @@
 use crate::lib::RustExpressionEngine::parser;
 use crate::lib::RustExpressionEngine::parser::OptMap;
+use serde_json::json;
+use serde_json::Value;
 
 #[test]
 fn TestParser() {
-    let vecStr = parser::Parser(String::from("a<=b+1-2>=1"));
-    for item in vecStr {
-        println!("item:{}", item);
-    }
+    let (boxNode,_ )= parser::Parser(String::from("a<=b+1-2>=1"));
+    let john = json!({
+        "name": "John Doe",
+        "age": Value::Null,
+         "sex":{
+            "a":"i'm a",
+            "b":"i'm b",
+         },
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    });
+    let (v,_)=boxNode.Eval(&john);
+    println!("item:{}", v);
+
 }
