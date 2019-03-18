7a21b1ffe61c9ec888500f0c6929466d272f2ae0
ce <zhuxiujia@qq.com>
2019-03-18 17:04:50 +0800

    add BinaryNode

--- src/lib/RustExpressionEngine/parser_test.rs
+++ src/lib/RustExpressionEngine/parser_test.rs
@@ -8,28 +8,3 @@ fn TestParser() {
         println!("item:{}", item);
     }
 }
-
-#[test]
-fn TestParserString() {
-    let s = String::from("ab+b");
-    println!("{}", s);
-
-    //let optMap = OptMap::new();
-
-    let mut tokens: Vec<String> = vec![];
-
-    let mut temp = String::new();
-    for c in (&s).chars() {
-        if c == ' ' {
-            continue;
-        }
-//        let op = optMap.OpsMap.get(c + tokens[tokens.len() - 1]);
-//        if op.unwrap_or_else(false)==true{
-//            //is opt
-//            tokens[c + tokens[tokens.len() - 1]]=
-//        }
-        temp.push(c);
-
-        println!("{}", temp);
-    }
-}
