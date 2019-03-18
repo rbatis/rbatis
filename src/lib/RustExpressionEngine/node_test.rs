75c39780cad9c94c805b003d879e5c964ff60926
ce <zhuxiujia@qq.com>
2019-03-18 13:30:14 +0800

    add number node

--- src/lib/RustExpressionEngine/node_test.rs
+++ src/lib/RustExpressionEngine/node_test.rs
@@ -1,4 +1,4 @@
-use crate::lib::RustExpressionEngine::node::{StringNode, Node, ArgNode};
+use crate::lib::RustExpressionEngine::node::{StringNode, Node, ArgNode, NumberNode};
 use crate::lib::RustExpressionEngine::node::NodeType::{NString, NArg};
 use serde_json::Value;
 use serde_json::json;
@@ -52,11 +52,30 @@ fn BenchmarkArgNode() {
 
     let argNode = ArgNode::new("sex.a");
 
-    let total=100000;
-    let now=Local::now();
-    for i in 0..total{
+    let total = 100000;
+    let now = Local::now();
+    for i in 0..total {
         argNode.Eval(&john);
     }
     time_util::count_time(total, now);
     time_util::count_tps(total, now);
+}
+
+#[test]
+fn TestNumberNode() {
+    let john = json!({
+        "name": "John Doe",
+        "age": 1,
+         "sex":{
+            "a":"i'm a",
+            "b":"i'm b",
+         },
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    });
+    let numb = NumberNode::new(String::from("1.02"));
+    let (value, _) = numb.Eval(&john);
+    println!("{}", value);
 }
