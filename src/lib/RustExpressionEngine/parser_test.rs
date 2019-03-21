8e2b7e7372c72c079d7e669e288dd2a6d7cb0515
ce <zhuxiujia@qq.com>
2019-03-22 02:07:49 +0800

    add runtime opt map

--- src/lib/RustExpressionEngine/parser_test.rs
+++ src/lib/RustExpressionEngine/parser_test.rs
@@ -2,23 +2,30 @@ use crate::lib::RustExpressionEngine::parser;
 use serde_json::json;
 use serde_json::Value;
 use crate::lib::RustExpressionEngine::runtime::OptMap;
+use crate::lib::RustExpressionEngine::node::Node;
+//#[test]
+//fn TestParser() {
+//    let (boxNode,_ )= parser::Parser(String::from("a<=b+1-2>=1=='asdf + sdaf '"),&OptMap::new());
+//    let john = json!({
+//        "name": "John Doe",
+//        "age": Value::Null,
+//         "sex":{
+//            "a":"i'm a",
+//            "b":"i'm b",
+//         },
+//        "phones": [
+//            "+44 1234567",
+//            "+44 2345678"
+//        ]
+//    });
+//    let (v,_)=boxNode.Eval(&john);
+//    println!("item:{}", v);
+//
+//}
 
 #[test]
-fn TestParser() {
-    let (boxNode,_ )= parser::Parser(String::from("a<=b+1-2>=1=='asdf + sdaf '"),&OptMap::new());
-    let john = json!({
-        "name": "John Doe",
-        "age": Value::Null,
-         "sex":{
-            "a":"i'm a",
-            "b":"i'm b",
-         },
-        "phones": [
-            "+44 1234567",
-            "+44 2345678"
-        ]
-    });
-    let (v,_)=boxNode.Eval(&john);
-    println!("item:{}", v);
+fn TestParser2() {
+   let (n,e)= parser::Parser(String::from("a<=b+1-2>=1=='asdf + sdaf '"),&OptMap::new());
 
+    println!("type:{}",n.Type())
 }
