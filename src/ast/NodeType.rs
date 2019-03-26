39faff28539545fcf05abe15d0a0ba1b8f4a5969
zhuxiujia <zhuxiujia@qq.com>
2019-03-26 21:46:52 +0800

    优化

--- src/ast/NodeType.rs
+++ src/ast/NodeType.rs
@@ -1,13 +1,71 @@
+use std::rc::Rc;
+
 pub enum NodeType {
-    NArg(String),
-    NString(String),
-    NIf(String),
-    NTrim(String),
-    NForEach(String),
-    NChoose(String),
-    NOtherwise(String),
-    NWhen(String),
-    NBind(String),
-    NInclude(String),
+    Null,
+    NString(StringNode),
+    NIf(If),
+    NTrim(Trim),
+    NForEach(ForEach),
+    NChoose(Choose),
+    NOtherwise(Otherwise),
+    NWhen(When),
+    NBind(Bind),
+    NInclude(Include),
 }
+
+pub struct Bind {
+    pub name: String,
+    pub value: String,
+}
+
+pub struct Choose {
+    pub whenNodes: Vec<NodeType>,
+    pub otherwiseNode: Rc<NodeType>,
+}
+
+pub struct ForEach {
+    pub childs: Vec<NodeType>,
+    pub collection: String,
+    pub  index: String,
+    pub item: String,
+    pub open: String,
+    pub close: String,
+    pub separator: String,
+}
+
+pub struct If {
+    pub childs: Vec<NodeType>,
+    pub test: String,
+}
+
+pub struct Include {
+    pub childs: Vec<NodeType>,
+}
+
+pub struct Otherwise {
+    pub childs: Vec<NodeType>,
+}
+
+pub struct StringNode {
+    pub value: String,
+    //去重的，需要替换的express map
+    pub expressMap: Vec<String>,
+    //去重的，需要替换的express map
+    pub noConvertExpressMap: Vec<String>,
+}
+
+pub struct Trim {
+    pub childs: Vec<NodeType>,
+    pub prefix: String,
+    pub suffix: String,
+    pub suffixOverrides: String,
+    pub prefixOverrides: String,
+}
+
+pub struct When {
+    pub childs: Vec<NodeType>,
+    pub test: String,
+}
+
+
 
