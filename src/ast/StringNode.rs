a1dc61aea9cd29dd441b884abf831a0732708f8d
ce <zhuxiujia@qq.com>
2019-03-27 11:31:52 +0800

    String node 性能优化

--- src/ast/StringNode.rs
+++ src/ast/StringNode.rs
@@ -1,6 +1,7 @@
 use crate::utils::string_util;
 use crate::ast::Node::Node;
 use serde_json::Value;
+use std::collections::HashMap;
 
 /**
 *  string抽象节点
@@ -8,18 +9,26 @@ use serde_json::Value;
 pub struct StringNode {
     pub value: String,
     //去重的，需要替换的要sql转换express map
-    pub expressMap: Vec<String>,
+    pub expressMap: HashMap<String,String>,
     //去重的，需要替换的免sql转换express map
-    pub noConvertExpressMap: Vec<String>,
+    pub noConvertExpressMap: HashMap<String,String>,
 }
 
 impl StringNode {
    pub fn new(v: &str) -> Self {
         //TODO find v #[] and find v$[]
+       let mut  expressMap=HashMap::new();
+       for item in &string_util::findConvertString(v.to_string()){
+           expressMap.insert(item.clone(),"#{".to_owned()+item.as_str()+"}");
+       }
+       let mut  noConvertExpressMap=HashMap::new();
+       for item in &string_util::findNoConvertString(v.to_string()){
+           noConvertExpressMap.insert(item.clone(),"${".to_owned()+item.as_str()+"}");
+       }
         Self {
             value: v.to_string(),
-            expressMap: string_util::findConvertString(v.to_string()),
-            noConvertExpressMap: string_util::findNoConvertString(v.to_string()),
+            expressMap: expressMap,
+            noConvertExpressMap: noConvertExpressMap,
         }
     }
 }
@@ -27,8 +36,8 @@ impl StringNode {
 impl Node for StringNode {
     fn eval(&self, env: &Value) -> String {
         let mut result = self.value.clone();
-        for item in &self.expressMap {
-            result = result.replace(("#{".to_owned()+item.as_str()+"}").as_str(), env.get(item).unwrap_or(&Value::String(String::new())).as_str().unwrap_or(""));
+        for (item,value) in &self.expressMap {
+            result = result.replace(value, env.get(item).unwrap_or(&Value::String(String::new())).as_str().unwrap_or(""));
         }
         return result;
     }
