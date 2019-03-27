ac9f62203d1124258d8d1f33a015ea07a65b0502
ce <zhuxiujia@qq.com>
2019-03-27 10:31:41 +0800

    String node

--- src/ast/Node_test.rs
+++ src/ast/Node_test.rs
@@ -1,20 +1,16 @@
-use crate::ast::NodeType::{NodeType};
+use crate::ast::NodeType::NodeType;
 use crate::ast::Node::Node;
 use serde_json::json;
 use crate::ast::StringNode::StringNode;
 
 #[test]
-fn TestStringNode(){
+fn TestStringNode() {
     let john = json!({
         "name": "John Doe",
     });
 
-    let strNode=NodeType::NString(StringNode{
-        value: "vvvvvvvvvvvvvvvvvvvvv".to_string(),
-        expressMap: vec![],
-        noConvertExpressMap: vec![]
-    });
+    let strNode = NodeType::NString(StringNode::new("vvvvvvvvvv#{name}vvvvvvvv"));
 
-   let result=  strNode.eval(john);
-    println!("{}",result);
+    let result = strNode.eval(john);
+    println!("{}", result);
 }
--- src/ast/StringNode.rs
+++ src/ast/StringNode.rs
@@ -1,5 +1,6 @@
-
 use crate::utils::string_util;
+use crate::ast::Node::Node;
+use serde_json::Value;
 
 /**
 *  string抽象节点
@@ -12,13 +13,23 @@ pub struct StringNode {
     pub noConvertExpressMap: Vec<String>,
 }
 
-impl StringNode{
-    fn new(v:&String)->Self{
+impl StringNode {
+   pub fn new(v: &str) -> Self {
         //TODO find v #[] and find v$[]
-        Self{
-            value: v.clone(),
-            expressMap: string_util::findConvertString(v.clone()),
-            noConvertExpressMap: string_util::findNoConvertString(v.clone()),
+        Self {
+            value: v.to_string(),
+            expressMap: string_util::findConvertString(v.to_string()),
+            noConvertExpressMap: string_util::findNoConvertString(v.to_string()),
         }
+    }
+}
+
+impl Node for StringNode {
+    fn eval(&self, env: Value) -> String {
+        let mut result = self.value.clone();
+        for item in &self.expressMap {
+            result = result.replace(("#{".to_owned()+item.as_str()+"}").as_str(), env.get(item).unwrap_or(&Value::String(String::new())).as_str().unwrap_or(""));
+        }
+        return result;
     }
 }
