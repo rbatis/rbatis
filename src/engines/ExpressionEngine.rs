00f07e53488c60d7648516c53ccd46f04b9bb36c
zhuxiujia <zhuxiujia@qq.com>
2019-03-28 22:11:04 +0800

    add ExpressionEngineDefault

--- src/engines/ExpressionEngine.rs
+++ src/engines/ExpressionEngine.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use serde_json::Value;
 
 /**
 * 表达式引擎，T:lexer类型,R 返回类型
@@ -6,6 +7,6 @@ use std::collections::HashMap;
 pub trait ExpressionEngine<T, R> {
     fn Name(&self) -> String;
     fn Lexer(&self,lexerArg: String) -> (T, String);
-    fn Eval(&self, lexerResult: T, arg: HashMap<&str, &str>) -> (R, String);
-    fn LexerAndEval(&self,lexerArg: String, arg: HashMap<&str, &str>) -> (R, String);
+    fn Eval(&self, lexerResult: T, arg: &Value) -> (R, String);
+    fn LexerAndEval(&self,lexerArg: String, arg:&Value) -> (R, String);
 }
