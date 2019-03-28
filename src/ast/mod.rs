cb5d24d37d0ab973eb3bbd43c7aeac4531923a4d
ce <zhuxiujia@qq.com>
2019-03-28 21:00:13 +0800

    add test file

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -15,3 +15,12 @@ pub mod WhenNode;
 //test
 mod Node_test;
 mod SqlArgTypeConvertDefault_test;
+mod BindNode_test;
+mod ChooseNode_test;
+mod ForEachNode_test;
+mod IfNode_test;
+mod IncludeNode_test;
+mod OtherwiseNode_test;
+mod StringNode_test;
+mod TrimNode_test;
+mod WhenNode_test;
