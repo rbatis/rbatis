c0cc170db648f7341bd7877ab11732287586f23f
ce <zhuxiujia@qq.com>
2019-04-01 20:07:51 +0800

    edit cache

--- src/engines/ExpressionEngineCache.rs
+++ src/engines/ExpressionEngineCache.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
+use crate::lib::RustExpressionEngine::node::Node;
+use crate::ast::StringNode::StringNode;
+use crate::ast::SqlArgTypeConvertDefault::SqlArgTypeConvertDefault;
+use std::rc::Rc;
+use serde_json::Value;
 
 pub struct ExpressionEngineCache<'a> {
-    cache: HashMap<&'a str, &'a str>,
+    cache: HashMap<&'a str, Node>,
 }
 
 impl<'a> ExpressionEngineCache<'a> {
@@ -11,12 +16,12 @@ impl<'a> ExpressionEngineCache<'a> {
         };
     }
 
-    pub fn put(&mut self, k: &'a str, v: &'a str) {
+    pub fn put(&mut self, k: &'a str, v: Node) {
         &self.cache.insert(k, v);
     }
 
-    pub fn get(&mut self, k: &str) -> &'a str {
-        return &self.cache.get(k).unwrap_or(&"null");
+    pub fn get(&mut self, k: &str) -> Option<&Node> {
+        return self.cache.get(k);
     }
 }
 
@@ -25,9 +30,8 @@ fn TestCache() {
     let mut cache = ExpressionEngineCache::new();
     let v;
     {
-
-        cache.put("sadf", "vvv");
+        cache.put("sadf", Node::newString("asdf"));
         v = cache.get("sadf");
     }
-    println!("{}", v);
+    println!("{}", v.unwrap().eval(&Value::Null));
 }
