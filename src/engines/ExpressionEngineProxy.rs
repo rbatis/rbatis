c6d3a3bfddc6db542ba2370651768ae957b07dd6
ce <zhuxiujia@qq.com>
2019-04-02 16:18:03 +0800

    检查点

--- src/engines/ExpressionEngineProxy.rs
+++ src/engines/ExpressionEngineProxy.rs
@@ -10,7 +10,7 @@ pub struct ExpressionEngineProxy<'a, T, R> {
     cache: ExpressionEngineCache<'a, T>,
 }
 
-impl<'a, T:Copy, R:Copy> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T, R> {
+impl<'a, T:Clone, R:Clone> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T, R> {
     fn Name(&self) -> String {
         return self.expressionEngine.Name();
     }
@@ -24,7 +24,7 @@ impl<'a, T:Copy, R:Copy> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T,
     }
 }
 
-impl<'a, T:Copy, R:Copy> ExpressionEngineProxy<'a, T, R> {
+impl<'a, T:Clone, R:Clone> ExpressionEngineProxy<'a, T, R> {
     pub fn new(expressionEngine: Rc<ExpressionEngine<T, R>>, expressionEngineCache: ExpressionEngineCache<'a, T>) -> Self {
         Self {
             expressionEngine: expressionEngine,
@@ -36,7 +36,7 @@ impl<'a, T:Copy, R:Copy> ExpressionEngineProxy<'a, T, R> {
         let cached = self.cache.get(lexerArg);
         if cached.is_none() {
             let (nodes, e) = self.Lexer(lexerArg.to_string());
-            self.cache.put(lexerArg, nodes);
+            self.cache.put(lexerArg, nodes.clone());
             let (v, e) = self.Eval(&nodes, arg);
             return (v.clone(), e.clone());
         } else {
