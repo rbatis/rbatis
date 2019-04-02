bb5aa8cae86c801f78c14f05cf4c9b51f4e4c149
ce <zhuxiujia@qq.com>
2019-04-02 15:55:35 +0800

    检查点

--- src/ast/BindNode_test.rs
+++ src/ast/BindNode_test.rs
@@ -3,13 +3,15 @@ use std::rc::Rc;
 use crate::engines::ExpressionEngineProxy::ExpressionEngineProxy;
 use crate::ast::Node::SqlNode;
 use serde_json::json;
+use crate::engines::ExpressionEngineDefault::ExpressionEngineDefault;
+use crate::engines::ExpressionEngineCache::ExpressionEngineCache;
 
 #[test]
 fn TestBindNode(){
 //    let bindNode=BindNode{
 //        name: "".to_string(),
 //        value: "".to_string(),
-//        engine: Rc::new(ExpressionEngineProxy::newDefault()),
+//        engine: Rc::new(ExpressionEngineProxy::new(Rc::new(ExpressionEngineDefault::new()),ExpressionEngineCache::new())),
 //    };
 //
 //    bindNode.eval(&json!("123321"));
--- src/engines/ExpressionEngineProxy.rs
+++ src/engines/ExpressionEngineProxy.rs
@@ -4,12 +4,13 @@ use crate::engines::ExpressionEngineCache::ExpressionEngineCache;
 use std::rc::Rc;
 use crate::engines::ExpressionEngineDefault::ExpressionEngineDefault;
 
+#[derive(Clone)]
 pub struct ExpressionEngineProxy<'a, T, R> {
     expressionEngine: Rc<ExpressionEngine<T, R>>,
     cache: ExpressionEngineCache<'a, T>,
 }
 
-impl<'a, T, R> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T, R> {
+impl<'a, T:Copy, R:Copy> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T, R> {
     fn Name(&self) -> String {
         return self.expressionEngine.Name();
     }
@@ -23,7 +24,7 @@ impl<'a, T, R> ExpressionEngine<T, R> for ExpressionEngineProxy<'a, T, R> {
     }
 }
 
-impl<'a, T: Copy, R: Copy> ExpressionEngineProxy<'a, T, R> {
+impl<'a, T:Copy, R:Copy> ExpressionEngineProxy<'a, T, R> {
     pub fn new(expressionEngine: Rc<ExpressionEngine<T, R>>, expressionEngineCache: ExpressionEngineCache<'a, T>) -> Self {
         Self {
             expressionEngine: expressionEngine,
@@ -31,25 +32,15 @@ impl<'a, T: Copy, R: Copy> ExpressionEngineProxy<'a, T, R> {
         }
     }
 
-//    pub fn newDefault() -> Self {
-//
-//        let engine:Rc<ExpressionEngine<T, R>> = Rc::new(ExpressionEngineDefault::new());
-//
-//        Self {
-//            expressionEngine: engine,
-//            cache: ExpressionEngineCache::new(),
-//        }
-//    }
-
     pub fn LexerAndEval(&mut self, lexerArg: &'a str, arg: &Value) -> (R, String) {
-        let  cached = self.cache.get(lexerArg);
+        let cached = self.cache.get(lexerArg);
         if cached.is_none() {
             let (nodes, e) = self.Lexer(lexerArg.to_string());
             self.cache.put(lexerArg, nodes);
             let (v, e) = self.Eval(&nodes, arg);
             return (v.clone(), e.clone());
         } else {
-            let c=cached.unwrap().clone();
+            let c = cached.unwrap().clone();
             let (v, e) = self.Eval(&c, arg);
             return (v.clone(), e.clone());
         }
