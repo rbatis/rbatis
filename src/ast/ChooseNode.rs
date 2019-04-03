4b8fc87a18effcbbae9f361c1ca2ada95b86467d
ce <zhuxiujia@qq.com>
2019-04-03 09:32:58 +0800

    choose

--- src/ast/ChooseNode.rs
+++ src/ast/ChooseNode.rs
@@ -4,11 +4,23 @@ use crate::ast::Node::SqlNode;
 use serde_json::Value;
 
 pub struct ChooseNode<'a> {
-    pub whenNodes: Vec<NodeType<'a>>,
-    pub otherwiseNode: Rc<NodeType<'a>>,
+    pub whenNodes: Option<Vec<NodeType<'a>>>,
+    pub otherwiseNode: Option<Rc<NodeType<'a>>>,
 }
-impl <'a> SqlNode for ChooseNode<'a>{
+
+impl<'a> SqlNode for ChooseNode<'a> {
     fn eval(&mut self, env: &mut Value) -> String {
         unimplemented!()
+//        let whenIsNone = self.whenNodes.is_none();
+//        let otherIsNone = self.otherwiseNode.is_none();
+//        if whenIsNone || otherIsNone {
+//            return String::new();
+//        }
+//        for mut item in self.whenNodes.unwrap() {
+//            let s = item.eval(env);
+//
+//        }
+//
+//        return String::new();
     }
 }
