02c49b57baa020ac8d7317e8e38a1dba6fb5dc1b
ce <zhuxiujia@qq.com>
2019-04-03 10:13:58 +0800

    ast return Result<String,String>

--- src/ast/ChooseNode.rs
+++ src/ast/ChooseNode.rs
@@ -2,25 +2,30 @@ use crate::ast::NodeType::NodeType;
 use std::rc::Rc;
 use crate::ast::Node::SqlNode;
 use serde_json::Value;
+use core::borrow::BorrowMut;
+use crate::ast::OtherwiseNode::OtherwiseNode;
+use std::ops::DerefMut;
 
+#[derive(Clone)]
 pub struct ChooseNode<'a> {
     pub whenNodes: Option<Vec<NodeType<'a>>>,
-    pub otherwiseNode: Option<Rc<NodeType<'a>>>,
+    pub otherwiseNode: Box<NodeType<'a>>,
 }
 
 impl<'a> SqlNode for ChooseNode<'a> {
-    fn eval(&mut self, env: &mut Value) -> Result<String,String> {
-        unimplemented!()
-//        let whenIsNone = self.whenNodes.is_none();
-//        let otherIsNone = self.otherwiseNode.is_none();
-//        if whenIsNone || otherIsNone {
-//            return String::new();
-//        }
-//        for mut item in self.whenNodes.unwrap() {
-//            let s = item.eval(env);
-//
-//        }
-//
-//        return String::new();
+    fn eval(&mut self, env: &mut Value) -> Result<String, String> {
+        let whenIsNone = self.whenNodes.is_none();
+        //let otherIsNone = self.otherwiseNode.is_none();
+        if whenIsNone {
+            return Result::Ok(String::new());
+        }
+        for mut item in self.whenNodes.clone().unwrap() {
+            let s = item.eval(env);
+            if s.is_ok() {
+                return s;
+            }
+        }
+        let mut node = self.otherwiseNode.deref_mut().eval(env);
+        return node;
     }
 }
