deee14d6b5f30c2834d110023c50b927c881b744
ce <zhuxiujia@qq.com>
2019-04-03 10:28:18 +0800

    ChooseNode

--- src/ast/ChooseNode_test.rs
+++ src/ast/ChooseNode_test.rs
@@ -1,6 +1,25 @@
+use crate::ast::ChooseNode::ChooseNode;
+use crate::ast::NodeType::NodeType::NString;
+use crate::ast::StringNode::StringNode;
+use std::rc::Rc;
+use crate::ast::SqlArgTypeConvertDefault::SqlArgTypeConvertDefault;
+use crate::ast::Node::SqlNode;
+use serde_json::json;
+
 
 #[test]
-pub fn TestChooseNode(){
+pub fn TestChooseNode() {
+    let mut john = json!({
+        "a": 1,
+    });
 
+    let sNode = NString(StringNode::new("dsaf", Rc::new(SqlArgTypeConvertDefault::new())));
 
+    let mut c = ChooseNode {
+        whenNodes: Option::Some(vec![sNode]),
+        otherwiseNode: None,
+    };
+
+    let r = c.eval(&mut john);
+    println!("{}", r.unwrap());
 }
