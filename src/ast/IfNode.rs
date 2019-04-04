0d95884500dd8860127c00638ec4b85e49010418
ce <zhuxiujia@qq.com>
2019-04-04 12:20:54 +0800

    if node

--- src/ast/IfNode.rs
+++ src/ast/IfNode.rs
@@ -5,11 +5,12 @@ use serde_json::Value;
 #[derive(Clone)]
 pub struct IfNode {
     pub childs: Vec<NodeType>,
+    pub t: NodeType,
     pub test: String,
 }
 
-impl  SqlNode for IfNode{
-    fn eval(&mut self, env: &mut Value) -> Result<String,String> {
+impl SqlNode for IfNode {
+    fn eval(&mut self, env: &mut Value) -> Result<String, String> {
         unimplemented!()
     }
 }
