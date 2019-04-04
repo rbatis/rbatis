5a6b22c35a8454190caa1e05a4e3749d6478c83d
ce <zhuxiujia@qq.com>
2019-04-04 15:15:33 +0800

    TrimNode

--- src/ast/TrimNode.rs
+++ src/ast/TrimNode.rs
@@ -1,5 +1,5 @@
 use crate::ast::NodeType::NodeType;
-use crate::ast::Node::SqlNode;
+use crate::ast::Node::{SqlNode, DoChildNodes};
 use serde_json::Value;
 
 #[derive(Clone)]
@@ -12,7 +12,30 @@ pub struct TrimNode {
 }
 
 impl SqlNode for TrimNode {
-    fn eval(&mut self, env: &mut Value) -> Result<String,String> {
-        unimplemented!()
+    fn eval(&mut self, env: &mut Value) -> Result<String, String> {
+        let resultValue = DoChildNodes(&mut self.childs, env);
+        let isError = resultValue.is_err();
+        if isError {
+            return Result::Err(resultValue.clone().err().unwrap());
+        }
+        let mut resultStr = resultValue.unwrap();
+        let mut result = resultStr.as_str().trim();
+
+        if !self.prefixOverrides.is_empty() {
+            let splits: Vec<&str> = self.prefixOverrides.split("|").collect();
+            for item in splits {
+                result = result.trim_start_matches(item);
+            }
+        }
+        if !self.suffixOverrides.is_empty() {
+            let splits: Vec<&str> = self.suffixOverrides.split("|").collect();
+            for item in splits {
+                result = result.trim_end_matches(item);
+            }
+        }
+
+        let mut newBuffer = String::new();
+        newBuffer = newBuffer + " " + self.prefix.as_str() + " " + result + " " + self.suffix.as_str();
+        return Result::Ok(newBuffer);
     }
 }
