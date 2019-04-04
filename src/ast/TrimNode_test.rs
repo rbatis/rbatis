30891fc33df8fff1b526dcfcc3ab271eabf14c7a
ce <zhuxiujia@qq.com>
2019-04-04 15:15:33 +0800

    TrimNode

---
+++ src/ast/TrimNode_test.rs
@@ -0,0 +1,23 @@
+use crate::ast::TrimNode::TrimNode;
+use serde_json::json;
+use crate::ast::Node::SqlNode;
+use crate::ast::NodeType::NodeType;
+use crate::ast::StringNode::StringNode;
+use crate::ast::NodeConfigHolder::NodeConfigHolder;
+
+#[test]
+pub fn TestTrimNode(){
+    let mut node =TrimNode{
+        childs: vec![NodeType::NString(StringNode::new("1trim value1",Box::new(NodeConfigHolder::new())))],
+        prefix: "(".to_string(),
+        suffix: ")".to_string(),
+        suffixOverrides: "1".to_string(),
+        prefixOverrides: "1".to_string()
+    };
+    let mut john = json!({
+        "arg": 2,
+    });
+
+   let r= node.eval(&mut john).unwrap();
+    println!("{}",r)
+}
