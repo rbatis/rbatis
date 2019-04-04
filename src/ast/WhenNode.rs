efebc465434a4332b06728fd75338faa0ffe4616
ce <zhuxiujia@qq.com>
2019-04-04 14:53:00 +0800

    WhenNode

--- src/ast/WhenNode.rs
+++ src/ast/WhenNode.rs
@@ -1,5 +1,5 @@
 use crate::ast::NodeType::NodeType;
-use crate::ast::Node::SqlNode;
+use crate::ast::Node::{SqlNode, DoChildNodes};
 use serde_json::Value;
 use crate::ast::NodeConfigHolder::NodeConfigHolder;
 
@@ -13,6 +13,17 @@ pub struct WhenNode {
 
 impl  SqlNode for WhenNode{
     fn eval(&mut self, env: &mut Value) -> Result<String,String> {
-        unimplemented!()
+        let resultValue = self.holder.engine.LexerAndEval(self.test.as_str(), env);
+        if resultValue.is_err(){
+            return Result::Err(resultValue.err().unwrap());
+        }
+        let result=resultValue.unwrap();
+        if !result.is_boolean(){
+            return Result::Err("[RustMybatis] test:'".to_owned()+self.test.as_str()+"' is not return bool!");
+        }
+        if result.as_bool().unwrap() {
+            return DoChildNodes(&mut self.childs, env);
+        }
+        return Result::Ok("".to_string());
     }
 }
