0d40f5c9c774c69343d2da86177f97011baca6df
ce <zhuxiujia@qq.com>
2019-04-04 14:23:26 +0800

    ifnode

--- src/ast/IfNode.rs
+++ src/ast/IfNode.rs
@@ -1,7 +1,8 @@
 use crate::ast::NodeType::NodeType;
-use crate::ast::Node::SqlNode;
+use crate::ast::Node::{SqlNode, DoChildNodes};
 use serde_json::Value;
 use crate::ast::NodeConfigHolder::NodeConfigHolder;
+use serde_json::ser::State::Rest;
 
 #[derive(Clone)]
 pub struct IfNode {
@@ -12,6 +13,17 @@ pub struct IfNode {
 
 impl SqlNode for IfNode {
     fn eval(&mut self, env: &mut Value) -> Result<String, String> {
-        unimplemented!()
+        let result = self.holder.engine.LexerAndEval(self.test.as_str(), env);
+        if result.is_err() {
+            return Result::Err(result.err().unwrap());
+        }
+        let b = &result.unwrap();
+        if !b.is_boolean() {
+           return  Result::Err("[RustMybatis] express:'".to_owned() + self.test.as_str() + "' is not return bool value!");
+        }
+        if b.as_bool().unwrap() {
+            return DoChildNodes(&mut self.childs, env);
+        }
+        return Result::Ok("".to_string());
     }
 }
--- src/ast/IfNode_test.rs
+++ src/ast/IfNode_test.rs
@@ -1,6 +1,21 @@
 use serde_json::json;
+use crate::ast::IfNode::IfNode;
+use crate::ast::StringNode::StringNode;
+use crate::ast::NodeConfigHolder::NodeConfigHolder;
+use crate::ast::NodeType::NodeType;
+use crate::ast::Node::SqlNode;
+use std::rc::Rc;
 
 #[test]
-pub fn TestIfNode(){
-
+pub fn TestIfNode() {
+    let h = Box::new(NodeConfigHolder::new());
+    let mut node = IfNode {
+        childs: vec![NodeType::NString(StringNode::new("yes", h.clone()))],
+        test: "arg == 1".to_string(),
+        holder: h,
+    };
+    let mut john = json!({
+        "arg": 1,
+    });
+    println!("{}", node.eval(&mut john).unwrap());
 }
