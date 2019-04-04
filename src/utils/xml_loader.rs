08184c53ef768e0b8f8896b9d51f7007f1c60b92
ce <zhuxiujia@qq.com>
2019-04-04 17:56:39 +0800

    add decode

--- src/utils/xml_loader.rs
+++ src/utils/xml_loader.rs
@@ -10,12 +10,21 @@ use std::fmt::Error;
 use core::borrow::Borrow;
 use self::xml::attribute::OwnedAttribute;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Element {
     pub tag: String,
     pub data: String,
     pub attributes: Vec<OwnedAttribute>,
     pub childs: Vec<Element>,
+}
+
+impl Element {
+    pub fn reset(&mut self) {
+        self.tag.clear();
+        self.data.clear();
+        self.attributes.clear();
+        self.childs.clear();
+    }
 }
 
 
@@ -27,34 +36,77 @@ pub fn load_xml(mut file_content: &mut String) {
 
 fn parserFunc(parser: EventReader<&[u8]>) {
     let mut depth = 0;
+
+    let mut tempElement = &mut Element {
+        tag: "".to_string(),
+        data: "".to_string(),
+        attributes: vec![],
+        childs: vec![],
+    };
+
+    let mut fathers = vec![];
+
     for item in parser {
+        //println!("depth={}", depth);
         match item {
             Ok(XmlEvent::StartElement { name, attributes, .. }) => {
                 print!("{} <{} ", indent(depth), name);
-                depth += 1;
+
 
                 //load attr
                 if attributes.len() != 0 {
-                    for item in attributes {
+                    for item in &attributes {
                         print!("{}=\"{}\" ", item.name, item.value)
                     }
                 }
                 println!(">");
+
+                tempElement.tag = name.local_name;
+                tempElement.attributes = attributes.clone();
+
+                &fathers.push(tempElement.clone());
+                depth += 1;
             }
             Ok(XmlEvent::Characters(data)) => {
                 println!("{} {}", indent(depth), data);
+//                tempElement.childs.push(Element{
+//                    tag: "".to_string(),
+//                    data: data.clone(),
+//                    attributes: vec![],
+//                    childs: vec![]
+//                });
+                tempElement.data=data;
+                let last=fathers.last_mut().unwrap();
+                *last=tempElement.clone();
             }
             Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
                 println!("{} </{}>", indent(depth), name);
+                //parent
+//                if name.local_name=="mapper" {
+//                    continue;
+//                }
+                if depth==1{
+                    continue;
+                }
+
+                let pop=fathers.pop().unwrap();
+                let last = fathers.last_mut();
+                if last.is_some() {
+                    last.unwrap().childs.push(pop);
+                }
+                tempElement.reset();
+
+                depth -= 1;
             }
             Err(e) => {
-                println!("Error: {}", e);
-                break;
+                println!("Error: {},{}", e,tempElement.tag);
+               break;
             }
             _ => {}
         }
     }
+
+    println!("result>>>>>>>  {}", fathers.len());
 }
 
 fn indent(size: usize) -> String {
