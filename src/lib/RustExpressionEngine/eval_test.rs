eabf60eac37150fb5e58272a468488ba61d9ab89
zhuxiujia <zhuxiujia@qq.com>
2019-04-06 11:34:00 +0800

    add check

--- src/lib/RustExpressionEngine/eval_test.rs
+++ src/lib/RustExpressionEngine/eval_test.rs
@@ -3,6 +3,7 @@ use crate::lib::RustExpressionEngine::runtime::OptMap;
 use chrono::Local;
 use serde_json::json;
 use test::Bencher;
+use std::{time, thread};
 
 #[bench]
 fn Bench_Parser(b: &mut Bencher) {
@@ -18,4 +19,43 @@ fn Bench_Parser(b: &mut Bencher) {
     b.iter(|| {
         boxNode.eval(&john);
     });
+}
+
+#[test]
+fn TestMemGC() {
+    let mut boxNode= parser::Parser(String::from("'1'+'1'"), &OptMap::new()).unwrap();
+    let john = json!({
+        "n":1,
+        "name": "John Doe",
+         "age": {
+           "yes":"sadf"
+        }
+    });
+
+    let total=1000000;
+    println!("start");
+    for i in 0..1000000{
+        boxNode.eval(&john);
+        if i==(total-1){
+            println!("999999");
+            let ten_millis = time::Duration::from_secs(5);
+            thread::sleep(ten_millis);
+        }
+    }
+    for i in 0..1000000{
+        boxNode.eval(&john);
+        if i==(total-1){
+            println!("999999");
+            let ten_millis = time::Duration::from_secs(5);
+            thread::sleep(ten_millis);
+        }
+    }
+    for i in 0..1000000{
+        boxNode.eval(&john);
+        if i==(total-1){
+            println!("999999");
+            let ten_millis = time::Duration::from_secs(5);
+            thread::sleep(ten_millis);
+        }
+    }
 }
