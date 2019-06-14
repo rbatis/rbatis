65cdd3e8657aff4a38589a3dd3d7a2a013cbdbe2
ce <zhuxiujia@qq.com>
2019-06-14 13:02:06 +0800

    edit decode

--- src/SqlBuilder.rs
+++ src/SqlBuilder.rs
@@ -10,12 +10,9 @@ use std::fmt::Display;
 use postgres::{Connection, TlsMode};
 use core::borrow::Borrow;
 use mysql::Value;
+use serde::{Serialize, Deserialize};
 
-pub struct SqlBuilder {
-
-}
-
-
+pub struct SqlBuilder {}
 
 
 #[test]
@@ -58,7 +55,7 @@ fn TestSqlBuilder() {
             sql.push_str(&" ");
         }
         let sizeStr = paramMap.get("size").unwrap();
-        let size:i32 = sizeStr.parse().unwrap();
+        let size: i32 = sizeStr.parse().unwrap();
         if size != 0 {
             sql.push_str(&size.to_string());
             sql.push_str(&" ");
@@ -71,28 +68,69 @@ fn TestSqlBuilder() {
 }
 
 
-
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Act {
+    pub id: String,
+    pub name: String,
+    pub version: i32,
+}
 
 
 #[test]
 fn TestLinkMysql() {
     let mut ops = mysql::OptsBuilder::new();
     ops.user(Option::Some("root"));
-    ops.pass(Option::Some("root"));
+    ops.pass(Option::Some("TEST"));
     ops.db_name(Option::Some("test"));
+    ops.ip_or_hostname(Option::Some("115.220.9.139"));
 
 
     let mut conn = mysql::Conn::new(ops).unwrap();
     for row in conn.prep_exec("SELECT * from biz_activity limit 1;", ()).unwrap() {
         let a = row.unwrap();
-        let cs=a.columns();
-        for t in cs.as_ref(){
-            let columnName=t.name_str();
+
+
+        let mut serialized = "{".to_owned();
+
+        let cs = a.columns();
+        for t in cs.as_ref() {
+            let columnName = t.name_str();
             println!("{:?}", columnName);
-            let k=columnName.as_ref();
-            let f:Value=a.get(k).unwrap();
+            let k = columnName.as_ref();
+            let f: Value = a.get(k).unwrap();
             println!("{:?}", f);
+
+            serialized = serialized + "\"" + columnName.as_ref() + "\"";
+
+            let mut sql = f.as_sql(true);
+            if sql.as_str() == "NULL" {
+                sql = "null".to_string();
+            } else {
+                if sql == "''" {
+                    sql = "\"\"".to_owned();
+                } else {
+                    let sqlLen = sql.len();
+                    let first = sql.find("'").unwrap_or_default();
+                    let last = sql.rfind("'").unwrap_or_default();
+                    if first == 0 && last == (sqlLen - 1) && first != last {
+                        let slice = &sql[1..(sqlLen - 1)];
+                        sql = "\"".to_owned() + slice + "\"";
+                    }
+                }
+            }
+
+            serialized = serialized + ":" + sql.as_str() + ",";
         }
+        serialized.pop();
+        serialized = serialized + "}";
+        println!("query sql result ==> {}", serialized);
+
+        //decode
+        // Convert the JSON string back to a Point.
+        let deserialized: Act = serde_json::from_str(&serialized).unwrap();
+
+        // Prints deserialized = Point { x: 1, y: 2 }
+        println!("deserialized = {:?}", deserialized);
     }
 }
 
@@ -107,16 +145,16 @@ fn TestLinkPostgres() {
     conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
                  &[&"Steven".to_string(), &"".to_string()]).unwrap();
     for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
-        let mut personMap =HashMap::new();
+        let mut personMap = HashMap::new();
         //personMap.insert(0,row.get(0));
-        let s:String=row.get(1);
-        personMap.insert("1",s);
+        let s: String = row.get(1);
+        personMap.insert("1", s);
         //personMap.insert(2,row.get(2));
-        println!("Found person {}",personMap.get("1").unwrap());
+        println!("Found person {}", personMap.get("1").unwrap());
     }
 }
 
 #[test]
-fn TestPool(){
-    println!("{}","dsaf")
+fn TestPool() {
+    println!("{}", "dsaf")
 }
