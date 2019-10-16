2fc222a9bc7dcd83c5360adb78849377be09c736
zhuxiujia <zhuxiujia@qq.com>
2019-10-17 00:49:59 +0800

    add test

--- src/core/Rbatis.rs
+++ src/core/Rbatis.rs
@@ -1,7 +1,8 @@
 use crate::ast::NodeConfigHolder::NodeConfigHolder;
-use crate::ast::Node::SqlNode;
+use crate::ast::Node::{SqlNode, LoopDecodeXml};
 use crate::ast::BindNode::BindNode;
 use crate::ast::StringNode::StringNode;
+use crate::utils::xml_loader::load_xml;
 
 pub struct  Rbatis{
 
@@ -12,6 +13,10 @@ impl Rbatis{
 
     pub fn build(&self,xml_content:String) -> impl SqlNode{
         //TODO load xml_content string,create ast
+
+        let nodes= load_xml(xml_content);
+        let result= LoopDecodeXml(nodes);
+
         let holder=NodeConfigHolder::new();
         let mut bindNode =StringNode::new("----------Str---------",Box::new(holder));
         return bindNode;
