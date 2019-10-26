ec438c4215bf3263633227d0f0ca439c1c9ff58c
zhuxiujia <zhuxiujia@qq.com>
2019-10-26 23:25:16 +0800

    add pg decode support

--- src/decode/PostgresJsonDecoder.rs
+++ src/decode/PostgresJsonDecoder.rs
@@ -1,12 +1,52 @@
-use crate::decode::Decoder::Decoder;
-use postgres::rows::Rows;
+use crate::decode::Decoder::{Decoder, isJsonArrayType};
+use postgres::rows::{Rows, Row};
 use serde::de;
 use rbatis_macro::RbatisMacro;
+use std::borrow::BorrowMut;
+use serde_json::Value;
 
 //PG 解析器
 impl Decoder for Rows{
     fn decode<T>(&mut self) -> Result<T, String> where
         T: de::DeserializeOwned + RbatisMacro {
-        unimplemented!()
+        //unimplemented!();
+        let mut js = serde_json::Value::Null;
+        if isJsonArrayType(T::decode_name()) {
+            //is array json
+            let mut vec_v = vec![];
+            for i in 0..self.len(){
+                let item=self.get(i);
+                let act= decodeRow(&item);
+                vec_v.push(act);
+            }
+            js = serde_json::Value::Array(vec_v);
+        }else{
+            let mut result: Result<T, String> = Result::Err("[Rbatis] rows.affected_rows > 1,but decode one result!".to_string());
+            //not array json
+            let mut index = 0;
+            for i in 0..self.len() {
+                let item = self.get(i);
+                if index > 1 {
+                    continue;
+                }
+                js = decodeRow(&item);
+                index = index + 1;
+            }
+            if index > 0 {
+                return result;
+            }
+        }
+        let decodeR = serde_json::from_value(js);
+        if decodeR.is_ok() {
+            return Result::Ok(decodeR.unwrap());
+        } else {
+            let e = decodeR.err().unwrap().to_string();
+            return Result::Err(e);
+        }
     }
+
+}
+
+fn decodeRow(row: &Row) -> Value {
+    unimplemented!()
 }
