30e052f1690df0f779f26d4492c41952f5db9ac5
ce <zhuxiujia@qq.com>
2019-10-31 16:45:47 +0800

    add bench

--- src/example/Example_bench.rs
+++ src/example/Example_bench.rs
@@ -9,3 +9,28 @@ use chrono::Local;
 use crate::utils;
 use crate::ast::NodeType::NodeType;
 
+
+#[bench]
+fn testLoadXmlBench(b: &mut Bencher) {
+    let mut holder=NodeConfigHolder::new();
+    let filePath = "./src/example/Example_ActivityMapper.xml";
+    println!(">>>>>>>>>>>>>>>>>>>>>>start load {} >>>>>>>>>>>>>>>>>>>>>>>", filePath);
+    let content = fs::read_to_string(filePath).unwrap();
+    //println!("With text:/n{}", content);
+    println!("start build -------------------------------------------------------");
+    let mut rbatis=Rbatis::new(content);
+    rbatis.print();
+
+
+    let mut arg=json!({
+       "name":"sadf",
+       "startTime":"startTime",
+       "endTime":"endTime",
+       "page":1,
+       "size":1,
+    });
+
+    b.iter(||{
+        let data=rbatis.eval("selectByCondition",&mut arg);
+    })
+}
