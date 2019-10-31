ba2995f2ba3b087e94738e4872083396834330de
ce <zhuxiujia@qq.com>
2019-10-31 11:04:59 +0800

    add test

--- src/core/Rbatis_test.rs
+++ src/core/Rbatis_test.rs
@@ -6,6 +6,7 @@ use serde_json::{json, Value};
 pub fn TestRbatis(){
     let mut john =  json!({
         "name": "John Doe",
+        "startTime":"2019-02-08"
     });
 
     let mut rbatis=Rbatis::new(r#"
@@ -14,14 +15,14 @@ pub fn TestRbatis(){
         <bind name="pattern" value="'%' + name + '%'"/>
         select * from biz_activity
         <where>
-            <if test="name != null">and name like #{pattern}\n</if>
-            <if test="startTime != null">and create_time >= #{startTime}\n</if>
-            <if test="endTime != null">and create_time &lt;= #{endTime}\n</if>
-        \n</where>
+            <if test="name != null">and name like #{pattern}</if>
+            <if test="startTime != null">and create_time >= #{startTime}</if>
+            <if test="endTime != null">and create_time &lt;= #{endTime}</if>
+        </where>
         order by create_time desc
-        <if test="page != null and size != null">limit #{page}, #{size}\n</if>
-    \n</select>
-    \n</mapper>
+        <if test="page != null and size != null">limit #{page}, #{size}</if>
+    </select>
+    </mapper>
     "#.to_string());
     let mut r=rbatis.eval("selectByCondition",&mut john).unwrap();
     println!("{}",r);
