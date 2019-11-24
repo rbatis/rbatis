f5186a2f87a11f112dd02185eee30575d8d7ed0e
zhuxiujia <zhuxiujia@qq.com>
2019-11-24 22:02:36 +0800

    add result map

--- src/templete/Insert.rs
+++ src/templete/Insert.rs
@@ -4,7 +4,7 @@ use serde_json::json;
 pub struct Insert {}
 
 impl Insert {
-    fn do_create_values_sql(&self,arg: &Value)->String{
+    fn do_create_values_sql(&self, arg: &Value) -> String {
         let obj = arg.as_object();
         let obj_map = obj.unwrap();
         let mut values = "".to_string();
@@ -33,7 +33,7 @@ impl Insert {
     }
 
     //todo replace to xml field
-    fn do_create_field_sql(&self,arg: Value)->String{
+    fn do_create_field_sql(&self, arg: Value) -> String {
         let obj = arg.as_object();
         let obj_map = obj.unwrap();
         let mut fields = "".to_string();
@@ -61,7 +61,7 @@ impl Insert {
         return fields;
     }
 
-    fn do_create_obj_sql(&self, mut sql:String, arg: Value) ->String{
+    fn do_create_obj_sql(&self, mut sql: String, arg: Value) -> String {
         let obj = arg.as_object();
         let obj_map = obj.unwrap();
         //todo replace to xml field
@@ -91,57 +91,56 @@ impl Insert {
             i = i + 1;
         }
         sql = sql.replace("#{fields}", fields.as_str());
-        sql = sql.replace("#{values}", ("(".to_string()+values.as_str()+")").as_str());
+        sql = sql.replace("#{values}", ("(".to_string() + values.as_str() + ")").as_str());
         return sql;
     }
 
 
-    pub fn doCreateSql(&self, table: &str, arg: Value) -> Option<String> {
+    pub fn eval(&self, table: &str, arg: Value) -> Option<String> {
         let mut sql = "insert into #{table} (#{fields}) VALUES #{values}".to_string();
         sql = sql.replace("#{table}", table);
 
         if arg.is_null() {
             return Option::None;
         }
-        if arg.is_object(){
-            return Option::Some(self.do_create_obj_sql(sql,arg));
-        }else if arg.is_array(){
-            let mut values="".to_string();
-            let arr=arg.as_array().unwrap();
-            if arr.len()==0{
+        if arg.is_object() {
+            return Option::Some(self.do_create_obj_sql(sql, arg));
+        } else if arg.is_array() {
+            let mut values = "".to_string();
+            let arr = arg.as_array().unwrap();
+            if arr.len() == 0 {
                 return Option::None;
             }
             //todo replace to xml field
-            let fields=self.do_create_field_sql(arr.get(0).unwrap().clone());
+            let fields = self.do_create_field_sql(arr.get(0).unwrap().clone());
             sql = sql.replace("#{fields}", fields.as_str());
 
             for x in arr {
-                let mut value_item_sql=self.do_create_values_sql(x);
-                value_item_sql="(".to_string()+value_item_sql.as_str()+"),";
+                let mut value_item_sql = self.do_create_values_sql(x);
+                value_item_sql = "(".to_string() + value_item_sql.as_str() + "),";
                 values.push_str(value_item_sql.as_str());
             }
             values.pop();
             sql = sql.replace("#{values}", values.as_str());
             return Option::Some(sql);
-        }else{
+        } else {
             return Option::None;
         }
-
     }
 }
 
 #[test]
 fn test_insert_templete_obj() {
-    let t = Insert{};
+    let t = Insert {};
     let arg = serde_json::from_str(r#"{  "a":"1","delete_flag":1}"#).unwrap();
-    let sql = t.doCreateSql("activity", arg).unwrap();
+    let sql = t.eval("activity", arg).unwrap();
     println!("{}", sql);
 }
 
 #[test]
 fn test_insert_templete_array() {
-    let t = Insert{};
+    let t = Insert {};
     let arg = serde_json::from_str(r#"[{"a":"1","delete_flag":1}]"#).unwrap();
-    let sql = t.doCreateSql("activity", arg).unwrap();
+    let sql = t.eval("activity", arg).unwrap();
     println!("{}", sql);
 }
