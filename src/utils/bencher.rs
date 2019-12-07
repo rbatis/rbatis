6a06451b62578682aa05a7380f620e0b01b780dd
zhuxiujia <zhuxiujia@qq.com>
2019-12-08 04:33:47 +0800

    add static

--- src/utils/bencher.rs
+++ src/utils/bencher.rs
@@ -1,40 +1,45 @@
 use crate::utils::time_util::count_time;
 use chrono::{Local, DateTime};
 
-pub struct Bencher{
-     total:i32,
-     now:DateTime<Local>
+pub struct Bencher {
+    total: i32,
+    now: i64,
 }
 
-impl Bencher{
-    pub fn new(total:i32)->Self{
-        return Self{
+impl Bencher {
+    pub fn new(total: i32) -> Self {
+        return Self {
             total,
-            now: Local::now()
-        }
+            now: 0,
+        };
     }
 
-    pub fn iter(&mut self,func:fn()){
-        for i in 0..self.total{
+    pub fn iter(&mut self, func: fn()) {
+        let mut current = 0;
+        self.now = Local::now().timestamp_millis();
+        loop {
             func();
-            if i==self.total-1{
-                let end=Local::now().timestamp_millis();
-                use_time(self.total,self.now,end);
-                use_tps(self.total,self.now,end);
+            if current == self.total - 1 {
+                let end = Local::now().timestamp_millis();
+                use_time(self.total, self.now, end);
+                use_tps(self.total, self.now, end);
+                break;
+            } else {
+                current = current + 1;
             }
         }
     }
 }
 
-fn use_tps(total: i32, start: DateTime<Local>,end:i64) {
-    let mut time = (end - start.timestamp_millis()) as f64;
+fn use_tps(total: i32, start: i64, end: i64) {
+    let mut time = (end - start) as f64;
     time = time / 1000.0;
     println!("use TPS: {} TPS/s", total as f64 / time);
 }
 
 //计算每个操作耗时nano纳秒
-fn use_time(total: i32, start: DateTime<Local>,end:i64) {
-    let mut time = (end - start.timestamp_millis()) as f64;
+fn use_time(total: i32, start: i64, end: i64) {
+    let mut time = (end - start) as f64;
     time = time / 1000.0;
     println!("use Time: {} s,each:{} nano/op", time, time * 1000000000.0 / (total as f64));
 }
