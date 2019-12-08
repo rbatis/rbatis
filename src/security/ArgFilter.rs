6bbe70f396a297c1856fd542487384ae7e80b554
zhuxiujia <zhuxiujia@qq.com>
2019-12-08 18:59:48 +0800

    add main async

--- src/security/ArgFilter.rs
+++ src/security/ArgFilter.rs
@@ -2,7 +2,7 @@ pub struct ArgFilter {}
 
 impl ArgFilter {
 
-    ///  select * from table where name = #{'hello' OR 'A'!=''}
+    ///  select * from table where name = 'hello\\' OR \\'A\\'!=\\''
     ///过滤替换 参数中可能包含的影响sql安全的因素
     pub fn filter(arg: &mut serde_json::Value) {
         if arg.is_object() {
