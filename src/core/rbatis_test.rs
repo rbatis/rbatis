a7e03a25f8b7263ab1bdc398c607dbf6983648e9
zhuxiujia <zhuxiujia@qq.com>
2019-12-08 19:41:27 +0800

    add db config

--- src/core/rbatis_test.rs
+++ src/core/rbatis_test.rs
@@ -1,15 +1,16 @@
 use crate::ast::config_holder::ConfigHolder;
 use crate::core::rbatis::Rbatis;
 use serde_json::{json, Value};
+use crate::core::db_config::DBConfig;
 
 #[test]
-pub fn test_rbatis(){
-    let mut john =  json!({
+pub fn test_rbatis() {
+    let mut john = json!({
         "name": "John Doe",
         "startTime":"2019-02-08"
     });
 
-    let mut rbatis=Rbatis::new(r#"
+    let mut rbatis = Rbatis::new(r#"
     <mapper>
     <select id="selectByCondition" resultMap="BaseResultMap">
         <bind name="pattern" value="'%' + name + '%'"/>
@@ -23,7 +24,16 @@ pub fn test_rbatis(){
         <if test="page != null and size != null">limit #{page}, #{size}</if>
     </select>
     </mapper>
-    "#.to_string());
-    let mut r=rbatis.eval("selectByCondition",&mut john).unwrap();
-    println!("{}",r);
+    "#.to_string(),"mysql://postgres:1233456@localhost:5432/test".to_string());
+    let mut r = rbatis.eval("selectByCondition", &mut john).unwrap();
+    println!("{}", r);
+}
+
+#[test]
+pub fn test_parser_url() {
+    let link = "mysql://postgres:1233456@localhost:5432/test";
+
+    let db_config=DBConfig::new(link.to_string());
+
+    println!("{:?}",db_config);
 }
