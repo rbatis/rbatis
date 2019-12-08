f42e78ab5bc399d6bb883c8220b8e4ec524b2d5f
zhuxiujia <zhuxiujia@qq.com>
2019-12-08 18:28:54 +0800

    add main async

---
+++ src/security/ArgFilter.rs
@@ -0,0 +1,16 @@
+pub struct ArgFilter {}
+
+impl ArgFilter {
+    ///过滤替换 参数中可能包含的影响sql安全的因素
+    pub fn filter(arg: &mut serde_json::Value) {
+        if arg.is_object() {
+            let mut m = arg.as_object_mut().unwrap();
+            for (k, v) in m {
+                if v.is_string() {
+                    let s = v.as_str().unwrap().replace(";", "\\;");
+                    *v = serde_json::Value::String(s);
+                }
+            }
+        }
+    }
+}
