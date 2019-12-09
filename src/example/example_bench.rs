05de07094faaefb650427d6bfeb43391dfb23001
zhuxiujia <zhuxiujia@qq.com>
2019-12-10 01:16:03 +0800

    add number decode

--- src/example/example_bench.rs
+++ src/example/example_bench.rs
@@ -8,29 +8,3 @@ use crate::ast::config_holder::ConfigHolder;
 use chrono::Local;
 use crate::utils;
 use crate::ast::node_type::NodeType;
-
-//
-//#[bench]
-//fn testLoadXmlBench(b: &mut Bencher) {
-//    let mut holder=NodeConfigHolder::new();
-//    let filePath = "./src/example/Example_ActivityMapper.xml";
-//    println!(">>>>>>>>>>>>>>>>>>>>>>start load {} >>>>>>>>>>>>>>>>>>>>>>>", filePath);
-//    let content = fs::read_to_string(filePath).unwrap();
-//    //println!("With text:/n{}", content);
-//    println!("start build -------------------------------------------------------");
-//    let mut rbatis=Rbatis::new(content);
-//    rbatis.print();
-//
-//
-//    let mut arg=json!({
-//       "name":"sadf",
-//       "startTime":"startTime",
-//       "endTime":"endTime",
-//       "page":1,
-//       "size":1,
-//    });
-//
-//    b.iter(||{
-//        let data=rbatis.eval("selectByCondition",&mut arg);
-//    })
-//}
