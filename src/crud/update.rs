62d882f66dbadff57142c7b02fecdbf94c46ea1b
ce <zhuxiujia@qq.com>
2019-12-12 11:26:31 +0800

    add method

--- src/crud/update.rs
+++ src/crud/update.rs
@@ -3,7 +3,7 @@ use serde_json::Value;
 pub struct Update {}
 
 impl Update {
-    pub fn eval(&self, table: &str, arg: Value) -> Result<String, String> {
+    pub fn eval<T>(&self, table: &str, arg: Value) -> Result<T, String> {
         unimplemented!()
         //update by id
 
