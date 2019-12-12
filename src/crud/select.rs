cd4df112910ac8245e9dcfe6fec839741fcd4fa5
ce <zhuxiujia@qq.com>
2019-12-12 11:06:35 +0800

    add method

--- src/crud/select.rs
+++ src/crud/select.rs
@@ -1,11 +1,16 @@
 use serde_json::Value;
 
-pub struct Select{
+pub struct Select {}
 
-}
-
-impl Select{
-    pub  fn eval(&self, table: &str, arg: Value) -> Result<String,String>{
+impl Select {
+    pub fn eval(&self, table: &str, arg: Value) -> Result<String, String> {
         unimplemented!()
+        //select by id
+
+        //select by id vec
+
+        //select by map
+
+        //select by page
     }
 }
--- src/crud/update.rs
+++ src/crud/update.rs
@@ -1,11 +1,14 @@
 use serde_json::Value;
 
-pub struct Update{
+pub struct Update {}
 
-}
-
-impl Update{
-    pub  fn eval(&self, table: &str, arg: Value) -> Result<String,String>{
+impl Update {
+    pub fn eval(&self, table: &str, arg: Value) -> Result<String, String> {
         unimplemented!()
+        //update by id
+
+        //update by id vec
+
+        //update by map
     }
 }
