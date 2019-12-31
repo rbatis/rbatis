5ea8a2b8338b85036168756b51bd80ce9750f390
ce <zhuxiujia@qq.com>
2019-12-31 16:14:48 +0800

    add todo

--- src/crud/update.rs
+++ src/crud/update.rs
@@ -4,12 +4,10 @@ use crate::core::rbatis::Rbatis;
 pub struct Update {}
 
 impl Update {
-    pub fn eval(&self, table: &str, arg: Value, engine: &Rbatis) -> Result<String, String> {
+    pub fn update(&self, table: &str, arg: Value, engine: &Rbatis) -> Result<String, String> {
         unimplemented!()
         //TODO update by id
-
-        //TODO update by id vec
-
         //TODO update by map
+        //TODO update by ids
     }
 }
