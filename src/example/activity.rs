9f57757844965082f8825c135bb23633c4b5f1b2
ce <zhuxiujia@qq.com>
2019-12-31 13:21:39 +0800

    移除macro

--- src/example/activity.rs
+++ src/example/activity.rs
@@ -4,7 +4,7 @@ use serde::{Serialize, Deserialize};
 /**
 * 数据库表模型
 */
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Activity {
     pub id: Option<String>,
     pub name: Option<String>,
