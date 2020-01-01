0b44103855e1a87a1515488128f7666058235194
zhuxiujia <zhuxiujia@qq.com>
2020-01-01 22:18:20 +0800

    add ast

--- src/ast/md/md_tree.rs
+++ src/ast/md/md_tree.rs
@@ -1,10 +1,17 @@
+use crate::ast::ast::Ast;
+use serde_json::Value;
+use crate::ast::config_holder::ConfigHolder;
 
 pub struct MDTree{
   //TODO bind,choose,delete,foreach if,include insert otherwise,result_map,select,set,string,trim,update,when,where
 
 }
 
-impl MDTree{
+impl Ast for MDTree{
+
+    fn eval(&self, env: &mut Value, holder: &mut ConfigHolder) -> Result<String, String> {
+        unimplemented!()
+    }
 
 }
 
