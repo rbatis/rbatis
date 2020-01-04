8b66a7ebc6b97933a4dadfc124d3df0c4edeb3ad
zhuxiujia <zhuxiujia@qq.com>
2020-01-05 01:55:30 +0800

    add delete.rs

--- src/crud/insert.rs
+++ src/crud/insert.rs
@@ -1,8 +1,8 @@
-use serde_json::Value;
 use serde_json::json;
-use crate::core::rbatis::Rbatis;
-use crate::convert::sql_value_convert::SqlValueConvert;
+use serde_json::Value;
 
+use crate::convert::sql_value_convert::{SqlColumnConvert, SqlValueConvert};
+use crate::core::rbatis::Rbatis;
 
 impl Rbatis {
     /// insert an object or  array
@@ -26,22 +26,24 @@ impl Rbatis {
             if arr.len() == 0 {
                 return Result::Err("[rbatis] arg array len = 0!".to_string());
             }
-            //todo replace to xml field
             if !arr.get(0).unwrap().is_object() {
                 return Result::Err("[rbatis] unsupport arg type,only support object json and object array!".to_string());
             }
-            let fields = do_create_field_sql(arr.get(0).unwrap().clone());
+            let fields = arr.get(0).unwrap().clone().to_sql_column();
             sql = sql.replace("#{fields}", fields.as_str());
 
+            let mut append = false;
             for x in arr {
                 if !x.is_object() {
                     return Result::Err("[rbatis] unsupport arg type,only support object json and object array!".to_string());
                 }
                 let mut value_item_sql = do_create_values_sql(x);
-                value_item_sql = "(".to_string() + value_item_sql.as_str() + "),";
-                values.push_str(value_item_sql.as_str());
+                values = values + value_item_sql.as_str() + ",";
+                append = true;
             }
-            values.pop();
+            if append {
+                values.pop();
+            }
             sql = sql.replace("#{values}", values.as_str());
             return Result::Ok(sql);
         } else {
@@ -54,67 +56,31 @@ impl Rbatis {
 fn do_create_values_sql(arg: &Value) -> String {
     let obj = arg.as_object();
     let obj_map = obj.unwrap();
-    let mut values = "".to_string();
-    let len = obj_map.len();
-    let mut i = 0;
+    let mut values = vec![];
     for (_, v) in obj_map {
-        let vstr=v.to_sql_value();
-        if i < (len - 1) {
-            values = values + vstr.as_str() + ",";
-        } else {
-            values = values + vstr.as_str();
-        }
-        i = i + 1;
+        values.push(v.clone());
     }
-    return values;
+    return serde_json::Value::Array(values).to_sql_value_custom(false);
 }
 
-//todo replace to xml field
-fn do_create_field_sql(arg: Value) -> String {
-    let obj = arg.as_object();
-    let obj_map = obj.unwrap();
-    let mut fields = "".to_string();
-    let len = obj_map.len();
-    let mut i = 0;
-    for (x, _) in obj_map {
-        if i < (len - 1) {
-            fields = fields + x.as_str() + ",";
-        } else {
-            fields = fields + x.as_str();
-        }
-        i = i + 1;
-    }
-    return fields;
-}
 
 fn do_create_obj_sql(mut sql: String, arg: Value) -> String {
     let obj = arg.as_object();
     let obj_map = obj.unwrap();
-    //todo replace to xml field
-    let mut fields = "".to_string();
-    let mut values = "".to_string();
-    let len = obj_map.len();
-    let mut i = 0;
-    for (x, v) in obj_map {
-        let vstr = v.to_sql_value();
-        if i < (len - 1) {
-            fields = fields + x.as_str() + ",";
-            values = values + vstr.as_str() + ",";
-        } else {
-            fields = fields + x.as_str();
-            values = values + vstr.as_str();
-        }
-        i = i + 1;
+    let mut values = vec![];
+    for (x, item) in obj_map {
+        values.push(item.clone());
     }
-    sql = sql.replace("#{fields}", fields.as_str());
-    sql = sql.replace("#{values}", ("(".to_string() + values.as_str() + ")").as_str());
+    let vals = serde_json::Value::Array(values);
+    sql = sql.replace("#{fields}", arg.to_sql_column().as_str());
+    sql = sql.replace("#{values}", vals.to_sql_value_custom(false).as_str());
     return sql;
 }
 
 
 #[test]
 fn test_insert_templete_obj() {
-    let rbatis=Rbatis::new();
+    let rbatis = Rbatis::new();
     let arg = serde_json::from_str(r#"{  "a":"1","delete_flag":1}"#).unwrap();
     let sql = rbatis.insert("activity", arg).unwrap();
     println!("{}", sql);
@@ -122,7 +88,7 @@ fn test_insert_templete_obj() {
 
 #[test]
 fn test_insert_templete_array() {
-    let rbatis=Rbatis::new();
+    let rbatis = Rbatis::new();
     let arg = serde_json::from_str(r#"[{"a":"1","delete_flag":1},{"a":"1","delete_flag":1}]"#).unwrap();
     let sql = rbatis.insert("activity", arg).unwrap();
     println!("{}", sql);
