85934d4f6d6eb8592f73fff441e8dfe5ac5a468f
zhuxiujia <zhuxiujia@qq.com>
2020-01-06 00:25:52 +0800

    add page.rs

---
+++ src/crud/page.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Page<T: Serialize + Clone> {
+    pub total: i64,
+    pub size: i64,
+    pub current: i64,
+    pub records: Option<Vec<T>>,
+}
+
+impl <T> Page<T>where T:Serialize + Clone {
+    pub fn new(current:i64,size:i64)-> Self{
+        return Page::new_total(current, size, 0);
+    }
+    pub fn new_total(current:i64,size:i64,total:i64)-> Self{
+        return Self{
+            total: total,
+            size,
+            current,
+            records: None
+        }
+    }
+    pub fn set_records(&mut self,data:Vec<T>){
+        self.records=Some(data);
+    }
+}
