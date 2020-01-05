5c7812804c2cd37d41fe9507bf50fbe866288c8d
zhuxiujia <zhuxiujia@qq.com>
2020-01-05 23:58:44 +0800

    add update.rs

--- src/example/activity.rs
+++ src/example/activity.rs
@@ -9,5 +9,10 @@ use std::path::Display;
 pub struct Activity {
     pub id: Option<String>,
     pub name: Option<String>,
+    pub pc_link:Option<String>,
+    pub h5_link:Option<String>,
+    pub remark:Option<String>,
+    pub create_time:Option<String>,
     pub version: Option<i32>,
+    pub delete_flag: Option<i32>,
 }
