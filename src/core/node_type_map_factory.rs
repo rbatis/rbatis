8bdbccd7f5cfa32ba188b8f5a56c39e1aef58ce3
ce <zhuxiujia@qq.com>
2020-01-06 17:02:24 +0800

    add loader TODO

--- src/core/node_type_map_factory.rs
+++ src/core/node_type_map_factory.rs
@@ -8,18 +8,23 @@ pub fn create_node_type_map(xml_content: String,holder:&ConfigHolder) -> HashMap
     let nodes = load_xml(xml_content);
     let data = loop_decode_xml(&nodes, holder);
     let mut m = HashMap::new();
-    for x in data {
-        match x.clone() {
-
-            NodeType::NResultMapNode(node) => m.insert(node.id, x),
-
-            NodeType::NSelectNode(node) => m.insert(node.id, x),//TODO where  insert del
-            NodeType::NDeleteNode(node) => m.insert(node.id, x), //TODO where  insert del
-            NodeType::NUpdateNode(node) => m.insert(node.id, x), //TODO sets insert version,del,where insert del
-            NodeType::NInsertNode(node) => m.insert(node.id, x),
-
+    for x in &data {
+        match x{
+            NodeType::NResultMapNode(node) => m.insert(node.id.clone(), x.clone()),
+            _ => {
+                continue;
+            }
+        };
+    }
+    for x in &data {
+        match x{
+            NodeType::NSelectNode(node) => m.insert(node.id.clone(), x.clone()),//TODO where  insert del
+            NodeType::NDeleteNode(node) => m.insert(node.id.clone(), x.clone()), //TODO where  insert del
+            NodeType::NUpdateNode(node) => m.insert(node.id.clone(), x.clone()), //TODO sets insert version,del,where insert del
+            NodeType::NInsertNode(node) => m.insert(node.id.clone(), x.clone()),
             _ => m.insert("unknow".to_string(), NodeType::Null),
         };
+
     }
     return m;
 }
