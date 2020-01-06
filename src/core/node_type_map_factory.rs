898edb3951b0ae8b55ad59bcd5f6263398f41093
ce <zhuxiujia@qq.com>
2020-01-06 16:58:06 +0800

    add loader TODO

--- src/core/node_type_map_factory.rs
+++ src/core/node_type_map_factory.rs
@@ -13,9 +13,9 @@ pub fn create_node_type_map(xml_content: String,holder:&ConfigHolder) -> HashMap
 
             NodeType::NResultMapNode(node) => m.insert(node.id, x),
 
-            NodeType::NSelectNode(node) => m.insert(node.id, x),
-            NodeType::NDeleteNode(node) => m.insert(node.id, x),
-            NodeType::NUpdateNode(node) => m.insert(node.id, x),
+            NodeType::NSelectNode(node) => m.insert(node.id, x),//TODO where  insert del
+            NodeType::NDeleteNode(node) => m.insert(node.id, x), //TODO where  insert del
+            NodeType::NUpdateNode(node) => m.insert(node.id, x), //TODO sets insert version,del,where insert del
             NodeType::NInsertNode(node) => m.insert(node.id, x),
 
             _ => m.insert("unknow".to_string(), NodeType::Null),
