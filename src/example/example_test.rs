b8550374d4f04c4d9dcda504e890a42b4b3784e4
ce <zhuxiujia@qq.com>
2020-01-06 16:36:37 +0800

    remove result map config

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -52,6 +52,17 @@ fn test_exec_sql(){
     println!("[rbatis] result==>  {:?}", data);
 }
 
+
+/**
+
+分页查询数据，测试
+
+2020-01-06T16:35:15.969770+08:00 INFO rbatis::core::rbatis - [rbatis] Query ==>  select * from biz_activity where name = '新人专享' AND delete_flag = 1 LIMIT 1,5
+2020-01-06T16:35:16.091525+08:00 INFO rbatis::core::rbatis - [rbatis] ReturnRows <== 1
+2020-01-06T16:35:16.091848+08:00 INFO rbatis::core::rbatis - [rbatis] Query ==>  select count(1) from biz_activity where name = '新人专享' AND delete_flag = 1
+2020-01-06T16:35:16.118317+08:00 INFO rbatis::core::rbatis - [rbatis] ReturnRows <== 1
+[rbatis] result==>  IPage { total: 2, size: 5, current: 1, records: Some([Activity { id: Some("\"dfbdd779-5f70-4b8f-9921-c235a9c75b69\""), name: Some("\"新人专享\""), pc_link: Some("\"http://115.220.9.139:8002/newuser/\""), h5_link: Some("\"http://115.220.9.139:8002/newuser/\""), remark: Some("\"\""), create_time: Some("\"2019-05-27 10:25:41\""), version: Some(6), delete_flag: Some(1) }]) }
+*/
 #[test]
 fn test_exec_select(){
     //1 启用日志(可选，不添加则不加载日志库)
@@ -59,7 +70,7 @@ fn test_exec_select(){
     //2 初始化rbatis
     let mut rbatis = Rbatis::new();
     //3 加载数据库url name 为空，则默认数据库
-    rbatis.load_db_url("".to_string(), "mysql://root:TEST@localhost:3306/test");
+    rbatis.load_db_url("".to_string(), "mysql://root:TEST@115.220.9.139:3306/test");
     //4 加载xml配置
     rbatis.load_xml("Example_ActivityMapper.xml".to_string(), fs::read_to_string("./src/example/Example_ActivityMapper.xml").unwrap());//加载xml数据
     //判断是否配置数据库
