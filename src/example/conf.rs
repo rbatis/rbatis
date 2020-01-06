c076f7ee66ad77e4ebdc2e081d697c689cc69130
zhuxiujia <zhuxiujia@qq.com>
2020-01-06 22:31:25 +0800

    add ipage.rs

---
+++ src/example/conf.rs
@@ -0,0 +1,4 @@
+
+
+//示例 mysql 链接地址
+pub const MYSQL_URL: &'static str = "mysql://root:TEST@localhost:3306/test";
--- src/example/mod.rs
+++ src/example/mod.rs
@@ -1,3 +1,4 @@
 mod example_test;
 mod example_bench;
 pub mod activity;
+pub mod conf;
