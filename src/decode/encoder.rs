9f57d66d6486aca58945f5b6c650efbedbbcba03
ce <zhuxiujia@qq.com>
2020-01-09 10:44:35 +0800

    fix

--- src/decode/encoder.rs
+++ src/decode/encoder.rs
@@ -1,49 +1,71 @@
-
 use rdbc::{DataType, ResultSetMetaData, Error, ResultSet};
 use serde_json::{Value, Number};
 use rdbc_mysql::MySQLResultSet;
 use serde_json::de::ParserNumber;
 
 
-pub fn encode_to_value(arg:&mut dyn ResultSet)->Vec<Value>{
-    let mut arr=vec![];
+pub fn encode_to_value(arg: &mut dyn ResultSet) -> Vec<Value> {
+    let mut arr = vec![];
     while arg.next() {
-        let meta_data =arg.meta_data().unwrap();
-        let mut m=serde_json::Map::new();
-        for c_index in 0..meta_data.num_columns(){
-            let c_name=meta_data.column_name(c_index);
-            let c_type=meta_data.column_type(c_index);
+        let meta_data = arg.meta_data().unwrap();
+        let mut m = serde_json::Map::new();
+        for c_index in 0..meta_data.num_columns() {
+            let c_name = meta_data.column_name(c_index);
+            let c_type = meta_data.column_type(c_index);
             match c_type {
-                DataType::Utf8 |DataType::Date| DataType::Time |  DataType::Datetime=>{
-                    let strings=arg.get_string(c_index);
+                DataType::Char | DataType::Utf8 | DataType::Date | DataType::Time | DataType::Datetime => {
+                    let strings = arg.get_string(c_index);
                     if strings.is_ok() {
-                        let v=strings.unwrap();
-                        if v.is_some(){
-                            m.insert(c_name,serde_json::Value::String(v.unwrap()));
-                        }else{
-                            m.insert(c_name,serde_json::Value::Null);
+                        let v = strings.unwrap();
+                        if v.is_some() {
+                            m.insert(c_name, serde_json::Value::String(v.unwrap()));
+                        } else {
+                            m.insert(c_name, serde_json::Value::Null);
                         }
                     }
                 }
-                DataType::Integer=>{
-                    let strings=arg.get_i64(c_index);
+                DataType::Integer => {
+                    let strings = arg.get_i64(c_index);
                     if strings.is_ok() {
-                        let v=strings.unwrap();
-                        if v.is_some(){
-                            m.insert(c_name,serde_json::Value::Number(Number::from(ParserNumber::I64(v.unwrap()))));
-                        }else{
-                            m.insert(c_name,serde_json::Value::Null);
+                        let v = strings.unwrap();
+                        if v.is_some() {
+                            m.insert(c_name, serde_json::Value::Number(Number::from(ParserNumber::I64(v.unwrap()))));
+                        } else {
+                            m.insert(c_name, serde_json::Value::Null);
                         }
                     }
                 }
-                DataType::Float | DataType::Double | DataType::Decimal=>{
-                    let strings=arg.get_f64(c_index);
+                DataType::Float | DataType::Double | DataType::Decimal => {
+                    let strings = arg.get_f64(c_index);
                     if strings.is_ok() {
-                        let v=strings.unwrap();
-                        if v.is_some(){
-                            m.insert(c_name,serde_json::Value::Number(Number::from_f64(v.unwrap()).unwrap()));
-                        }else{
-                            m.insert(c_name,serde_json::Value::Null);
+                        let v = strings.unwrap();
+                        if v.is_some() {
+                            m.insert(c_name, serde_json::Value::Number(Number::from_f64(v.unwrap()).unwrap()));
+                        } else {
+                            m.insert(c_name, serde_json::Value::Null);
+                        }
+                    }
+                }
+                DataType::Bool => {
+                    let byte_opt = arg.get_bytes(c_index);
+                    if byte_opt.is_err() {
+                        m.insert(c_name, serde_json::Value::Null);
+                    } else {
+                        let data = byte_opt.unwrap();
+                        if data.is_none() {
+                            m.insert(c_name, serde_json::Value::Null);
+                        } else {
+                            let data_bool_vec = data.unwrap();
+                            if data_bool_vec.is_empty() {
+                                m.insert(c_name, serde_json::Value::Null);
+                            } else {
+                                let data_bool = *data_bool_vec.get(0).unwrap();
+                                if data_bool == 1 {
+                                    m.insert(c_name, serde_json::Value::Bool(true));
+                                } else {
+                                    m.insert(c_name, serde_json::Value::Bool(false));
+                                }
+                            }
                         }
                     }
                 }
