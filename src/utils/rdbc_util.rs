479e6db79578d3a71a5d45beb058fb14752f0382
ce <zhuxiujia@qq.com>
2020-01-10 15:05:05 +0800

    add driver

--- src/utils/rdbc_util.rs
+++ src/utils/rdbc_util.rs
@@ -1,24 +1,26 @@
 use serde_json::Value;
 use crate::convert::sql_value_convert::SqlValueConvert;
 
-pub fn to_rdbc_values(arg_array:&mut Vec<Value>)->Vec<rdbc::Value>{
-    let mut params =vec![];
+pub fn to_rdbc_values(arg_array: &mut Vec<Value>) -> Vec<rdbc::Value> {
+    let mut params = vec![];
     for x in arg_array {
-        let item=x.to_sql_value_def();
+        let item = x.to_sql_value_def();
         params.push(rdbc::Value::String(item));
     }
     return params;
 }
 
-pub fn rdbc_vec_to_string(arg:&Vec<rdbc::Value>) ->String{
+pub fn rdbc_vec_to_string(arg: &Vec<rdbc::Value>) -> String {
     let mut s = String::new();
     for x in arg {
         s = s + x.to_string().as_str() + ",";
     }
     if s.len() > 0 {
         s.pop();
+        s = "[".to_string() + s.as_str();
+        s = s + "]";
+        return s;
+    } else {
+        return "[]".to_string();
     }
-    s = "{".to_string()+s.as_str();
-    s = s + "}";
-    return s;
 }
