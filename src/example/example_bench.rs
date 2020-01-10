cea23da6d53896da244a060262abe808a21bfb64
zhuxiujia <zhuxiujia@qq.com>
2020-01-10 20:46:26 +0800

    add lib

--- src/example/example_bench.rs
+++ src/example/example_bench.rs
@@ -10,19 +10,47 @@ use crate::utils;
 use crate::ast::xml::node_type::NodeType;
 use crate::utils::bencher::Bencher;
 
+//lazy_static! {
+//    static ref ARRAY: Mutex<Vec<u8>> = Mutex::new(vec![]);
+//}
 
-///fn test_benchmark_tps()
-///use Time: 0.042 s,each:420 nano/op
-///use TPS: 2380952.3809523806 TPS/s
-///
+//#[async_std::main]
+//async fn main() {
+//    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+//    info!("=====================================================================================");
+//    error!("================================= [rbatis] now is started============================");
+//    warn!("=====================================================================================");
+////    ARRAY.lock().unwrap().push(1);
+////    println!("{:?}",ARRAY.lock().unwrap().get(0).unwrap());
+//    let id = task::current().id();
+//    println!("{:?}", id);
+//    let task = task::spawn(async {
+//        let id = task::current().id();
+//        println!("{:?}", id);
+//        task::sleep(Duration::from_millis(1000)).await;
+//    });
+//    println!("waiting for the task");
+//    let res = task.await;
+//    println!("task ended with result {:?}", res);
+//}
 
+
+#[test]
+fn bench_main() {
+    let mut b =Bencher::new(1000000);
+    b.iter( || {
+        //println!("asdf");
+        let js:serde_json::Value=serde_json::from_str(r#"{"id":"","name":"","version":0}"#).unwrap();
+//        Uuid::new_v4();
+    });
+}
 
 
 ///use Time: 0.202 s,each:2020 nano/op
 ///use TPS: 495049.50495049503 TPS/s
 //cargo test --release --color=always --package rbatis --bin rbatis example::example_bench::bench_main --all-features -- --nocapture --exact
 #[test]
-fn bench_main() {
+fn bench_example() {
     let mut rbatis=Rbatis::new();
     rbatis.set_enable_log(false);//禁用日志以准确获取性能数据
     rbatis.load_xml("Example_ActivityMapper.xml".to_string(),fs::read_to_string("./src/example/Example_ActivityMapper.xml").unwrap());//加载xml数据
