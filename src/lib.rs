fe5d473c1c72a047ff27b7ea6066984a4380305c
ce <zhuxiujia@qq.com>
2020-01-14 11:53:03 +0800

    add driver

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@
 #![allow(unused_must_use)]
 #[macro_use]
 extern crate lazy_static;
-extern crate serde_yaml;
 extern crate rdbc;
 extern crate rdbc_mysql;
 extern crate rdbc_postgres;
+extern crate serde_yaml;
 
 pub mod example;
 pub mod ast;
@@ -27,3 +27,4 @@ pub mod session;
 pub mod local_session;
 pub mod conn_factory;
 pub mod queryable;
+pub mod session_factory;
