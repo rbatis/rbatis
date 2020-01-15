c4fdd976c94a8303a932ea88526371270ac34a25
zhuxiujia <zhuxiujia@qq.com>
2020-01-15 21:33:39 +0800

    add tx, bind life time

--- src/crud/update.rs
+++ src/crud/update.rs
@@ -12,7 +12,7 @@ use crate::utils::string_util::count_string_num;
 
 pub const SKIP_SETS: &'static str = "null,object,array";
 
-impl <'a> Rbatis<'a>  {
+impl Rbatis {
 
     pub fn update<T>(&mut self, mapper_name: &str,  arg: &mut Value) -> Result<T, String> where T: DeserializeOwned {
         let mut arg_array=vec![];
