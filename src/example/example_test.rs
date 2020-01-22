ef5e4d51d82f405f8af2a8ab3da3eb2a940e7c90
zhuxiujia <zhuxiujia@qq.com>
2020-01-22 18:46:24 +0800

    add web test

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -307,7 +307,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(c.clone())
             .route("/", web::get().to(index))
     })
-        .bind("127.0.0.1:8088")?
+        .bind("127.0.0.1:8000")?
         .run()
         .await
 }
