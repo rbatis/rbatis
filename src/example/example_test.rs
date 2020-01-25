e15260d4e12024adf63e1ac8f326e310f12f468d
ce <zhuxiujia@qq.com>
2020-01-26 02:34:58 +0800

    fix

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -317,52 +317,6 @@ pub fn test_web() {
 }
 
 
-
-
-///代理实现服务内容
-macro_rules! impl_service {
-   ($($p:expr,  $fn: ident (&self $(,$x:ident:$t:ty)*         ) -> Result<$return_type:ty,String> ),*) => {
-    $(
-    fn $fn(&self  $(,$x:$t)*    ) -> Result<$return_type,String> {
-           //TODO 判断是否启用事务，启用则根据事务最后一条传播行为创建。
-           if $p!=Propagation::None{
-              singleton().begin( "", $p)?;
-           }
-           let data = (self.$fn)(self  $(,$x)*    );
-           if $p!=Propagation::None{
-              if data.is_ok(){
-                singleton().commit("")?;
-              }else{
-                singleton().rollback("")?;
-              }
-           }
-           return data;
-        }
-    )*
-   }
-}
-macro_rules! impl_service_mut {
-   ($($p:expr,  $fn: ident (&mut self $(,$x:ident:$t:ty)*         ) -> Result<$return_type:ty,String> ),*) => {
-    $(
-    fn $fn(&mut self  $(,$x:$t)*    ) -> Result<$return_type,String> {
-            //TODO 判断是否启用事务，启用则根据事务最后一条传播行为创建。
-            if $p!=Propagation::None{
-              singleton().begin( "", $p)?;
-           }
-            let data = (self.$fn)(self  $(,$x)*    );
-            if $p!=Propagation::None{
-              if data.is_ok(){
-                singleton().commit("")?;
-              }else{
-                singleton().rollback("")?;
-              }
-           }
-            return data;
-        }
-    )*
-   }
-}
-
 pub trait Service {
     fn select_activity(&self) -> Result<Activity, String>;
     fn update_activity(&mut self) -> Result<String, String>;
