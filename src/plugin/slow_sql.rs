cc5da9cde4136a40560701013788ee633d641322
zhuxiujia <zhuxiujia@qq.com>
2020-02-04 15:28:51 +0800

    add slow_sql.rs plugin

--- src/plugin/slow_sql.rs
+++ src/plugin/slow_sql.rs
@@ -4,20 +4,19 @@ use chrono::{DateTime, Utc};
 use std::collections::LinkedList;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct Log {
+pub struct SqlLog {
     pub sql: String,
     pub return_data: String,
     pub second: i64,
 }
 
-pub fn find_slow_sql(log_path: &str) {
+pub fn find_slow_sql(log_path: &str) -> LinkedList<SqlLog>{
     let data_result = fs::read_to_string(log_path);
+    let mut use_time_vec = LinkedList::new();
     if data_result.is_err(){
-        return;
+        return use_time_vec;
     }
     let data = data_result.unwrap();
-
-    let mut use_time_vec = LinkedList::new();
     let mut temp = 0;
     let mut time_start = None;
     let mut sql = "";
@@ -33,7 +32,7 @@ pub fn find_slow_sql(log_path: &str) {
 
             let use_time = date_time_end.timestamp() - date_time.timestamp();
             if use_time > temp {
-                use_time_vec.push_front(Log {
+                use_time_vec.push_front(SqlLog {
                     sql: sql.to_string(),
                     return_data: x.to_string(),
                     second: use_time,
@@ -45,17 +44,11 @@ pub fn find_slow_sql(log_path: &str) {
             sql = "";
         }
     }
-    println!("top use time: {:?}", use_time_vec);
+    return use_time_vec;
 }
 
 #[test]
 pub fn test_find_slow_sql() {
-    //let date_time = "2020-01-10T21:03:47.617953100+08:00".parse::<DateTime<Utc>>().unwrap();
-
-    // println!("{}", date_time.timestamp());
-    //let date_time2 = "2020-01-10T21:04:26.669343400+08:00".parse::<DateTime<Utc>>().unwrap();
-
-    //println!("{}", date_time2.timestamp() - date_time.timestamp());
-
-    find_slow_sql("D:/RustProject/rbatis/requests.log");
+    let  use_time_vec= find_slow_sql("D:/RustProject/rbatis/requests.log");
+    println!("top use time: {:?}", use_time_vec);
 }
