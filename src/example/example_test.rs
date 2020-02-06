490c75a4625e6e705cd41f27301f3200064b4f35
zhuxiujia <zhuxiujia@qq.com>
2020-02-07 02:13:49 +0800

    add tokio support

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -358,8 +358,8 @@ async fn index() -> impl Responder {
     //写法1
     //singleton().raw_sql("","select * from biz_activity where id  = '2';").unwrap();
     //写法2
-    let data:Result<Activity,RbatisError>=to_tokio_await!({
-        let data: Result<Activity,RbatisError> = singleton().raw_sql(format!("{:?}",std::thread::current().id()).as_str(),"select * from biz_activity where id  = '2';");
+    let data=to_tokio_await!(Activity,{
+        let data = singleton().raw_sql(format!("{:?}",std::thread::current().id()).as_str(),"select * from biz_activity where id  = '2';");
         println!("{:?}", data);
         return data;
     });
