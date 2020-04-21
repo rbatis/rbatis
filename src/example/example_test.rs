f2feddd536540787019d6f573863ce9f363a3c7e
zhuxiujia <zhuxiujia@qq.com>
2020-04-21 15:12:55 +0800

    add driver test

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -439,11 +439,11 @@ use sqlx_core::row::Row;
 pub async fn test_sqlx() {
     let pool = MySqlPool::new(MYSQL_URL).await.unwrap();
     let mut conn = pool.acquire().await.unwrap();
-    let mut c = conn.fetch("SELECT count(1) FROM user;");
+    let mut c = conn.fetch("SELECT count(1) as total FROM biz_activity;");
     while let Some(row) = c.next().await.unwrap() {
         let row: MySqlRow = row;
         println!("{:?}", row);
-        let counts: i32 = row.try_get("count(1)").unwrap();
+        let counts: i32 = row.try_get("total").unwrap();
         println!("{:?}", counts);
     }
     println!("done");
