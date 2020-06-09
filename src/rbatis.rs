a80cb441379760d5f67f4a2f9050ee5c26e822d6
zhuxiujia <zhuxiujia@qq.com>
2020-06-09 11:17:39 +0800

    add py load

--- src/rbatis.rs
+++ src/rbatis.rs
@@ -3,16 +3,16 @@ use crate::error::RbatisError;
 use std::collections::HashMap;
 use crate::ast::node::node_type::NodeType;
 
-pub struct Rbatis<'a> {
-    node_type: HashMap<&'a str, Vec<NodeType>>
+pub struct Rbatis<'r> {
+    node_type: HashMap<&'r str, Vec<NodeType>>
 }
 
 
-impl<'a> Rbatis<'a> {
-    pub fn new() -> Rbatis<'a> {
+impl<'r> Rbatis<'r> {
+    pub fn new() -> Rbatis<'r> {
         return Rbatis { node_type: HashMap::new() };
     }
-    pub fn load_py(&mut self, name: &'a str, data: &str) -> Result<(), RbatisError> {
+    pub fn load_py(&mut self, name: &'r str, data: &str) -> Result<(), RbatisError> {
         let data = Py::parser(data)?;
         self.node_type.insert(name, data);
         return Ok(());
