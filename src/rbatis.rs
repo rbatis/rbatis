2564e90f6e06415fa4377bba254a1772da17b264
zhuxiujia <zhuxiujia@qq.com>
2020-06-09 11:52:43 +0800

    add xml load

--- src/rbatis.rs
+++ src/rbatis.rs
@@ -2,19 +2,28 @@ use crate::ast::lang::py::Py;
 use crate::error::RbatisError;
 use std::collections::HashMap;
 use crate::ast::node::node_type::NodeType;
+use crate::engine::runtime::RbatisEngine;
+use crate::ast::lang::xml::Xml;
+use crate::ast::node::insert_node::InsertNode;
+use crate::ast::node::delete_node::DeleteNode;
+use crate::ast::node::update_node::UpdateNode;
+use crate::ast::node::select_node::SelectNode;
 
 pub struct Rbatis<'r> {
-    node_type: HashMap<&'r str, Vec<NodeType>>
+    engine: RbatisEngine,
+    /// map<mapper_name,map<method_name,NodeType>>
+    mapper_node_map: HashMap<&'r str,HashMap<String, NodeType>>,
 }
 
 
 impl<'r> Rbatis<'r> {
     pub fn new() -> Rbatis<'r> {
-        return Rbatis { node_type: HashMap::new() };
+        return Rbatis { mapper_node_map: HashMap::new(), engine: RbatisEngine::new() };
     }
-    pub fn load_py(&mut self, name: &'r str, data: &str) -> Result<(), RbatisError> {
-        let data = Py::parser(data)?;
-        self.node_type.insert(name, data);
+
+    pub fn load_xml(&mut self, mapper_name: &'r str, data: &str) -> Result<(), RbatisError> {
+        let xml = Xml::parser(data);
+        self.mapper_node_map.insert(mapper_name, xml);
         return Ok(());
     }
 }
