f70c51693fa776aeda7e2d4d4c5e1019b343ebed
zhuxiujia <zhuxiujia@qq.com>
2020-06-12 14:24:30 +0800

    add method

--- src/rbatis.rs
+++ src/rbatis.rs
@@ -8,9 +8,14 @@ use crate::ast::node::insert_node::InsertNode;
 use crate::ast::node::delete_node::DeleteNode;
 use crate::ast::node::update_node::UpdateNode;
 use crate::ast::node::select_node::SelectNode;
+use rbatis_core::mysql::MySqlPool;
+use rbatis_core::executor::Executor;
+use serde::de::DeserializeOwned;
+use rbatis_core::cursor::Cursor;
 
 /// rbatis engine
 pub struct Rbatis<'r> {
+    pool: Option<MySqlPool>,
     engine: RbatisEngine,
     /// map<mapper_name,map<method_name,NodeType>>
     mapper_node_map: HashMap<&'r str, HashMap<String, NodeType>>,
@@ -18,13 +23,31 @@ pub struct Rbatis<'r> {
 
 
 impl<'r> Rbatis<'r> {
-    pub fn new() -> Rbatis<'r> {
-        return Rbatis { mapper_node_map: HashMap::new(), engine: RbatisEngine::new() };
+    pub async fn new(url: &str) -> Rbatis<'r> {
+        let mut pool = Option::None;
+        if url.ne("") {
+            pool = Some(MySqlPool::new(url).await.unwrap());
+        }
+        return Rbatis { pool, mapper_node_map: HashMap::new(), engine: RbatisEngine::new() };
     }
 
     pub fn load_xml(&mut self, mapper_name: &'r str, data: &str) -> Result<(), RbatisError> {
         let xml = Xml::parser(data);
         self.mapper_node_map.insert(mapper_name, xml);
         return Ok(());
+    }
+
+    /// fetch result
+    pub async fn fetch<T>(&self, sql: &str) -> Result<T, rbatis_core::Error>
+        where T: DeserializeOwned {
+        let mut conn = self.pool.as_ref().unwrap().acquire().await.unwrap();
+        let mut c = conn.fetch(sql);
+        return c.decode().await;
+    }
+
+    /// exec sql
+    pub async fn exec(&self, sql: &str) -> Result<u64, rbatis_core::Error> {
+        let mut conn = self.pool.as_ref().unwrap().acquire().await.unwrap();
+        return conn.execute(sql).await;
     }
 }
