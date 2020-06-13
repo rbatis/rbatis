5c5ebb3a00cac5f38b9625d087f21e721b8360d2
zhuxiujia <zhuxiujia@qq.com>
2020-06-14 00:03:04 +0800

    del unsafe unwrap()

--- src/utils/error_util.rs
+++ src/utils/error_util.rs
@@ -4,31 +4,25 @@ use rbatis_core::Error;
 
 use crate::ast::node::node_type::NodeType;
 
-/// Option to Result
-fn to_result<'a,T,F>(arg: &Option<&'a T>, fail_method: F) -> Result<&'a T, Error>
-    where F : Fn() -> String
-{
-    if arg.is_none() {
-        return Err(Error::from(fail_method()));
-    }
-    return Ok(arg.unwrap());
-}
-
-pub trait ToResult<T>  {
+pub trait ToResult<T> {
     fn to_result<F>(&self, fail_method: F) -> Result<&T, Error>
-        where F : Fn() -> String;
+        where F: Fn() -> String;
 }
 
-impl <T>ToResult<T> for Option<&T> {
+impl<T> ToResult<T> for Option<&T> {
     #[inline]
-    fn to_result<F>(&self, fail_method: F) -> Result<&T, Error> where F: Fn() -> String {
-        to_result(self, fail_method)
+    fn to_result<F>(&self, fail_method: F) -> Result<&T, Error>
+        where F: Fn() -> String {
+        if self.is_none() {
+            return Err(Error::from(fail_method()));
+        }
+        return Ok(self.unwrap());
     }
 }
 
 #[test]
-fn test_to_result(){
-    let i=1;
-    let v= Option::Some(&i);
-    let r= v.to_result(||"".to_string());
+fn test_to_result() {
+    let i = 1;
+    let v = Option::Some(&i);
+    let r = v.to_result(|| "".to_string());
 }
