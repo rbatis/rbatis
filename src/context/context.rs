a0305791d79470c504b1c5207a3cc0c227fdea4e
zhuxiujia <zhuxiujia@qq.com>
2020-06-18 00:48:01 +0800

    add mod

--- src/context/context.rs
+++ src/context/context.rs
@@ -10,19 +10,19 @@ use rbatis_core::pool::PoolConnection;
 //TODO impl Context hold tx
 pub struct Context {
     //TODO Arc use async std's Arc ???
-    tx: Mutex<RefCell<HashMap<String, PoolConnection<MySqlConnection>>>>,
+    tx_map: Mutex<RefCell<HashMap<String, PoolConnection<MySqlConnection>>>>,
 }
 
 
 impl Context {
     pub fn new() -> Context {
         Context {
-            tx: Mutex::new(RefCell::new(HashMap::new()))
+            tx_map: Mutex::new(RefCell::new(HashMap::new()))
         }
     }
 
     pub fn put(&self, key: &str, value: PoolConnection<MySqlConnection>) {
-        let mut m = self.tx.lock().unwrap();
+        let mut m = self.tx_map.lock().unwrap();
         let map = m.get_mut();
         map.insert(key.to_string(), value);
     }
