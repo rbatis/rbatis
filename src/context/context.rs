b502b224e7efaaadfd4cfaf1f78fc148da11962d
zhuxiujia <zhuxiujia@qq.com>
2020-06-17 23:49:57 +0800

    add mod

--- src/context/context.rs
+++ src/context/context.rs
@@ -1,10 +1,29 @@
-use std::sync::Arc;
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
-use rbatis_core::pool::PoolConnection;
+use std::collections::HashMap;
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
+
 use rbatis_core::mysql::MySqlConnection;
+use rbatis_core::pool::PoolConnection;
 
 //TODO impl Context hold tx
-pub struct Context{
+pub struct Context {
     //TODO Arc use async std's Arc ???
-    tx:Arc<RefCell<Option<PoolConnection<MySqlConnection>>>>,
+    tx: Mutex<RefCell<HashMap<String, PoolConnection<MySqlConnection>>>>,
+}
+
+
+impl Context {
+    pub fn new() -> Context {
+        Context {
+            tx: Mutex::new(RefCell::new(HashMap::new()))
+        }
+    }
+
+    pub fn put(&self, key: &str, value: PoolConnection<MySqlConnection>) {
+        let mut m = self.tx.lock().unwrap();
+        let map = m.get_mut();
+        map.insert(key.to_string(), value);
+    }
 }
