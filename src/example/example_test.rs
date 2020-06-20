ebc463e53d64490b509602053dfabd17b2f35d48
zhuxiujia <zhuxiujia@qq.com>
2020-06-20 23:46:06 +0800

    fix bug

--- src/example/example_test.rs
+++ src/example/example_test.rs
@@ -57,7 +57,7 @@ pub fn test_prepare_sql() {
             fast_log::log::init_log("requests.log", &RuntimeType::Std).unwrap();
             let rb = Rbatis::new(MYSQL_URL).await.unwrap();
             //pooledConn 交由rbatis上下文管理
-            let arg = &vec![json!(1), serde_json::Value::String("test%".to_string())];
+            let arg = &vec![json!(1), json!("test%")];
             let r: Vec<Activity> = rb.fetch_prepare("", "SELECT * FROM biz_activity WHERE delete_flag =  ? AND name like ?", arg).await.unwrap();
             println!("done:{:?}", r);
         }
