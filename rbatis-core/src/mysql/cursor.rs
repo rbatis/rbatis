2712f617a831491f1f84e17189466a7076a806fa
zhuxiujia <zhuxiujia@qq.com>
2020-06-27 02:27:39 +0800

    add fetch json method

--- rbatis-core/src/mysql/cursor.rs
+++ rbatis-core/src/mysql/cursor.rs
@@ -63,18 +63,7 @@ impl<'c, 'q> Cursor<'c, 'q> for MySqlCursor<'c, 'q> {
     fn decode_json<T>(&mut self) -> BoxFuture<Result<T, crate::Error>>
         where T: DeserializeOwned {
         Box::pin(async move {
-            let mut arr = vec![];
-            while let Some(row) = self.next().await? as Option<MySqlRow<'_>> {
-                let mut m = serde_json::Map::new();
-                let keys = row.names.keys();
-                for x in keys {
-                    let key = x.to_string();
-                    let key_str=key.as_str();
-                    let v:serde_json::Value = row.json_decode_impl(key_str)?;
-                    m.insert(key, v);
-                }
-                arr.push(serde_json::Value::Object(m));
-            }
+            let arr = self.fetch_json().await?;
             let r = json_decode(arr)?;
             return Ok(r);
         })
