ca6e2ec9ad0ddeff4120295c443c317f6b9bd195
zhuxiujia <zhuxiujia@qq.com>
2020-06-26 11:32:47 +0800

    fix future

--- rbatis-core/src/runtime.rs
+++ rbatis-core/src/runtime.rs
@@ -1,10 +1,10 @@
 #![allow(unused_imports)]
 
 #[cfg(not(any(feature = "runtime-tokio", feature = "runtime-async-std")))]
-compile_error!("one of 'runtime-async-std' or 'runtime-tokio' features must be enabled");
+compile_error!("'runtime-async-std' or 'runtime-tokio' features which one of must be enabled");
 
 #[cfg(all(feature = "runtime-tokio", feature = "runtime-async-std"))]
-compile_error!("only one of 'runtime-async-std' or 'runtime-tokio' features must be enabled");
+compile_error!("'runtime-async-std' or 'runtime-tokio' features which one of must be enabled");
 
 #[cfg(feature = "runtime-async-std")]
 pub(crate) use async_std::{
