8fb31cbb61ca7fe6bff68eef22278dc309a027ee
zhuxiujia <zhuxiujia@qq.com>
2020-06-29 21:28:35 +0800

    add lang support

--- src/lang/limit.rs
+++ src/lang/limit.rs
@@ -9,14 +9,24 @@ impl PageLimit for DriverType {
                 Ok(format!(" LIMIT {},{}", offset, size))
             }
             DriverType::Postgres => {
-                Ok(format!(" LIMIT {} offset {}", size, offset))
+                Ok(format!(" LIMIT {} OFFSET {}", size, offset))
             }
             DriverType::Sqlite => {
-                Ok(format!(" LIMIT {} offset {}", size, offset))
+                Ok(format!(" LIMIT {} OFFSET {}", size, offset))
             }
             _ => {
                 Err(rbatis_core::Error::from(format!("[rbatis] not support now for DriverType:{:?}", self)))
             }
         };
     }
+}
+
+#[test]
+pub fn test_create_limit() {
+    let mysql_limit = DriverType::Mysql.create(1, 20).unwrap();
+    println!("{}", mysql_limit);
+    let pg_limit = DriverType::Postgres.create(1, 20).unwrap();
+    println!("{}", pg_limit);
+    let sqlite_limit = DriverType::Sqlite.create(1, 20).unwrap();
+    println!("{}", sqlite_limit);
 }
