eeb72699e8895a954621b62e13a7ea7cdc900b2a
zhuxiujia <zhuxiujia@qq.com>
2020-06-30 16:17:58 +0800

    remove clone limit

--- src/rbatis.rs
+++ src/rbatis.rs
@@ -357,7 +357,7 @@ impl<'r> Rbatis<'r> {
 
 
     pub async fn fetch_page<T>(&self, tx_id: &str, sql: &str, args: Vec<serde_json::Value>, page: &dyn IPageRequest) -> Result<Page<T>, rbatis_core::Error>
-        where T: DeserializeOwned + Serialize + Clone {
+        where T: DeserializeOwned + Serialize {
         let mut page_result = Page::new(page.get_current(), page.get_size());
         let (count_sql, sql) = self.page_plugin.create_page_sql(&self.driver_type()?, tx_id, sql, &args, page)?;
         if page.is_serch_count() {
@@ -376,13 +376,13 @@ impl<'r> Rbatis<'r> {
 
     /// fetch result(prepare sql)
     pub async fn xml_fetch_page<T>(&self, tx_id: &str, mapper: &str, method: &str, arg: &serde_json::Value, page: &dyn IPageRequest) -> Result<Page<T>, rbatis_core::Error>
-        where T: DeserializeOwned + Serialize + Clone {
+        where T: DeserializeOwned + Serialize {
         let (sql, args) = self.xml_to_sql(mapper, method, arg)?;
         return self.fetch_page::<T>(tx_id, sql.as_str(), args, page).await;
     }
 
     pub async fn py_fetch_page<T>(&self, tx_id: &str, py: &str, arg: &serde_json::Value, page: &dyn IPageRequest) -> Result<Page<T>, rbatis_core::Error>
-        where T: DeserializeOwned + Serialize + Clone {
+        where T: DeserializeOwned + Serialize {
         let (sql, args) = self.py_to_sql(py, arg)?;
         return self.fetch_page::<T>(tx_id, sql.as_str(), args, page).await;
     }
