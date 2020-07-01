2149b6d32c6a968bfffed7263c5d3f9a1925a66a
zhuxiujia <zhuxiujia@qq.com>
2020-07-01 17:03:41 +0800

    add version_lock.rs

--- src/plugin/version_lock.rs
+++ src/plugin/version_lock.rs
@@ -1,1 +1,27 @@
 //TODO version lock
+
+use rbatis_core::db::DriverType;
+use serde_json::Value;
+use rbatis_core::Error;
+
+pub trait VersionLock: Send + Sync {
+
+    fn make_update_sql(&self, driver_type: &DriverType, tx_id: &str, sql: &str, args: &Vec<serde_json::Value>) -> Result<String, rbatis_core::Error>;
+}
+
+
+pub struct RbatisLogicDeletePlugin{}
+
+
+impl VersionLock for RbatisLogicDeletePlugin{
+
+
+    fn make_update_sql(&self, driver_type: &DriverType, tx_id: &str, sql: &str, args: &Vec<Value>) -> Result<String, Error> {
+        //TODO SET sql insert del_flag = 0
+        unimplemented!();
+
+        let mut new_sql= sql.to_string();
+        new_sql = new_sql.replace("update ","UPDATE ");
+        new_sql = new_sql.replace("set ","SET ");
+    }
+}
