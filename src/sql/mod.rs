87b3359342f2ab71ecb970cdb416a4514eb97cfe
zhuxiujia <zhuxiujia@qq.com>
2020-07-18 11:57:26 +0800

    add sql date convert

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,8 +1,17 @@
+use serde_json::Value;
 
 pub mod limit;
 
+pub mod date;
 
 
 pub trait PageLimit {
+    /// return  sql
     fn page_limit_sql(&self, offset:u64, size:u64) -> rbatis_core::Result<String>;
+}
+
+
+pub trait Date {
+    /// return  (sql,value)
+    fn date_convert(&self,value:&serde_json::Value,index:usize) -> rbatis_core::Result<(String, Value)>;
 }
