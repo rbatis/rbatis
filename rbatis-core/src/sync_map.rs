f2eb2b4550264d346114ab8c39bd2f5522c98bec
zhuxiujia <zhuxiujia@qq.com>
2020-08-01 18:12:08 +0800

    fix sync map

--- rbatis-core/src/sync_map.rs
+++ rbatis-core/src/sync_map.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 use crate::runtime::Mutex;
+use crate::runtime::MutexGuard;
+use dashmap::DashMap;
+use dashmap::mapref::one::RefMut;
+use crate::Error;
 
 /// sync map is safe for sync and send
 ///
@@ -16,25 +20,35 @@ use crate::runtime::Mutex;
 ///
 #[derive(Debug)]
 pub struct SyncMap<T> {
-    pub cell: Mutex<HashMap<String, T>>
+    pub cell: Mutex<DashMap<String, T>>
 }
+
 
 impl<T> SyncMap<T> {
     pub fn new() -> SyncMap<T> {
         SyncMap {
-            cell: Mutex::new(HashMap::new())
+            cell: Mutex::new(DashMap::new())
         }
     }
 
     /// put an value,this value will move lifetime into SyncMap
     pub async fn put(&self, key: &str, value: T) {
-        let mut lock = self.cell.lock().await;
-        lock.insert(key.to_string(), value);
+        self.cell.lock().await.insert(key.to_string(), value);
     }
 
     /// pop value,lifetime will move to caller
     pub async fn pop(&self, key: &str) -> Option<T> {
-        let mut lock = self.cell.lock().await;
-        return lock.remove(key);
+        let data: Option<(String, T)> = self.cell.lock().await.remove(key);
+        if data.is_none() {
+            return None;
+        } else {
+            return Some(data.unwrap().1);
+        }
     }
+
+    pub async fn lock<'a>(&'a self) -> MutexGuard<'a, DashMap<String, T>> {
+        let data = self.cell.lock().await;
+        return data;
+    }
+
 }
