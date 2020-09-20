e0bf21badff4b55a3aaecd0e9ee0cc23230b4e73
zhuxiujia <zhuxiujia@qq.com>
2020-09-20 21:44:35 +0800

    fix logic_delete.rs select sql bug

--- src/plugin/logic_delete.rs
+++ src/plugin/logic_delete.rs
@@ -11,10 +11,16 @@ pub trait LogicDelete: Send + Sync {
     fn deleted(&self) -> i32;
     /// un deleted data,must be i32
     fn un_deleted(&self) -> i32;
-    /// create_update_sql
-    fn create_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &Vec<&str>, sql_where: &str) -> Result<String, rbatis_core::Error>;
+    /// create_remove_sql
+    fn create_remove_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &str, sql_where: &str) -> Result<String, rbatis_core::Error>;
+    /// create_select_sql
+    fn create_select_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &str, sql_where: &str) -> Result<String, rbatis_core::Error>;
 }
 
+pub enum LogicAction {
+    Select,
+    Delete,
+}
 
 pub struct RbatisLogicDeletePlugin {
     pub column: String,
@@ -56,8 +62,8 @@ impl LogicDelete for RbatisLogicDeletePlugin {
     }
 
 
-    fn create_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &Vec<&str>, sql_where: &str) -> Result<String, Error> {
-        return if table_fields.contains(&self.column.as_str()) {
+    fn create_remove_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &str, sql_where: &str) -> Result<String, Error> {
+        return if table_fields.contains(self.column()) {
             //fields have column
             let new_sql = format!("UPDATE {} SET {} = {}", table_name, self.column(), self.deleted()) + sql_where;
             Ok(new_sql)
@@ -68,6 +74,24 @@ impl LogicDelete for RbatisLogicDeletePlugin {
             Err(Error::from("[rbatis] del data must have where sql!"))
         };
     }
+
+    fn create_select_sql(&self, driver_type: &DriverType, table_name: &str, table_fields: &str, where_sql: &str) -> Result<String, Error> {
+        let mut where_sql = where_sql.to_string();
+        let mut sql = String::new();
+        if table_fields.contains(self.column()) {
+            if where_sql.is_empty() {
+                where_sql = format!("{} = {}", self.column(), self.un_deleted());
+            } else {
+                where_sql = format!("{} = {} AND ", self.column(), self.un_deleted()) + where_sql.as_str();
+            }
+        }
+        if !where_sql.is_empty() {
+            sql = format!("SELECT {} FROM {} WHERE {}", table_fields, table_name, where_sql);
+        } else {
+            sql = format!("SELECT {} FROM {}", table_fields, table_name);
+        }
+        Ok(sql)
+    }
 }
 
 #[cfg(test)]
@@ -77,23 +101,23 @@ mod tests {
     #[test]
     fn test_logic_delete_plugin_create_sql() {
         let r = RbatisLogicDeletePlugin::new("del");
-        let table_fields = vec!["name", "age", "del"];
+        let table_fields = "name,age,del".to_string();
         let sql_where = "";
-        let result = r.create_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
+        let result = r.create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
         assert_eq!("UPDATE test SET del = 1", &result);
 
         let sql_where = " WHERE name = 'zhangsan'";
-        let result = r.create_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
+        let result = r.create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
         assert_eq!("UPDATE test SET del = 1 WHERE name = 'zhangsan'", &result);
 
-        let table_fields = vec!["name", "age"];
+        let table_fields = "name,age";
         let sql_where = " WHERE name = 'zhangsan'";
-        let result = r.create_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
+        let result = r.create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where).unwrap();
         assert_eq!("DELETE FROM test WHERE name = 'zhangsan'", &result);
 
-        let table_fields = vec!["name", "age"];
+        let table_fields = "name,age";
         let sql_where = "";
-        let result = r.create_sql(&DriverType::Mysql, "test", &table_fields, sql_where);
+        let result = r.create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where);
         assert!(result.is_err());
     }
 }
