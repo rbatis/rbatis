91e84f3d86333b06577377a2276986a856a2ca67
zhuxiujia <zhuxiujia@qq.com>
2020-09-27 00:45:53 +0800

    add intercept.rs

--- src/plugin/intercept.rs
+++ src/plugin/intercept.rs
@@ -1,4 +1,7 @@
 /// sql intercept
 pub trait SqlIntercept: Send + Sync {
+    ///intercept name
+    fn name(&self) -> &str;
+    /// do intercept sql/args
     fn do_intercept(&self, sql: &mut String, args: &mut Vec<serde_json::Value>) -> Result<(), rbatis_core::Error>;
 }
