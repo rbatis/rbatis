f6a6b6184790da75cb8a662170af586da299ce18
ce <zhuxiujia@qq.com>
2020-09-29 09:45:57 +0800

    edit format

--- src/sql/date.rs
+++ src/sql/date.rs
@@ -4,22 +4,6 @@ use rbatis_core::convert::StmtConvert;
 use rbatis_core::db::DriverType;
 
 use crate::crud::ColumnFormat;
-use crate::sql::Date;
-
-impl Date for DriverType {
-    fn date_convert(&self, value: &Value, index: usize) -> rbatis_core::Result<(String, Value)> {
-        let mut sql = String::new();
-        match self {
-            DriverType::Postgres => {
-                sql = format!("cast({} as timestamp)", self.stmt_convert(index).as_str());
-            }
-            _ => {
-                sql = self.stmt_convert(index);
-            }
-        }
-        return Ok((sql, value.to_owned()));
-    }
-}
 
 #[derive(Copy, Clone, Debug)]
 pub struct DateFormat {}
