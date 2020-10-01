50d0221b12032f27ce8d126ba9d663ba73bde72c
zhuxiujia <zhuxiujia@qq.com>
2020-10-01 15:03:41 +0800

    Wrapper add push_arg method

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -484,12 +484,22 @@ impl Wrapper {
     }
 
     pub fn trim_and(&mut self) -> &mut Self {
-        self.sql = self.sql.trim_start_matches(" AND ").trim_end_matches(" AND ").to_string();
+        self.sql = self.sql
+            .trim_start_matches(" AND ")
+            .trim_end_matches(" AND ")
+            .trim_start_matches("AND ")
+            .trim_end_matches(" AND")
+            .to_string();
         self
     }
 
     pub fn trim_or(&mut self) -> &mut Self {
-        self.sql = self.sql.trim_start_matches(" OR ").trim_end_matches(" OR ").to_string();
+        self.sql = self.sql.
+            trim_start_matches(" OR ")
+            .trim_end_matches(" OR ")
+            .trim_start_matches("OR ")
+            .trim_end_matches(" OR")
+            .to_string();
         self
     }
 }
