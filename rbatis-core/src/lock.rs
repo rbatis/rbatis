4ec57cd6ecbae202524d1c591d7d40b47f945e17
ce <zhuxiujia@qq.com>
2020-10-15 14:34:57 +0800

    add lock

--- rbatis-core/src/lock.rs
+++ rbatis-core/src/lock.rs
@@ -1,10 +1,10 @@
 #![allow(unsafe_code)]
 use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
-use std::borrow::Borrow;
+use std::borrow::{Borrow, BorrowMut};
 use std::hash::Hash;
 use std::ops::Deref;
-use crate::runtime::{RwLock, RwLockReadGuard};
+use crate::runtime::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 
 pub struct SyncMap<K, V> where K: Eq + Hash {
@@ -38,10 +38,31 @@ impl<'a, K: 'a + Eq + Hash, V: 'a> SyncMap<K, V> where K: Eq + Hash {
             }
         }
     }
+
+    pub async fn get_mut<Q>(&'a self, k: &Q) -> Option<RefMut<'a, K, V>>
+        where K: Borrow<Q>,
+              Q: Hash + Eq + ?Sized {
+        let mut get_ref = RefMut::new(self.shard.write().await, None);
+        unsafe {
+            let v = get_ref._guard.get_mut(k);
+            if v.is_some() {
+                let vptr = change_lifetime_mut(v.unwrap());
+                get_ref.v = Option::Some(vptr);
+                Some(get_ref)
+            } else {
+                None
+            }
+        }
+    }
+
 }
 
 pub unsafe fn change_lifetime_const<'a, 'b, T>(x: &'a T) -> &'b T {
     &*(x as *const T)
+}
+
+pub unsafe fn change_lifetime_mut<'a, 'b, T>(x: &'a mut T) -> &'b mut T {
+    &mut *(x as *mut T)
 }
 
 pub struct Ref<'a, K, V>
@@ -70,6 +91,32 @@ impl<'a, K: Eq + Hash, V> Deref for Ref<'a, K, V> {
         &self.value()
     }
 }
+
+
+pub struct RefMut<'a, K, V, S = RandomState> {
+    _guard: RwLockWriteGuard<'a, HashMap<K, V, S>>,
+    v: Option<&'a mut V>,
+}
+
+impl<'a, K: Eq + Hash, V> RefMut<'a, K, V> {
+
+    pub fn new(guard: RwLockWriteGuard<'a, HashMap<K, V, RandomState>>, v: Option<&'a mut V>) -> Self {
+        let mut s = Self {
+            _guard: guard,
+            v: v,
+        };
+        s
+    }
+
+    // pub fn value(&self) -> &V {
+    //     self.v.unwrap()
+    // }
+    //
+    // pub fn value_mut(&mut self) -> &mut V {
+    //     self.v.unwrap()
+    // }
+}
+
 
 
 #[cfg(test)]
