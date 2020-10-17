328079816630d24dc282b11740576ab3d433693a
zhuxiujia <zhuxiujia@qq.com>
2020-10-17 20:41:01 +0800

    add log plugin

---
+++ src/plugin/log.rs
@@ -0,0 +1,36 @@
+use log::{debug, error, info, LevelFilter, trace, warn};
+use log::Level;
+
+/// log plugin
+pub trait LogPlugin: Send + Sync {
+    fn error(&self, data: &str);
+    fn warn(&self, data: &str);
+    fn info(&self, data: &str);
+    fn debug(&self, data: &str);
+    fn trace(&self, data: &str);
+}
+
+
+pub struct RbatisLog {}
+
+impl LogPlugin for RbatisLog {
+    fn error(&self, data: &str) {
+        error!("{}", data);
+    }
+
+    fn warn(&self, data: &str) {
+        warn!("{}", data);
+    }
+
+    fn info(&self, data: &str) {
+        info!("{}", data);
+    }
+
+    fn debug(&self, data: &str) {
+        debug!("{}", data);
+    }
+
+    fn trace(&self, data: &str) {
+        trace!("{}", data);
+    }
+}
