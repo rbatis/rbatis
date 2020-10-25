e5f31ace45f44c76c4b7914f6d9ad5f66185aa36
zhuxiujia <zhuxiujia@qq.com>
2020-10-25 13:56:33 +0800

    add test

--- src/utils/bencher.rs
+++ src/utils/bencher.rs
@@ -64,7 +64,7 @@ impl Bencher {
 
     pub fn use_tps(total: u64, start: SystemTime, end: SystemTime) {
         let time = end.duration_since(start).unwrap();
-        println!("use TPS: {} TPS/s", total / time.as_secs());
+        println!("use TPS: {} TPS/s", (total as u128 * 1000000000 as u128 / time.as_nanos() as u128));
     }
 
     //计算每个操作耗时nano纳秒
