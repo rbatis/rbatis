f95642ff4c95ad7d6f000e75df15249c30c4a23b
zhuxiujia <zhuxiujia@qq.com>
2020-10-30 01:26:12 +0800

    fix decode bug

--- rbatis-core/src/mysql/sqlx_value.rs
+++ rbatis-core/src/mysql/sqlx_value.rs
@@ -2,13 +2,14 @@ use sqlx_core::decode::Decode;
 use sqlx_core::error::BoxDynError;
 use sqlx_core::mysql::{MySql, MySqlRow, MySqlValueRef, MySqlValue};
 use sqlx_core::type_info::TypeInfo;
-use sqlx_core::types::BigDecimal;
 use sqlx_core::value::ValueRef;
 
 use crate::convert::{JsonCodec, RefJsonCodec};
 use sqlx_core::row::Row;
 use sqlx_core::column::Column;
 use crate::db_adapter::convert_result;
+use serde_json::{json, Value};
+use sqlx_core::types::BigDecimal;
 
 impl<'r> JsonCodec for sqlx_core::mysql::MySqlValueRef<'r> {
     fn try_to_json(self) -> crate::Result<serde_json::Value> {
@@ -19,91 +20,94 @@ impl<'r> JsonCodec for sqlx_core::mysql::MySqlValueRef<'r> {
                 return Ok(serde_json::Value::Null);
             }
             "NEWDECIMAL" => {
-                let r: Result<BigDecimal, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<BigDecimal>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap().to_string()));
+                if r.as_ref().unwrap().is_none(){
+                    return Ok(serde_json::Value::Null);
+                }
+                return Ok(json!(r.unwrap().unwrap().to_string()));
             }
             "BIGINT UNSIGNED" => {
-                let r: Result<u64, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<u64>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "BIGINT" => {
-                let r: Result<i64, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<i64>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "INT UNSIGNED" => {
-                let r: Result<u32, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<u32>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "INT" => {
-                let r: Result<i32, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<i32>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "SMALLINT" => {
-                let r: Result<i16, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<i16>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "SMALLINT UNSIGNED" => {
-                let r: Result<u16, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<u16>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "TINYINT UNSIGNED" => {
-                let r: Result<u8, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<u8>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "TINYINT" => {
-                let r: Result<i8, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<i8>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "FLOAT" => {
-                let r: Result<f32, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<f32>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "DOUBLE" => {
-                let r: Result<f64, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<f64>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "BINARY" | "VARBINARY" | "BLOB" | "CHAR" | "VARCHAR" | "TEXT" => {
-                let r: Result<String, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<String>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
-                return Ok(serde_json::Value::from(r.unwrap()));
+                return Ok(json!(r.unwrap()));
             }
             "DATE" => {
-                let r: Result<chrono::NaiveDate, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<chrono::NaiveDate>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
@@ -111,7 +115,7 @@ impl<'r> JsonCodec for sqlx_core::mysql::MySqlValueRef<'r> {
                 return Ok(t.unwrap_or(serde_json::Value::Null));
             }
             "TIME" => {
-                let r: Result<chrono::NaiveTime, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<chrono::NaiveTime>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
@@ -119,7 +123,7 @@ impl<'r> JsonCodec for sqlx_core::mysql::MySqlValueRef<'r> {
                 return Ok(t.unwrap_or(serde_json::Value::Null));
             }
             "DATETIME" => {
-                let r: Result<chrono::NaiveDateTime, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<chrono::NaiveDateTime>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
@@ -127,7 +131,7 @@ impl<'r> JsonCodec for sqlx_core::mysql::MySqlValueRef<'r> {
                 return Ok(t.unwrap_or(serde_json::Value::Null));
             }
             "TIMESTAMP" => {
-                let r: Result<chrono::NaiveDateTime, BoxDynError> = Decode::<'_, MySql>::decode(self);
+                let r: Result<Option<chrono::NaiveDateTime>, BoxDynError> = Decode::<'_, MySql>::decode(self);
                 if r.is_err() {
                     return Err(crate::Error::from(r.err().unwrap().to_string()));
                 }
@@ -153,6 +157,6 @@ impl RefJsonCodec for Vec<MySqlRow>{
             }
             arr.push(serde_json::Value::Object(m));
         }
-        Ok(serde_json::Value::from(arr))
+        Ok(json!(arr))
     }
 }
