ee7dfdfb453e7f6754fcee800b946836d270254e
zhuxiujia <zhuxiujia@qq.com>
2020-11-08 18:17:02 +0800

    replace and or

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -6,6 +6,5 @@ use crate::engine::runtime::RbatisEngine;
 
 /// Abstract syntax tree node
 pub trait RbatisAST: Send + Sync + Clone {
-    /// env: &mut Value,因为bind node 会绑定变量，env必须为可修改的值 arg_result: 执行后 提交到 驱动的参数
     fn eval(&self, convert: &impl StmtConvert, env: &mut Value, engine: &RbatisEngine, arg_result: &mut Vec<Value>) -> Result<String, rbatis_core::Error>;
 }
