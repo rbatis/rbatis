b465646d3484eea22229989e4e7179b3b7cf4045
ce <zhuxiujia@qq.com>
2020-11-11 10:32:51 +0800

    foreach_node support object

--- src/ast/node/foreach_node.rs
+++ src/ast/node/foreach_node.rs
@@ -34,30 +34,51 @@ impl RbatisAST for ForEachNode {
         if collection_value.is_null() {
             return Result::Err(rbatis_core::Error::from("[rbatis] collection name:".to_owned() + self.collection.as_str() + " is none value!"));
         }
-        if !collection_value.is_array() {
-            return Result::Err(rbatis_core::Error::from("[rbatis] collection name:".to_owned() + self.collection.as_str() + " is not a array value!"));
-        }
-        let collection = collection_value.as_array().unwrap();
-
-        let collection_len = collection.len() as i32;
-        let mut index = -1;
-        let have_separator = !self.separator.is_empty();
-        for item in collection {
-            index = index + 1;
-            //build temp arg
-            let mut obj_map = serde_json::Map::new();
-            obj_map.insert("item".to_string(), item.clone());
-            obj_map.insert("index".to_string(), Value::Number(serde_json::Number::from_f64(index as f64).unwrap()));
-            let mut temp_arg: Value = Value::Object(obj_map);
-            let item_result = do_child_nodes(convert, &self.childs, &mut temp_arg, engine, arg_array)?;
-            result = result + item_result.as_str();
-            if have_separator && (index + 1) < collection_len {
-                result = result + self.separator.as_str();
+        if collection_value.is_array(){
+            let collection = collection_value.as_array().unwrap();
+            let collection_len = collection.len() as i32;
+            let mut index = -1;
+            let have_separator = !self.separator.is_empty();
+            for item in collection {
+                index = index + 1;
+                //build temp arg
+                let mut obj_map = serde_json::Map::new();
+                obj_map.insert("item".to_string(), item.clone());
+                obj_map.insert("index".to_string(), json!(index));
+                let mut temp_arg: Value = Value::Object(obj_map);
+                let item_result = do_child_nodes(convert, &self.childs, &mut temp_arg, engine, arg_array)?;
+                result = result + item_result.as_str();
+                if have_separator && (index + 1) < collection_len {
+                    result = result + self.separator.as_str();
+                }
             }
+            //close
+            result = result + self.close.as_str();
+            return Result::Ok(result);
+        }else if collection_value.is_object(){
+            let collection = collection_value.as_object().unwrap();
+            let collection_len = collection.len() as i32;
+            let mut index = -1;
+            let have_separator = !self.separator.is_empty();
+            for (key,item) in collection {
+                index = index + 1;
+                //build temp arg
+                let mut obj_map = serde_json::Map::new();
+                obj_map.insert("item".to_string(), item.clone());
+                obj_map.insert("index".to_string(), json!(key));
+                let mut temp_arg: Value = Value::Object(obj_map);
+                let item_result = do_child_nodes(convert, &self.childs, &mut temp_arg, engine, arg_array)?;
+                result = result + item_result.as_str();
+                if have_separator && (index + 1) < collection_len {
+                    result = result + self.separator.as_str();
+                }
+            }
+            //close
+            result = result + self.close.as_str();
+            return Result::Ok(result);
+        }else{
+            return Result::Err(rbatis_core::Error::from("[rbatis] collection name:".to_owned() + self.collection.as_str() + " is not a array or object/map value!"));
         }
-        //close
-        result = result + self.close.as_str();
-        return Result::Ok(result);
     }
 }
 
@@ -91,9 +112,33 @@ pub fn test_for_each_node() {
         separator: ",".to_string(),
     };
     let mut john = json!({
-        "arg": 1,
+        "arg": [1,2,3],
     });
     let mut arg_array = vec![];
     let r = n.eval(&DriverType::Mysql, &mut john, &mut engine, &mut arg_array);
     println!("{}", r.unwrap_or("null".to_string()));
+    println!("{}", json!(arg_array));
+}
+
+#[test]
+pub fn test_for_each_object_node() {
+    let mut engine = RbatisEngine::new();
+    let n = ForEachNode {
+        childs: vec![NodeType::NString(StringNode::new("index:#{index},item:#{item}"))],
+        collection: "arg".to_string(),
+        index: "index".to_string(),
+        item: "item".to_string(),
+        open: "(".to_string(),
+        close: ")".to_string(),
+        separator: ",".to_string(),
+    };
+    let mut john = json!({
+        "arg": {
+           "id":1
+        },
+    });
+    let mut arg_array = vec![];
+    let r = n.eval(&DriverType::Mysql, &mut john, &mut engine, &mut arg_array);
+    println!("{}", r.unwrap_or("null".to_string()));
+    println!("{}", json!(arg_array));
 }
