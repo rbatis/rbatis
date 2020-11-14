b3ccdef48f11f9bf47d2cce7d17a8fbff05b6d7a
zhuxiujia <zhuxiujia@qq.com>
2020-11-14 18:14:01 +0800

    format error info

--- rbatis-core/src/decode.rs
+++ rbatis-core/src/decode.rs
@@ -78,7 +78,7 @@ pub fn json_decode<T: ?Sized>(datas: Vec<serde_json::Value>) -> Result<T, crate:
         return Result::Ok(decode_result.unwrap());
     } else {
         let e = decode_result.err().unwrap().to_string();
-        return Result::Err(Error::from(format!("[rbatis] json decode: {}, fail:{}", type_name, e)));
+        return Result::Err(Error::from(format!("[rbatis] json decode fail '{}',info :'{}'", type_name, e)));
     }
 }
 
