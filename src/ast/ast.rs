8eb2c292fd4314b59889b975198b2d39f9cd5b6e
zhuxiujia <zhuxiujia@qq.com>
2020-11-15 18:08:33 +0800

    fix

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -5,6 +5,6 @@ use crate::core::convert::StmtConvert;
 use crate::engine::runtime::RbatisEngine;
 
 /// Abstract syntax tree node
-pub trait RbatisAST: Send + Sync + Clone {
+pub trait RbatisAST: Send + Sync {
     fn eval(&self, convert: &crate::core::db::DriverType, env: &mut Value, engine: &RbatisEngine, arg_result: &mut Vec<Value>) -> Result<String, crate::core::Error>;
 }
