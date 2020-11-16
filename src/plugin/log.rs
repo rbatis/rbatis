5dcd9e66538e973d9bb5aa744195db7f1e452eb6
ce <zhuxiujia@qq.com>
2020-11-16 12:03:56 +0800

    log filter

--- src/plugin/log.rs
+++ src/plugin/log.rs
@@ -1,5 +1,9 @@
+use log::{debug, error, info, LevelFilter, trace, warn};
+use std::ops::Deref;
 /// log plugin
 pub trait LogPlugin: Send + Sync {
+    fn is_enable(&self) -> bool;
+    fn do_log(&self, data: &str);
     fn error(&self, data: &str);
     fn warn(&self, data: &str);
     fn info(&self, data: &str);
@@ -7,11 +11,44 @@ pub trait LogPlugin: Send + Sync {
     fn trace(&self, data: &str);
 }
 
+pub struct RbatisLog {
+    pub filter: LevelFilter
+}
 
-use log::{debug, error, info, LevelFilter, trace, warn};
-pub struct RbatisLog {}
+impl Default for RbatisLog {
+    fn default() -> Self {
+        Self {
+            filter: log::LevelFilter::Info
+        }
+    }
+}
 
 impl LogPlugin for RbatisLog {
+    fn is_enable(&self) -> bool {
+        return !self.filter.eq(&log::LevelFilter::Off);
+    }
+
+    fn do_log(&self, data: &str) {
+        match self.filter {
+            log::LevelFilter::Error => {
+                self.error(data);
+            }
+            log::LevelFilter::Warn => {
+                self.warn(data);
+            }
+            log::LevelFilter::Info => {
+                self.info(data);
+            }
+            log::LevelFilter::Debug => {
+                self.debug(data);
+            }
+            log::LevelFilter::Trace => {
+                self.trace(data);
+            }
+            _ => {}
+        }
+    }
+
     fn error(&self, data: &str) {
         error!("{}", data);
     }
