93c6329ec3e77602d3bb7b2ae142cbb27b0e63d4
zhuxiujie <zhuxiujia@qq.com>
2020-12-09 15:01:03 +0800

    optimize the eval

--- src/interpreter/json/eval_test.rs
+++ src/interpreter/json/eval_test.rs
@@ -71,6 +71,7 @@ mod test {
         assert_eq!(exec_expr(&arg, "2*(1+1)"), json!(2*(1+1)));
         assert_eq!(exec_expr(&arg, "2*(1+(1+1)+1)"), json!(2*(1+(1+1)+1)));
         assert_eq!(exec_expr(&arg, "(((34 + 21) / 5) - 12) * 348"), json!((((34 + 21) / 5) - 12) * 348));
+        assert_eq!(exec_expr(&arg, "null ^ null"), json!(0 ^ 0));
     }
 
     #[test]
