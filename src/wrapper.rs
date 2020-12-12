475cce78b5c2dbd2234f2d60baee472882154761
zhuxiujia <zhuxiujia@qq.com>
2020-12-12 23:17:55 +0800

    fix

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -522,14 +522,14 @@ impl Wrapper {
 
     pub fn trim_and(&mut self) -> &mut Self {
         self.sql = self.sql.trim()
-            .trim_start_matches("AND")
+            .trim_end_matches("AND")
             .to_string();
         self
     }
 
     pub fn trim_or(&mut self) -> &mut Self {
         self.sql = self.sql.trim()
-            .trim_start_matches("OR")
+            .trim_end_matches("OR")
             .to_string();
         self
     }
