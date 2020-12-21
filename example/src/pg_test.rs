2ed117da132d917868b430d188d4d3ad0408b1ea
zhuxiujie <zhuxiujia@qq.com>
2020-12-21 13:30:07 +0800

    add test

--- example/src/pg_test.rs
+++ example/src/pg_test.rs
@@ -1,25 +1,21 @@
 use rbatis::rbatis::Rbatis;
 use rbatis::crud::CRUD;
-use serde::{Serialize,Deserialize};
-
-#[derive(Serialize,Deserialize,Clone,Debug)]
-pub struct Po{
-   pub x:i32,
-   pub y:i32
-}
-
+use serde_json::json;
 #[crud_enable]
-#[derive(Clone,Debug)]
-pub struct P{
-    pub id:i32,
-    pub po: Vec<u8>
+#[derive(Clone, Debug)]
+pub struct P {
+    pub id: i32,
+    pub po: Vec<u8>,
 }
 
 #[async_std::test]
 pub async fn test_select_point() {
-    let rb=Rbatis::new();
+    fast_log::init_log("requests.log",1000,log::Level::Info,None,true);
+    let rb = Rbatis::new();
     rb.link("postgres://postgres:123456@localhost:5432/postgres").await.unwrap();
 
-    let v:P=rb.fetch_by_id("",&1).await.unwrap();
-    println!("{:?}",v);
+    //insert point
+    let v = rb.exec_prepare("", "insert into p (id,po) values (1,Point($1,$2))",&vec![json!(1),json!(2)]).await.unwrap();
+
+    let v: Vec<P> = rb.list_by_wrapper("", &rb.new_wrapper_table::<P>().eq("id",1)).await.unwrap();
 }
