e9e8c7c16872a60c8bf01637e32b4e401cb238c2
zhuxiujia <zhuxiujia@qq.com>
2020-12-23 22:07:51 +0800

    add map_key method

--- rbatis-core/src/results/mod.rs
+++ rbatis-core/src/results/mod.rs
@@ -29,3 +29,17 @@ impl<T> ResultCodec<T> for Result<T, sqlx_core::error::Error> {
         }
     }
 }
+
+/// convert sqlx-Result to rbatis-core Result
+impl<T> ResultCodec<T> for Result<T, serde_json::Error> {
+    fn into_result(self) -> crate::Result<T> {
+        match self {
+            Ok(t) => {
+                return Ok(t);
+            }
+            Err(e) => {
+                return Err(crate::Error::from(e.to_string()));
+            }
+        }
+    }
+}
