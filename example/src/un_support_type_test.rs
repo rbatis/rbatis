45a25a581b0dfe345b63b5ea1c6c91e5ddc0213a
zhuxiujia <zhuxiujia@qq.com>
2020-12-24 20:44:10 +0800

    add doc test code

--- example/src/un_support_type_test.rs
+++ example/src/un_support_type_test.rs
@@ -1,23 +1,27 @@
-use rbatis::rbatis::Rbatis;
-use rbatis::crud::CRUD;
-use serde_json::json;
-#[crud_enable]
-#[derive(Clone, Debug)]
-pub struct P {
-    pub id: i32,
-    ///Postgres Point is not currently supported, but Vec is used instead
-    pub po: Vec<u8>,
-}
+#[cfg(test)]
+mod test {
+    use rbatis::rbatis::Rbatis;
+    use rbatis::crud::CRUD;
+    use serde_json::json;
 
-#[async_std::test]
-pub async fn test_select_point() {
-    fast_log::init_log("requests.log",1000,log::Level::Info,None,true);
-    let rb = Rbatis::new();
-    rb.link("postgres://postgres:123456@localhost:5432/postgres").await.unwrap();
-    //create table
-    rb.exec("",r#"CREATE TABLE "public"."p" ( "id" int4, "po" point);"#).await;
-    //insert point
-    rb.exec_prepare("", "insert into p (id,po) values (1,Point($1,$2))",&vec![json!(1),json!(2)]).await.unwrap();
-    //query table
-    let v: Vec<P> = rb.list_by_wrapper("", &rb.new_wrapper_table::<P>().eq("id",1)).await.unwrap();
+    #[crud_enable]
+    #[derive(Clone, Debug)]
+    pub struct P {
+        pub id: i32,
+        ///Postgres Point is not currently supported, but Vec is used instead
+        pub po: Vec<u8>,
+    }
+
+    #[async_std::test]
+    pub async fn test_select_point() {
+        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+        let rb = Rbatis::new();
+        rb.link("postgres://postgres:123456@localhost:5432/postgres").await.unwrap();
+        //create table
+        rb.exec("", r#"CREATE TABLE "public"."p" ( "id" int4, "po" point);"#).await;
+        //insert point
+        rb.exec_prepare("", "insert into p (id,po) values (1,Point($1,$2))", &vec![json!(1), json!(2)]).await.unwrap();
+        //query table
+        let v: Vec<P> = rb.list_by_wrapper("", &rb.new_wrapper_table::<P>().eq("id", 1)).await.unwrap();
+    }
 }
--- example/src/wrapper_test.rs
+++ example/src/wrapper_test.rs
@@ -1,19 +1,22 @@
-use rbatis::rbatis::Rbatis;
+#[cfg(test)]
+mod test {
+    use rbatis::rbatis::Rbatis;
 
-#[async_std::test]
-pub async fn test_use_driver_wrapper() {
-    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
-    let rb = Rbatis::new();
-    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    #[async_std::test]
+    pub async fn test_use_driver_wrapper() {
+        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+        let rb = Rbatis::new();
+        rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
 
-    let name = "test";
-    let w = rb.new_wrapper()
-        .push_sql("SELECT count(1) FROM biz_activity WHERE ")
-        .r#in("delete_flag", &[0, 1])
-        .and()
-        .ne("delete_flag", -1)
-        .do_if(!name.is_empty(), |w| w.and().like("name", name))
-        .check().unwrap();
-    let r: serde_json::Value = rb.fetch_prepare_wrapper("", &w).await.unwrap();
-    println!("done:{:?}", r);
+        let name = "test";
+        let w = rb.new_wrapper()
+            .push_sql("SELECT count(1) FROM biz_activity WHERE ")
+            .r#in("delete_flag", &[0, 1])
+            .and()
+            .ne("delete_flag", -1)
+            .do_if(!name.is_empty(), |w| w.and().like("name", name))
+            .check().unwrap();
+        let r: serde_json::Value = rb.fetch_prepare_wrapper("", &w).await.unwrap();
+        println!("done:{:?}", r);
+    }
 }
