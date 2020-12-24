357602c117aa4f0d035d3445d5852905e4d8e767
zhuxiujia <zhuxiujia@qq.com>
2020-12-24 20:24:38 +0800

    add doc test code

---
+++ example/src/page_test.rs
@@ -0,0 +1,50 @@
+use crate::BizActivity;
+use rbatis::rbatis::Rbatis;
+use rbatis::plugin::page::{Page, PageRequest};
+use rbatis::crud::CRUD;
+
+
+#[async_std::test]
+pub async fn test_sql_page() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    let rb = Rbatis::new();
+    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    let wraper = rb.new_wrapper()
+        .eq("delete_flag", 0).check().unwrap();
+    let data: Page<BizActivity> = rb.fetch_page_by_wrapper("", &wraper, &PageRequest::new(1, 20)).await.unwrap();
+    println!("{}", serde_json::to_string(&data).unwrap());
+}
+
+
+#[async_std::test]
+pub async fn test_py_sql_page() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    let rb = Rbatis::new();
+    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    let py = r#"
+    SELECT * FROM biz_activity
+    WHERE delete_flag = #{delete_flag}
+    if name != null:
+      AND name like #{name+'%'}"#;
+    let data: Page<BizActivity> = rb.py_fetch_page("", py, &serde_json::json!({   "delete_flag": 1 }), &PageRequest::new(1, 20)).await.unwrap();
+    println!("{}", serde_json::to_string(&data).unwrap());
+}
+
+
+lazy_static! {
+  static ref RB:Rbatis=Rbatis::new();
+}
+
+#[py_sql(RB, "select * from biz_activity where delete_flag = 0
+                  if name != '':
+                    and name=#{name}")]
+async fn py_select_page(page_req: &PageRequest, name: &str) -> Page<BizActivity> {}
+
+#[async_std::test]
+pub async fn test_macro_py_select_page() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    //use static ref
+    RB.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    let a = py_select_page(&PageRequest::new(1, 10), "test").await.unwrap();
+    println!("{:?}", a);
+}
---
+++ example/src/transaction_test.rs
@@ -0,0 +1,47 @@
+use rbatis::rbatis::Rbatis;
+use crate::BizActivity;
+
+//示例-Rbatis使用事务
+#[async_std::test]
+pub async fn test_tx_commit() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    let rb: Rbatis = Rbatis::new();
+    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    let tx_id = rb.begin_tx().await.unwrap();
+    let v: serde_json::Value = rb.fetch(&tx_id, "SELECT count(1) FROM biz_activity;").await.unwrap();
+    println!("{}", v.clone());
+    rb.commit(&tx_id).await.unwrap();
+}
+
+//tx_id: is context_id
+#[py_sql(rb, "select * from biz_activity")]
+async fn py_select_data(rb: &Rbatis, tx_id: &str) -> Result<Vec<BizActivity>, rbatis::core::Error> {}
+
+//示例-Rbatis使用宏事务
+#[async_std::test]
+pub async fn test_tx_py() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    let rb: Rbatis = Rbatis::new();
+    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+
+    let tx_id = rb.begin_tx().await.unwrap();
+    let v = py_select_data(&rb, &tx_id).await.unwrap();
+    println!("{:?}", v);
+    rb.commit(&tx_id).await.unwrap();
+}
+
+//示例-Rbatis使用事务,类似golang defer，守卫如果被回收则 释放事务
+#[async_std::test]
+pub async fn test_tx_commit_defer() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    let rb: Rbatis = Rbatis::new();
+    rb.link("mysql://root:123456@localhost:3306/test").await.unwrap();
+    forget_commit(&rb).await.unwrap();
+}
+
+pub async fn forget_commit(rb: &Rbatis) -> rbatis::core::Result<serde_json::Value> {
+    // tx will be commit.when func end
+    let guard = rb.begin_tx_defer(true).await?;
+    let v: serde_json::Value = rb.fetch(&guard.tx_id, "SELECT count(1) FROM biz_activity;").await?;
+    return Ok(v);
+}
