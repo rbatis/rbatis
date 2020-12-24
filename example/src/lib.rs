8437d259bdcd749271021c67153d2f48fc0ef541
zhuxiujia <zhuxiujia@qq.com>
2020-12-24 20:58:15 +0800

    add doc

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -9,7 +9,6 @@ extern crate lazy_static;
 #[macro_use]
 extern crate rbatis_macro_driver;
 
-mod model;
 mod crud_test;
 mod un_support_type_test;
 mod raw_driver_test;
@@ -21,4 +20,21 @@ mod macro_test;
 mod raw_identifiers_test;
 mod transaction_test;
 
-use model::BizActivity;
+use chrono::NaiveDateTime;
+/// this is table model(see ../database.sql)
+#[crud_enable]
+#[derive(Clone, Debug)]
+pub struct BizActivity {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub pc_link: Option<String>,
+    pub h5_link: Option<String>,
+    pub pc_banner_img: Option<String>,
+    pub h5_banner_img: Option<String>,
+    pub sort: Option<String>,
+    pub status: Option<i32>,
+    pub remark: Option<String>,
+    pub create_time: Option<NaiveDateTime>,
+    pub version: Option<i32>,
+    pub delete_flag: Option<i32>,
+}
