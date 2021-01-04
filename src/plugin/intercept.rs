2d46cf8803d37a70a163339940bf12b2187cb16f
zhuxiujia <zhuxiujia@qq.com>
2021-01-05 00:07:16 +0800

    add page plugin

--- src/plugin/intercept.rs
+++ src/plugin/intercept.rs
@@ -17,25 +17,3 @@ pub trait SqlIntercept: Send + Sync + Debug {
     /// is_prepared_sql: if is run in prepared_sql=ture
     fn do_intercept(&self, rb: &Rbatis, sql: &mut String, args: &mut Vec<serde_json::Value>, is_prepared_sql: bool) -> Result<(), crate::core::Error>;
 }
-
-
-///dyn_table(old_table,new_table)
-#[derive(Debug, Clone)]
-pub struct RbatisDynTableNameIntercept {}
-
-impl RbatisDynTableNameIntercept {
-    pub fn dyn_table_arg_str(old_table: &str, new_table: &str) -> String {
-        return format!("dyn_table({},{})", old_table, new_table);
-    }
-    pub fn dyn_table_arg<T>(new_table: &str) -> String where T: CRUDEnable {
-        return format!("dyn_table({},{})", T::table_name(), new_table);
-    }
-}
-
-impl SqlIntercept for RbatisDynTableNameIntercept {
-    fn do_intercept(&self, rb: &Rbatis, sql: &mut String, args: &mut Vec<Value>, is_prepared_sql: bool) -> Result<(), Error> {
-        //TODO
-        unimplemented!();
-        //return Ok(());
-    }
-}
