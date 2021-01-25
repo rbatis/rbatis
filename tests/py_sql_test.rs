88f53ec437799f02f1c65cfa50c1e02f76824832
zhuxiujie <zhuxiujia@qq.com>
2021-01-25 14:30:32 +0800

    add bench

--- tests/py_sql_test.rs
+++ tests/py_sql_test.rs
@@ -5,21 +5,29 @@ mod test {
     use rexpr::runtime::RExprRuntime;
     use rbatis::utils::bencher::QPS;
 
+
+    //cargo test --release --package rbatis --test py_sql_test test::test_bench_py_sqsl --no-fail-fast -- --exact -Z unstable-options --show-output
     #[test]
     pub fn test_bench_py_sqsl(){
         let py_runtime=PyRuntime::new(vec![]);
         let engine=RExprRuntime::new();
-        let (sql,arg)=py_runtime.eval(&DriverType::Mysql, "select * from table where
+        let (sql,arg) = py_runtime.eval(&DriverType::Mysql, "select * from table where
                                                                                                 if 1 == 1:
-                                                                                                   column = 1",
-                                       &mut serde_json::json!({}), &engine).unwrap();
+                                                                                                   column = 1
+                                                                                                and age in (1,2,3)
+                                                                                                if 2==2:
+                                                                                                and age = 2",
+                                        &mut serde_json::json!({}), &engine).unwrap();
         println!("sql:{},arg:{:?}",sql,arg);
         let total=10000;
         let now=std::time::Instant::now();
         for _ in 0..total{
             let (sql,arg) = py_runtime.eval(&DriverType::Mysql, "select * from table where
                                                                                                 if 1 == 1:
-                                                                                                   column = 1",
+                                                                                                   column = 1
+                                                                                                and age in (1,2,3)
+                                                                                                if 2==2:
+                                                                                                and age = 2",
                                           &mut serde_json::json!({}), &engine).unwrap();
         }
         now.time(total);
