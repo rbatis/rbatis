0b972a59a1f44f43464033c88d841e96762768cc
zhuxiujie <zhuxiujia@qq.com>
2021-02-15 20:40:07 +0800

    clear doc

--- example/src/hyper/main.rs
+++ example/src/hyper/main.rs
@@ -43,12 +43,7 @@ async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
     RB.link(MYSQL_URL).await.unwrap();
-    // For every connection, we must make a `Service` to handle all
-    // incoming HTTP requests on said connection.
     let make_svc = make_service_fn(|_conn| {
-        // This is the `Service` that will handle the connection.
-        // `service_fn` is a helper to convert a function that
-        // returns a Response into a `Service`.
         async { Ok::<_, Infallible>(service_fn(hello)) }
     });
     let addr = ([127, 0, 0, 1], 8000).into();
