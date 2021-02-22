0c6df914df44f7975d25a1e00abf1c9b8683451b
zxj_pc <zhuxiujia@qq.com>
2021-02-22 21:14:00 +0800

    add plugin_exclude test

--- example/src/plugin_exclude_test.rs
+++ example/src/plugin_exclude_test.rs
@@ -13,8 +13,8 @@ mod test {
         fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
         let mut rb = Rbatis::new();
         let mut plugin=RbatisLogicDeletePlugin::new("delete_flag");
-        plugin.excludes.push("exclude:".to_string());
-        plugin.excludes.push("tx:exclude:".to_string());
+        plugin.excludes.push("disable_del:".to_string());
+        plugin.excludes.push("tx:disable_del:".to_string());
         rb.set_logic_plugin(Some(plugin));
         rb.link("mysql://root:123456@localhost:3306/test")
             .await
@@ -24,7 +24,7 @@ mod test {
         //logic delete sql:   "UPDATE biz_activity SET delete_flag = 1 WHERE id = ?"
         rb.remove_by_id::<BizActivity>("", &id).await;
         //delete sql          "DELETE FROM biz_activity WHERE id = ?"
-        rb.remove_by_id::<BizActivity>("exclude:", &id).await;
+        rb.remove_by_id::<BizActivity>("disable_del:", &id).await;
 
 
         //fix data
