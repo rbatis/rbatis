f2c670760b9c5297d8556168d59161e845a084b7
zxj_pc <zhuxiujia@qq.com>
2021-02-22 21:40:04 +0800

    fix logic plugin

--- example/src/plugin_exclude_test.rs
+++ example/src/plugin_exclude_test.rs
@@ -7,7 +7,7 @@ mod test {
     use chrono::NaiveDateTime;
     use rbatis::core::value::DateTimeNow;
     #[async_std::test]
-    async fn plugin_exclude(){
+    async fn plugin_exclude_delete(){
         fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
         let mut rb = Rbatis::new();
         let mut plugin=RbatisLogicDeletePlugin::new("delete_flag");
@@ -40,5 +40,26 @@ mod test {
             version: Some(0),
             delete_flag: Some(1),
         }).await;
+    }
+
+
+    #[async_std::test]
+    async fn plugin_exclude_select(){
+        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+        let mut rb = Rbatis::new();
+        let mut plugin=RbatisLogicDeletePlugin::new("delete_flag");
+        plugin.excludes.push("disable_del:".to_string());
+        plugin.excludes.push("tx:disable_del:".to_string());
+        rb.set_logic_plugin(Some(plugin));
+        rb.link("mysql://root:123456@localhost:3306/test")
+            .await
+            .unwrap();
+
+        let id="12312".to_string();
+        //logic delete sql:   "SELECT * FROM biz_activity  WHERE delete_flag = 0 AND id = ? "
+        rb.fetch_by_id::<BizActivity>("", &id).await;
+        //delete sql          "SELECT * FROM biz_activity  WHERE id = ? "
+        rb.fetch_by_id::<BizActivity>("disable_del:", &id).await;
+
     }
 }
