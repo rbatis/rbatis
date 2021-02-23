da50354b370c4d1d1a850f66b59a41c32280fd49
zxj_pc <zhuxiujia@qq.com>
2021-02-24 00:08:54 +0800

    fix test

--- tests/logic_delete_test.rs
+++ tests/logic_delete_test.rs
@@ -9,26 +9,26 @@ mod tests {
         let table_fields = "name,age,del".to_string();
         let sql_where = "";
         let result = r
-            .create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where)
+            .create_remove_sql("",&DriverType::Mysql, "test", &table_fields, sql_where)
             .unwrap();
         assert_eq!("UPDATE test SET del = 1", &result);
 
         let sql_where = " WHERE name = 'zhangsan'";
         let result = r
-            .create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where)
+            .create_remove_sql("",&DriverType::Mysql, "test", &table_fields, sql_where)
             .unwrap();
         assert_eq!("UPDATE test SET del = 1 WHERE name = 'zhangsan'", &result);
 
         let table_fields = "name,age";
         let sql_where = " WHERE name = 'zhangsan'";
         let result = r
-            .create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where)
+            .create_remove_sql("",&DriverType::Mysql, "test", &table_fields, sql_where)
             .unwrap();
         assert_eq!("DELETE FROM test WHERE name = 'zhangsan'", &result);
 
         let table_fields = "name,age";
         let sql_where = "";
-        let result = r.create_remove_sql(&DriverType::Mysql, "test", &table_fields, sql_where);
+        let result = r.create_remove_sql("",&DriverType::Mysql, "test", &table_fields, sql_where);
         assert!(result.is_err());
     }
 }
