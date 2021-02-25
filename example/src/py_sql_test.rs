38a899beb0aa2c46f4011fe3de417949a732a590
zhuxiujie <zhuxiujia@qq.com>
2021-02-25 10:36:59 +0800

    add load file

--- example/src/py_sql_test.rs
+++ example/src/py_sql_test.rs
@@ -3,6 +3,8 @@ mod test {
     use crate::BizActivity;
     use rbatis::plugin::page::{Page, PageRequest};
     use rbatis::rbatis::Rbatis;
+    use std::fs::File;
+    use std::io::Read;
 
     /// ctx_id Used to trace SQL records
     #[py_sql(rb, "select * from biz_activity where delete_flag = 0")]
@@ -63,6 +65,33 @@ mod test {
         //guard will be Automatically commit or rollback transactions
         let tx_guard = rb.begin_tx_defer(true).await.unwrap();
         let a = py_sql_tx(&rb, &tx_guard.tx_id, "test").await.unwrap();
+        println!("{:?}", a);
+    }
+
+
+    ///load from file
+    fn load_file_str(file_name:&str)->String{
+        let mut f =File::open(file_name).unwrap();
+        let mut s=String::new();
+        f.read_to_string(&mut s);
+        return s;
+    }
+
+    ///load file py_sql
+    #[py_sql(rb, load_file_str("py_sql.sql"))]
+    async fn py_select_file(rb: &Rbatis, page_req: &PageRequest, name: &str) -> Page<BizActivity> {}
+
+    #[async_std::test]
+    pub async fn test_py_select_file() {
+        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+        //use static ref
+        let rb = Rbatis::new();
+        rb.link("mysql://root:123456@localhost:3306/test")
+            .await
+            .unwrap();
+        let a = py_select_file(&rb, &PageRequest::new(1, 10), "test")
+            .await
+            .unwrap();
         println!("{:?}", a);
     }
 }
