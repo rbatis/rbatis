b084e51e25a0a37422d1224c5c82d8b566d80eb9
zxj_pc <zhuxiujia@qq.com>
2021-03-01 21:18:04 +0800

    add bench

--- tests/snowflake_test.rs
+++ tests/snowflake_test.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod test {
     use rbatis::plugin::snowflake::new_snowflake_id;
-    use rbatis::utils::bencher::bench;
     #[test]
     fn test_new_block_id() {
         println!("{}", new_snowflake_id());
