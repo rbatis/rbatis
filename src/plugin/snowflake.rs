f5c4a266b6a610982aa68bd0800f624b2052e216
zhuxiujie <zhuxiujia@qq.com>
2021-03-01 14:00:58 +0800

    snowflake Lock-free impl

--- src/plugin/snowflake.rs
+++ src/plugin/snowflake.rs
@@ -1,28 +1,80 @@
-use std::sync::{Mutex, MutexGuard, TryLockError};
-use std::time::{Instant, SystemTime};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicI64, Ordering};
 
-use rustflake::Snowflake;
+use chrono::Utc;
 
-use crate::core::runtime::sync::Mutex as AsyncMutex;
-
-lazy_static! {
-    pub static ref SNOWFLAKE: Mutex<Snowflake> = Mutex::new(Snowflake::default());
-    pub static ref ASYNC_SNOWFLAKE: AsyncMutex<Snowflake> = AsyncMutex::new(Snowflake::default());
+pub struct Snowflake {
+    epoch: i64,
+    worker_id: i64,
+    datacenter_id: i64,
+    sequence: AtomicI64,
+    time: AtomicI64,
 }
 
-/// return an snowflake id,this method use async Mutex
-pub async fn async_snowflake_id() -> i64 {
-    ASYNC_SNOWFLAKE.lock().await.generate()
+impl Snowflake {
+    pub fn default() -> Snowflake {
+        Snowflake {
+            epoch: 1_564_790_400_000,
+            worker_id: 1,
+            datacenter_id: 1,
+            sequence: AtomicI64::new(0),
+            time: AtomicI64::new(0),
+        }
+    }
+
+    pub fn new(epoch: i64, worker_id: i64, datacenter_id: i64) -> Snowflake {
+        Snowflake {
+            epoch,
+            worker_id,
+            datacenter_id,
+            sequence: AtomicI64::new(0),
+            time: AtomicI64::new(0),
+        }
+    }
+
+    pub fn epoch(&mut self, epoch: i64) -> &mut Self {
+        self.epoch = epoch;
+        self
+    }
+
+    pub fn worker_id(&mut self, worker_id: i64) -> &mut Self {
+        self.worker_id = worker_id;
+        self
+    }
+
+    pub fn datacenter_id(&mut self, datacenter_id: i64) -> &mut Self {
+        self.datacenter_id = datacenter_id;
+        self
+    }
+
+    pub fn generate(&self) -> i64 {
+        let last_timestamp = self.time.fetch_or(0, Ordering::Relaxed);
+        let mut timestamp = self.get_time();
+        let sequence = self.sequence.fetch_or(0, Ordering::Relaxed);
+        if timestamp == last_timestamp {
+            let sequence = (sequence + 1) & (-1 ^ (-1 << 12));
+            self.sequence.swap(sequence, Ordering::Relaxed);
+            if sequence == 0 && timestamp <= last_timestamp {
+                timestamp = self.get_time();
+            }
+        } else {
+            self.sequence.swap(0, Ordering::Relaxed);
+        }
+        self.time.swap(timestamp, Ordering::Relaxed);
+        (timestamp << 22) | (self.worker_id << 17) | (self.datacenter_id << 12) | self.sequence.fetch_or(0, Ordering::Relaxed)
+    }
+
+    fn get_time(&self) -> i64 {
+        Utc::now().timestamp_millis() - self.epoch
+    }
+}
+
+
+lazy_static! {
+    pub static ref SNOWFLAKE: Snowflake = Snowflake::default();
 }
 
 ///if lock fail,will return -1
-pub fn block_snowflake_id() -> i64 {
-    match SNOWFLAKE.lock() {
-        Ok(mut v) => {
-            return v.generate();
-        }
-        _ => {
-            return -1;
-        }
-    }
+pub fn new_snowflake_id() -> i64 {
+    SNOWFLAKE.generate()
 }
--- tests/snowflake_test.rs
+++ tests/snowflake_test.rs
@@ -1,18 +1,10 @@
 #[cfg(test)]
 mod test {
-    use rbatis::plugin::snowflake::{async_snowflake_id, block_snowflake_id};
+    use rbatis::plugin::snowflake::new_snowflake_id;
 
     #[test]
     fn test_new_block_id() {
-        println!("{}", block_snowflake_id());
-        println!("{}", block_snowflake_id());
-    }
-
-    #[test]
-    fn test_new_async_id() {
-        rbatis::core::runtime::block_on(async {
-            println!("{}", async_snowflake_id().await);
-            println!("{}", async_snowflake_id().await);
-        });
+        println!("{}", new_snowflake_id());
+        println!("{}", new_snowflake_id());
     }
 }
