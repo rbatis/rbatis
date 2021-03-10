63c6bfa8cb7ff246d3aec9b92906f489dee4d02a
zxj_pc <zhuxiujia@qq.com>
2021-03-10 20:36:27 +0800

    add plugin

--- src/plugin/intercept.rs
+++ src/plugin/intercept.rs
@@ -1,11 +1,10 @@
 use serde_json::Value;
 use std::fmt::{Debug, Display};
-
 use rbatis_core::Error;
-
 use crate::core::convert::StmtConvert;
 use crate::crud::CRUDTable;
 use crate::rbatis::Rbatis;
+use crate::DriverType;
 
 /// sql intercept
 pub trait SqlIntercept: Send + Sync + Debug {
@@ -23,4 +22,24 @@ pub trait SqlIntercept: Send + Sync + Debug {
         args: &mut Vec<serde_json::Value>,
         is_prepared_sql: bool,
     ) -> Result<(), crate::core::Error>;
+}
+
+#[derive(Debug)]
+pub struct RbatisLogFormatSqlIntercept {}
+
+impl SqlIntercept for RbatisLogFormatSqlIntercept {
+    fn do_intercept(&self, rb: &Rbatis, context_id: &str, sql: &mut String, args: &mut Vec<Value>, is_prepared_sql: bool) -> Result<(), Error> {
+        let driver_type = rb.driver_type()?;
+        match driver_type {
+            DriverType::None => {}
+            DriverType::Mysql | DriverType::Postgres | DriverType::Sqlite | DriverType::Mssql => {
+                let mut formated = format!("[format_sql]{}",sql);
+                for index in 0..args.len() {
+                    formated = formated.replacen(&driver_type.stmt_convert(index), &format!("{}", args.get(index).unwrap()),1);
+                }
+                rb.log_plugin.info(context_id, &formated);
+            }
+        }
+        return Ok(());
+    }
 }
