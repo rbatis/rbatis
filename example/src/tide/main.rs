1cc0c35371d9edb92cd1e0fe150efc21c097a1ef
zxj_pc <zhuxiujia@qq.com>
2021-03-12 23:07:38 +0800

    add tide

---
+++ example/src/tide/main.rs
@@ -0,0 +1,48 @@
+#![allow(unused_must_use)]
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate rbatis;
+
+use chrono::NaiveDateTime;
+use rbatis::crud::CRUD;
+use rbatis::rbatis::Rbatis;
+use serde_json::{json, Value};
+use tide::Request;
+
+#[crud_enable]
+#[derive(Clone, Debug)]
+pub struct BizActivity {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub pc_link: Option<String>,
+    pub h5_link: Option<String>,
+    pub pc_banner_img: Option<String>,
+    pub h5_banner_img: Option<String>,
+    pub sort: Option<String>,
+    pub status: Option<i32>,
+    pub remark: Option<String>,
+    pub create_time: Option<NaiveDateTime>,
+    pub version: Option<i32>,
+    pub delete_flag: Option<i32>,
+}
+
+pub const MYSQL_URL: &'static str = "mysql://root:123456@localhost:3306/test";
+
+lazy_static! {
+    static ref RB: Rbatis = Rbatis::new();
+}
+
+#[async_std::main]
+async fn main() {
+    fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    RB.link(MYSQL_URL).await.unwrap();
+    let mut app = tide::new();
+    app.at("/").get(|_: Request<()>| async move {
+        let v = RB.fetch_list::<BizActivity>("").await;
+        Ok(serde_json::json!(v).to_string())
+    });
+    let addr = "127.0.0.1:8000";
+    println!("http server listen on http://{}", addr);
+    app.listen(addr).await.unwrap();
+}
