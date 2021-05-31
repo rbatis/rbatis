b2fb816cb9007d44dc2d050a9d0e52c6181212f7
zxj_pc <zhuxiujia@qq.com>
2021-06-01 00:55:52 +0800

    move package

--- src/executor.rs
+++ src/executor.rs
@@ -8,6 +8,7 @@ use crate::core::Error;
 use crate::DriverType;
 use crate::rbatis::Rbatis;
 use std::ops::Deref;
+use crate::utils::string_util;
 
 #[async_trait]
 pub trait Executor {
@@ -35,8 +36,10 @@ pub struct RBatisConnExecutor<'a> {
     pub rb: &'a Rbatis,
 }
 
+macro_rules! impl_executor {
+    ($t:ty) => {
 #[async_trait]
-impl<'a> Executor for RBatisConnExecutor<'a> {
+impl<'a> Executor for $t {
     async fn execute(&mut self, sql: &str, args: &Vec<serde_json::Value>) -> Result<DBExecResult, Error> {
         let mut sql = sql.to_string();
         let mut args = args.clone();
@@ -44,14 +47,40 @@ impl<'a> Executor for RBatisConnExecutor<'a> {
         for item in &self.sql_intercepts {
             item.do_intercept(self, &mut sql, &mut args, is_prepared)?;
         }
+        if self.log_plugin.is_enable() {
+            self.log_plugin.info(
+                "",
+                &format!(
+                    "Exec  ==> {}\n{}[rbatis] [{}] Args  ==> {}",
+                    &sql,
+                    string_util::LOG_SPACE,
+                    "",
+                    serde_json::Value::Array(args.clone()).to_string()
+                ),
+            );
+        }
+        let result;
         if is_prepared {
             let q: DBQuery = self.bind_arg(&self.conn.driver_type, &sql, &args)?;
-            let result = self.conn.exec_prepare(q).await;
-            return result;
+            result = self.conn.exec_prepare(q).await;
         } else {
-            let result = self.conn.execute(&sql).await;
-            return result;
+            result = self.conn.execute(&sql).await;
         }
+        if self.log_plugin.is_enable() {
+            match &result {
+                Ok(result) => {
+                    self.log_plugin.info(
+                        "",
+                        &format!("RowsAffected <== {}", result.rows_affected),
+                    );
+                }
+                Err(e) => {
+                    self.log_plugin
+                        .error("", &format!("ReturnErr  <== {}", e));
+                }
+            }
+        }
+        return result;
     }
 
     async fn fetch<T>(&mut self, sql: &str, args: &Vec<serde_json::Value>) -> Result<T, Error> where T: DeserializeOwned {
@@ -61,16 +90,57 @@ impl<'a> Executor for RBatisConnExecutor<'a> {
         for item in &self.sql_intercepts {
             item.do_intercept(self, &mut sql, &mut args, is_prepared)?;
         }
+        if self.log_plugin.is_enable() {
+            self.log_plugin.info(
+                "",
+                &format!(
+                    "Exec  ==> {}\n{}[rbatis] [{}] Args  ==> {}",
+                    &sql,
+                    string_util::LOG_SPACE,
+                    "",
+                    serde_json::Value::Array(args.clone()).to_string()
+                ),
+            );
+        }
         if is_prepared {
             let q: DBQuery = self.bind_arg(&self.conn.driver_type, &sql, &args)?;
-            let result: (T, usize) = self.conn.fetch_parperd(q).await?;
-            return Ok(result.0);
+            let result = self.conn.fetch_parperd(q).await;
+            if self.log_plugin.is_enable() {
+                match &result {
+                    Ok(result) => {
+                        self.log_plugin
+                            .info("", &format!("ReturnRows <== {}", result.1));
+                    }
+                    Err(e) => {
+                        self.log_plugin
+                            .error("", &format!("ReturnErr  <== {}", e));
+                    }
+                }
+            }
+            return Ok(result?.0);
         } else {
-            let result: (T, usize) = self.conn.fetch(&sql).await?;
-            return Ok(result.0);
+            let result = self.conn.fetch(&sql.to_owned()).await;
+            if self.log_plugin.is_enable() {
+                match &result {
+                    Ok(result) => {
+                        self.log_plugin
+                            .info("", &format!("ReturnRows <== {}", result.1));
+                    }
+                    Err(e) => {
+                        self.log_plugin
+                            .error("", &format!("ReturnErr  <== {}", e));
+                    }
+                }
+            }
+            return Ok(result?.0);
         }
     }
 }
+    };
+}
+
+impl_executor!(RBatisConnExecutor<'a>);
+
 
 #[derive(Debug)]
 pub struct RBatisTxExecutor<'a> {
@@ -78,42 +148,7 @@ pub struct RBatisTxExecutor<'a> {
     pub rb: &'a Rbatis,
 }
 
-#[async_trait]
-impl<'a> Executor for RBatisTxExecutor<'a> {
-    async fn execute(&mut self, sql: &str, args: &Vec<serde_json::Value>) -> Result<DBExecResult, Error> {
-        let mut sql = sql.to_string();
-        let mut args = args.clone();
-        let is_prepared = args.len() > 0;
-        for item in &self.sql_intercepts {
-            item.do_intercept(self, &mut sql, &mut args, is_prepared)?;
-        }
-        if is_prepared {
-            let q: DBQuery = self.bind_arg(&self.conn.driver_type, &sql, &args)?;
-            let result = self.conn.exec_prepare(q).await;
-            return result;
-        } else {
-            let result = self.conn.execute(&sql).await;
-            return result;
-        }
-    }
-
-    async fn fetch<T>(&mut self, sql: &str, args: &Vec<serde_json::Value>) -> Result<T, Error> where T: DeserializeOwned {
-        let mut sql = sql.to_string();
-        let mut args = args.clone();
-        let is_prepared = args.len() > 0;
-        for item in &self.sql_intercepts {
-            item.do_intercept(self, &mut sql, &mut args, is_prepared)?;
-        }
-        if is_prepared {
-            let q: DBQuery = self.bind_arg(&self.conn.driver_type, &sql, &args)?;
-            let result: (T, usize) = self.conn.fetch_parperd(q).await?;
-            return Ok(result.0);
-        } else {
-            let result: (T, usize) = self.conn.fetch(&sql).await?;
-            return Ok(result.0);
-        }
-    }
-}
+impl_executor!(RBatisTxExecutor<'a>);
 
 
 /// impl Deref has all the capabilities of RBatis
