fda900c9b79f3c8a9d74a30d174be023769c7cd9
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-06-19 23:32:22 +0800

    add as_executor

--- example/src/html_sql_test.rs
+++ example/src/html_sql_test.rs
@@ -13,22 +13,6 @@ mod test {
     #[html_sql(rb, "example/example.html")]
     async fn select_by_condition(rb: &mut RbatisExecutor<'_>, page_req: &PageRequest, name: &str) -> Page<BizActivity> { todo!() }
 
-
-    // #[html_sql(self, "example/example.html")]
-    // #[async_trait]
-    // pub trait Example{
-    //     #[html_sql(rb, "example/example.html")]
-    //     async fn select_by_condition(&self, page_req: &PageRequest, name: &str) -> Page<BizActivity>;
-    // }
-    //
-    // #[async_trait]
-    // impl Example for RbatisExecutor<'_>{
-    //     async fn select_by_condition(page_req: &PageRequest, name: &str) -> Page<BizActivity> {
-    //         todo!()
-    //     }
-    // }
-
-
     #[tokio::test]
     pub async fn test_py_select_page() {
         fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
