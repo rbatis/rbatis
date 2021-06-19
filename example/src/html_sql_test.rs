03ace01cdb56dec5b647b184be5d496b7f087759
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-06-19 23:00:00 +0800

    add as_executor

--- example/src/html_sql_test.rs
+++ example/src/html_sql_test.rs
@@ -13,6 +13,22 @@ mod test {
     #[html_sql(rb, "example/example.html")]
     async fn select_by_condition(rb: &mut RbatisExecutor<'_>, page_req: &PageRequest, name: &str) -> Page<BizActivity> { todo!() }
 
+
+    // #[html_sql(self, "example/example.html")]
+    // #[async_trait]
+    // pub trait Example{
+    //     #[html_sql(rb, "example/example.html")]
+    //     async fn select_by_condition(&self, page_req: &PageRequest, name: &str) -> Page<BizActivity>;
+    // }
+    //
+    // #[async_trait]
+    // impl Example for RbatisExecutor<'_>{
+    //     async fn select_by_condition(page_req: &PageRequest, name: &str) -> Page<BizActivity> {
+    //         todo!()
+    //     }
+    // }
+
+
     #[tokio::test]
     pub async fn test_py_select_page() {
         fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
@@ -21,7 +37,7 @@ mod test {
         rb.link("mysql://root:123456@localhost:3306/test")
             .await
             .unwrap();
-        let a = select_by_condition(&mut (&rb).into(), &PageRequest::new(1, 10), "test")
+        let a = select_by_condition(&mut rb.as_executor(), &PageRequest::new(1, 10), "test")
             .await
             .unwrap();
         println!("{:?}", a);
