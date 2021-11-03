bb76e09c67734a10ed38a35703c52d920f15e885
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-11-03 11:19:51 +0800

    add test

--- example/src/improve_code_maintainability.rs
+++ example/src/improve_code_maintainability.rs
@@ -21,6 +21,8 @@ mod test {
             .eq("name", "xiao ming")
             .eq("delete_flag",1);
 
+        assert_eq!(w.sql,"id = ? and name = ? and delete_flag = ?");
+
         // so we change "name" to column_name!(BizActivity::name),
         // when field name change to user_name ,it will be Compilation fails
         let w = Wrapper::new(&DriverType::Mysql)
@@ -28,9 +30,13 @@ mod test {
             .eq(field_name!(BizActivity.name), "xiao ming")
             .eq(field_name!(BizActivity.delete_flag),1);
 
+        assert_eq!(w.sql,"id = ? and name = ? and delete_flag = ?");
+
         let w = Wrapper::new(&DriverType::Mysql)
             .eq(BizActivity::id(),"1")
             .eq(BizActivity::name(), "xiao ming")
             .eq(BizActivity::delete_flag(),1);
+
+        assert_eq!(w.sql,"id = ? and name = ? and delete_flag = ?");
     }
 }
