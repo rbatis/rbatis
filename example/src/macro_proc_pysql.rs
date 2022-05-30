3998d41455850f11a0dd5584e5735ab2d598a86d
zxj <zhuxiujia@qq.com>
2022-05-30 10:56:37 +0800

    support mut rb: RbatisExecutor<'_, '_>

--- example/src/macro_proc_pysql.rs
+++ example/src/macro_proc_pysql.rs
@@ -25,14 +25,14 @@ mod test {
     #[py_sql("select * from biz_activity where delete_flag = 0
                   if name != '':
                     and name=#{name}")]
-    async fn py_select_page(rb: &mut RbatisExecutor<'_, '_>, page_req: &PageRequest, name: &str) -> Page<BizActivity> { impled!() }
+    async fn py_select_page(mut rb: RbatisExecutor<'_, '_>, page_req: &PageRequest, name: &str) -> Page<BizActivity> { impled!() }
 
     #[tokio::test]
     pub async fn test_py_select_page() {
         fast_log::init(fast_log::config::Config::new().console());
         //use static ref
         let rb = init_sqlite().await;
-        let a = py_select_page(&mut rb.as_executor(), &PageRequest::new(1, 10), "test")
+        let a = py_select_page( rb.as_executor(), &PageRequest::new(1, 10), "test")
             .await
             .unwrap();
         println!("{:?}", a);
