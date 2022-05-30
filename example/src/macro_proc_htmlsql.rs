bf28de16cf7c712c8e8c35a27d5fef7a54f6b0f1
zxj <zhuxiujia@qq.com>
2022-05-30 10:53:48 +0800

    support mut rb: RbatisExecutor<'_, '_>

--- example/src/macro_proc_htmlsql.rs
+++ example/src/macro_proc_htmlsql.rs
@@ -11,14 +11,14 @@ mod test {
 
     ///select page must have  '?:&PageRequest' arg and return 'Page<?>'
     #[html_sql("example/example.html")]
-    async fn select_by_condition(rb: &mut RbatisExecutor<'_, '_>, page_req: &PageRequest, name: &str, dt: &rbatis::DateTimeNative) -> Page<BizActivity> { impled!() }
+    async fn select_by_condition(mut rb: RbatisExecutor<'_, '_>, page_req: &PageRequest, name: &str, dt: &rbatis::DateTimeNative) -> Page<BizActivity> { impled!() }
 
     #[tokio::test]
     pub async fn test_select_by_condition() {
         fast_log::init(fast_log::config::Config::new().console());
         //use static ref
         let rb = init_sqlite().await;
-        let a = select_by_condition( &mut rb.as_executor(), &PageRequest::new(1, 10), "test", &rbatis::DateTimeNative::now())
+        let a = select_by_condition(  rb.as_executor(), &PageRequest::new(1, 10), "test", &rbatis::DateTimeNative::now())
             .await
             .unwrap();
         println!("{:?}", a);
