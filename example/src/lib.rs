13f29d202c6b219118cc3d4005c923e8518184a8
zhuxiujia <zhuxiujia@qq.com>
2022-06-24 19:43:33 +0800

    add doc

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -62,12 +62,6 @@ pub async fn init_sqlite_path(path:&str) -> Rbatis {
     //first init log carte
     fast_log::init(fast_log::config::Config::new().console());
 
-    //create sqlite file
-    if File::open(format!("{}target/sqlite.db", path)).is_err() {
-        create_dir_all(format!("{}target/", path));
-        let f = File::create(format!("{}target/sqlite.db", path)).unwrap();
-        drop(f);
-    }
     // // mysql custom connection option
     // // let db_cfg=DBConnectOption::from("mysql://root:123456@localhost:3306/test")?;
     // let db_cfg= DBConnectOption::from("sqlite://../target/sqlite.db")?;
@@ -78,8 +72,15 @@ pub async fn init_sqlite_path(path:&str) -> Rbatis {
     // opt.max_size = 20;
     // rb.link_opt("sqlite://../target/sqlite.db", &opt).await.unwrap();
 
+
     // init rbatis
     let rb = Rbatis::new();
+    //create sqlite file
+    if File::open(format!("{}target/sqlite.db", path)).is_err() {
+        create_dir_all(format!("{}target/", path));
+        let f = File::create(format!("{}target/sqlite.db", path)).unwrap();
+        drop(f);
+    }
     rb.link(&format!("sqlite://{}target/sqlite.db", path))
         .await
         .unwrap();
