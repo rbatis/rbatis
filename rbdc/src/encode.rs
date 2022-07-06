9569906e40e4058a4c44f8c7871348ab5dbad1e9
zxj <zhuxiujia@qq.com>
2022-07-06 16:22:15 +0800

    add ser enc

--- rbdc/src/encode.rs
+++ rbdc/src/encode.rs
@@ -1,19 +1,15 @@
 use std::mem;
 
 /// Encode a single value to be sent to the database.
-pub trait Encode{
-
-    /// Writes the value of `self` into `buf` without moving `self`.
-    ///
-    /// Where possible, make use of `encode` instead as it can take advantage of re-using
-    /// memory.
-    #[must_use]
-    fn encode(&self, buf: &mut rbson::Bson);
-
-
-
-    #[inline]
-    fn size_hint(&self) -> usize {
-        mem::size_of_val(self)
-    }
+pub trait Encode {
+    fn encode_i32(&self,v: Option<i32>) -> Vec<u8>;
+    fn encode_i64(&self,v: Option<i64>) -> Vec<u8>;
+    fn encode_f32(&self,v: Option<f32>) -> Vec<u8>;
+    fn encode_f64(&self,v: Option<f64>) -> Vec<u8>;
+    fn encode_u32(&self,v: Option<u32>) -> Vec<u8>;
+    fn encode_u64(&self,v: Option<u64>) -> Vec<u8>;
+    fn encode_str(&self,v: Option<&str>) -> Vec<u8>;
+    fn encode_bool(&self,v: Option<bool>) -> Vec<u8>;
+    fn encode_bin(&self,v: Option<&[u8]>) -> Vec<u8>;
+    fn encode_null(&self) -> Vec<u8>;
 }
