d9bac8f9fc19317a901737a917d0ce25c8889021
zxj <zhuxiujia@qq.com>
2022-07-08 10:36:15 +0800

    struct ser to map

--- rbdc/src/lib.rs
+++ rbdc/src/lib.rs
@@ -7,18 +7,20 @@ pub mod db;
 
 
 #[cfg(test)]
-mod test{
+mod test {
     #[test]
-    fn test_ser(){
-        let buf = rbmp_serde::to_vec(&(42, "the Answer")).unwrap();
+    fn test_ser() {
+        #[derive(serde::Serialize, serde::Deserialize, Debug)]
+        pub struct A {
+            pub name: String,
+        }
+        let buf = rbmp_serde::to_vec(&A {
+            name: "s".to_string()
+        }).unwrap();
+        let v: rbmpv::Value = rbmpv::decode::read_value(&mut &buf[..]).unwrap();
+        println!("{}", v);
 
-        assert_eq!(
-            vec![0x92, 0x2a, 0xaa, 0x74, 0x68, 0x65, 0x20, 0x41, 0x6e, 0x73, 0x77, 0x65, 0x72],
-            buf
-        );
-
-        let v:rbmpv::Value = rbmpv::decode::read_value(&mut &buf[..]).unwrap();
-        println!("{}",v);
-        // assert_eq!((42, "the Answer"), rmp_serde::from_slice(&buf).unwrap());
+        let v: A = rbmp_serde::decode::from_slice(&buf).unwrap();
+        println!("{:?}", v);
     }
 }
