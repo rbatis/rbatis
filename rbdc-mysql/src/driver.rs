aae9dd836533455fb7da7b9863c15e6bfb7e6f03
zhuxiujia <zhuxiujia@qq.com>
2022-07-17 03:22:37 +0800

    impl mysql encode

--- rbdc-mysql/src/driver.rs
+++ rbdc-mysql/src/driver.rs
@@ -30,67 +30,67 @@ impl Driver for MysqlDriver {
 
 #[cfg(test)]
 mod test {
-    use crate::driver::MysqlDriver;
-    use rbdc::db::Driver;
-    use rbs::Value;
-    use std::collections::BTreeMap;
-
-    #[tokio::test]
-    async fn test_mysql_rows() {
-        let mut d = MysqlDriver {};
-        let mut c = d
-            .connect("mysql://root:123456@localhost:3306/test")
-            .await
-            .unwrap();
-        let data = c
-            .get_values("select * from biz_activity", vec![])
-            .await
-            .unwrap();
-        for mut x in data {
-            println!("row: {}", x);
-        }
-    }
-
-    #[tokio::test]
-    async fn test_mysql_count() {
-        let mut d = MysqlDriver {};
-        let mut c = d
-            .connect("mysql://root:123456@localhost:3306/test")
-            .await
-            .unwrap();
-        let data = c
-            .exec(
-                "update biz_activity set pc_link = '111' where id  = '1'",
-                vec![],
-            )
-            .await
-            .unwrap();
-        println!("{}", data);
-    }
-
-    #[tokio::test]
-    async fn test_mysql_param() {
-        let mut d = MysqlDriver {};
-        let mut c = d
-            .connect("mysql://root:123456@localhost:3306/test")
-            .await
-            .unwrap();
-        let param = vec![
-            Value::String("http://www.test.com".to_string()),
-            Value::Map(vec![(
-                Value::String("datetime".to_string()),
-                Value::String("2022-08-08T22:39:11.666667".to_string()),
-            )]),
-            Value::String("12312".to_string()),
-        ];
-        println!("param => {}", Value::Array(param.clone()));
-        let data = c
-            .exec(
-                "update biz_activity set pc_link = ?,create_time = ? where id  = ?",
-                param,
-            )
-            .await
-            .unwrap();
-        println!("{}", data);
-    }
+    // use crate::driver::MysqlDriver;
+    // use rbdc::db::Driver;
+    // use rbs::Value;
+    // use std::collections::BTreeMap;
+    //
+    // #[tokio::test]
+    // async fn test_mysql_rows() {
+    //     let mut d = MysqlDriver {};
+    //     let mut c = d
+    //         .connect("mysql://root:123456@localhost:3306/test")
+    //         .await
+    //         .unwrap();
+    //     let data = c
+    //         .get_values("select * from biz_activity", vec![])
+    //         .await
+    //         .unwrap();
+    //     for mut x in data {
+    //         println!("row: {}", x);
+    //     }
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_mysql_count() {
+    //     let mut d = MysqlDriver {};
+    //     let mut c = d
+    //         .connect("mysql://root:123456@localhost:3306/test")
+    //         .await
+    //         .unwrap();
+    //     let data = c
+    //         .exec(
+    //             "update biz_activity set pc_link = '111' where id  = '1'",
+    //             vec![],
+    //         )
+    //         .await
+    //         .unwrap();
+    //     println!("{}", data);
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_mysql_param() {
+    //     let mut d = MysqlDriver {};
+    //     let mut c = d
+    //         .connect("mysql://root:123456@localhost:3306/test")
+    //         .await
+    //         .unwrap();
+    //     let param = vec![
+    //         Value::String("http://www.test.com".to_string()),
+    //         Value::Map(vec![(
+    //             Value::String("datetime".to_string()),
+    //             Value::String("2022-08-08T22:39:11.666667".to_string()),
+    //         )]),
+    //         Value::String("12312".to_string()),
+    //     ];
+    //     println!("param => {}", Value::Array(param.clone()));
+    //     let data = c
+    //         .exec(
+    //             "update biz_activity set pc_link = ?,create_time = ? where id  = ?",
+    //             param,
+    //         )
+    //         .await
+    //         .unwrap();
+    //     println!("{}", data);
+    // }
 }
