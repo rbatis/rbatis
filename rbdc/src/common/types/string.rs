b0ca4305b17a2370aeaa1a17b413e4f8b3a0b9c0
zhuxiujia <zhuxiujia@qq.com>
2022-07-20 19:45:43 +0800

    impl uuid

--- rbdc/src/common/types/string.rs
+++ rbdc/src/common/types/string.rs
@@ -28,6 +28,15 @@ impl CommonType for &str {
         if self.ends_with("D") {
             return "decimal";
         }
+        //UUID-V4 = 4b3f82bc-fa70-48e5-914c-17f0c8d246e2
+        if self.len() == 36
+            && bytes[8] == '-' as u8
+            && bytes[13] == '-' as u8
+            && bytes[18] == '-' as u8
+            && bytes[23] == '-' as u8
+        {
+            return "uuid";
+        };
         // json
         if bytes[0] == '{' as u8 && bytes[bytes.len() - 1] == '}' as u8 {
             return "json";
