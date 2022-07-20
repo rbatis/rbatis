a67194d528abd500dc1ada6d6e9c1d1d5af1b3cc
zxj <zhuxiujia@qq.com>
2022-07-20 18:17:10 +0800

    add common type

--- rbdc/src/common/types/string.rs
+++ rbdc/src/common/types/string.rs
@@ -3,15 +3,15 @@ use crate::CommonType;
 impl CommonType for &str {
     fn common_type(&self) -> &'static str {
         let bytes = self.as_bytes();
-        ///Date RFC3339 = "2006-01-02"
+        //Date RFC3339 = "2006-01-02"
         if self.len() == 10 && (bytes[4] == '-' as u8 && bytes[7] == '-' as u8) {
             return "date";
         }
-        ///Time RFC3339 = "15:04:05.999999"
+        //Time RFC3339 = "15:04:05.999999"
         if self.len() >= 8 && (bytes[2] == ':' as u8 && bytes[5] == ':' as u8) {
             return "time";
         }
-        ///DateTime RFC3339 = "2006-01-02 15:04:05.999999"
+        //DateTime RFC3339 = "2006-01-02 15:04:05.999999"
         if self.len() == 19
             && (bytes[4] == '-' as u8
                 && bytes[7] == '-' as u8
@@ -20,16 +20,23 @@ impl CommonType for &str {
         {
             return "datetime";
         }
-        ///TimeStamp = 9999999999999Z
+        //TimeStamp = 9999999999999Z
         if self.ends_with("Z") {
             return "timestamp";
         }
-        ///Decimal   = 12345678D
+        //Decimal   = 12345678D
         if self.ends_with("D") {
             return "decimal";
         }
+        // json
         if bytes[0] == '{' as u8 && bytes[bytes.len() - 1] == '}' as u8 {
             return "json";
+        }
+        // json array
+        if bytes.len() >= 4 && bytes[0] == '[' as u8 && bytes[bytes.len() - 1] == ']' as u8 {
+            if bytes[1] == '{' as u8 && bytes[bytes.len() - 2] == '}' as u8 {
+                return "json";
+            }
         }
         return "string";
     }
