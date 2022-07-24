3ca72e1d8df3514a5000ad896e0a63b5a84f129e
zhuxiujia <zhuxiujia@qq.com>
2022-07-24 23:25:46 +0800

    fix

--- rbdc/src/types/time.rs
+++ rbdc/src/types/time.rs
@@ -1,5 +1,13 @@
+use std::fmt::{Display, Formatter};
 
-#[derive(serde::Serialize,serde::Deserialize,Debug,Clone,Eq, PartialEq)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Eq, PartialEq)]
 #[serde(rename = "time")]
 pub struct Time(String);
 
+impl Display for Time {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "time")?;
+        write!(f, "{}", self.0)?;
+        write!(f, ")")
+    }
+}
--- rbdc/src/types/timestamp.rs
+++ rbdc/src/types/timestamp.rs
@@ -1,5 +1,13 @@
+use std::fmt::{Display, Formatter};
 
-#[derive(serde::Serialize,serde::Deserialize,Debug,Clone,Eq, PartialEq)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Eq, PartialEq)]
 #[serde(rename = "timestamp")]
 pub struct Timestamp(u64);
 
+impl Display for Timestamp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "timestamp")?;
+        write!(f, "{}", self.0)?;
+        write!(f, ")")
+    }
+}
