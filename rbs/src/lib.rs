d200017256da5db5292785932cc140b1141a7227
zxj <zhuxiujia@qq.com>
2022-07-25 17:58:09 +0800

    add into ext

--- rbs/src/lib.rs
+++ rbs/src/lib.rs
@@ -6,9 +6,38 @@
 
 #[macro_use]
 extern crate serde;
+
 #[allow(deprecated)]
 pub mod value;
+
 pub use crate::value::ext::to_value_ref;
-pub use value::ext::to_value;
+pub use value::ext::{to_value,to_value_def};
 pub use value::ext::{deserialize_from, from_value};
 pub use value::{Value, ValueRef};
+
+
+impl Value {
+    pub fn into_ext(self, name: &'static str) -> Self {
+        match self {
+            Value::Ext(_, _) => {
+                self
+            }
+            _ => {
+                Value::Ext(name, Box::new(self))
+            }
+        }
+    }
+}
+
+impl ValueRef<'_> {
+    pub fn into_ext(self, name: &'static str) -> Self {
+        match self {
+            ValueRef::Ext(_, _) => {
+                self
+            }
+            _ => {
+                ValueRef::Ext(name, Box::new(self))
+            }
+        }
+    }
+}
