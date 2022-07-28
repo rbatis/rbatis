00189c7c59e64b2cbf9072bf0f22b34ae5292513
zhuxiujia <zhuxiujia@qq.com>
2022-07-28 23:08:17 +0800

    impl pg type

---
+++ rbdc-pg/src/types/timestamp.rs
@@ -0,0 +1,27 @@
+use std::str::FromStr;
+use rbdc::Error;
+use rbdc::timestamp::Timestamp;
+use crate::arguments::PgArgumentBuffer;
+use crate::types::decode::Decode;
+use crate::types::encode::{Encode, IsNull};
+use crate::value::{PgValue, PgValueFormat};
+
+impl Encode for Timestamp {
+    fn encode(self, buf: &mut PgArgumentBuffer) -> Result<IsNull,Error> {
+        self.0.encode(buf)
+    }
+}
+
+impl Decode for Timestamp {
+    fn decode(value: PgValue) -> Result<Self, Error> {
+        Ok(match value.format() {
+            PgValueFormat::Binary => {
+                Timestamp(u64::decode(value)?)
+            }
+            PgValueFormat::Text => {
+                let s = value.as_str()?;
+                Timestamp(fastdate::DateTime::from_str(s).unwrap().unix_timestamp_millis() as u64)
+            }
+        })
+    }
+}
