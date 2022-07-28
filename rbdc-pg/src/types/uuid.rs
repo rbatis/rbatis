3ebd626df9039cb86c738b7601d698817c70823c
zhuxiujia <zhuxiujia@qq.com>
2022-07-28 23:43:46 +0800

    impl pg type

--- rbdc-pg/src/types/uuid.rs
+++ rbdc-pg/src/types/uuid.rs
@@ -3,16 +3,20 @@ use rbdc::uuid::Uuid;
 use crate::arguments::PgArgumentBuffer;
 use crate::types::decode::Decode;
 use crate::types::encode::{Encode, IsNull};
-use crate::value::PgValue;
+use crate::value::{PgValue, PgValueFormat};
 
 impl Encode for Uuid{
     fn encode(self, buf: &mut PgArgumentBuffer) -> Result<IsNull, Error> {
-        todo!()
+        buf.extend_from_slice(self.0.as_bytes());
+        Ok(IsNull::No)
     }
 }
 
 impl Decode for Uuid{
     fn decode(value: PgValue) -> Result<Self, Error> {
-        todo!()
+        Ok(Self(match value.format() {
+            PgValueFormat::Binary => String::from_utf8(value.as_bytes()?.to_vec()).map_err(|e|Error::from(format!("Decode Uuid:{}",e)))?,
+            PgValueFormat::Text => value.as_str()?.parse().map_err(|e|Error::from(format!("Decode Uuid str:{}",e)))?,
+        }))
     }
 }
