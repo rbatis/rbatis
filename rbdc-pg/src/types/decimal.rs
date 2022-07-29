4fec8c86879120240e04716d2694cfed8ac05e04
zxj <zhuxiujia@qq.com>
2022-07-29 14:49:29 +0800

    impl pg decimal

--- rbdc-pg/src/types/decimal.rs
+++ rbdc-pg/src/types/decimal.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+use bigdecimal::BigDecimal;
 use rbdc::decimal::Decimal;
 use rbdc::Error;
 use crate::arguments::PgArgumentBuffer;
@@ -5,17 +7,19 @@ use crate::types::decode::Decode;
 use crate::types::encode::{Encode, IsNull};
 use crate::value::{PgValue, PgValueFormat};
 
-impl Encode for Decimal{
+
+impl Encode for Decimal {
     fn encode(self, buf: &mut PgArgumentBuffer) -> Result<IsNull, Error> {
-        self.0.encode(buf)?;
+        let b = BigDecimal::from_str(&self.0).map_err(|e| Error::from(e.to_string()))?;
+        b.encode(buf)?;
         Ok(IsNull::No)
     }
 }
 
-impl Decode for Decimal{
+impl Decode for Decimal {
     fn decode(value: PgValue) -> Result<Self, Error> {
         match value.format() {
-            PgValueFormat::Binary => Ok(Self(String::from_utf8(value.into_bytes()?).unwrap_or("0".to_string()))),
+            PgValueFormat::Binary => Ok(Self(BigDecimal::decode(value)?.to_string())),
             PgValueFormat::Text => Ok(Self(value.as_str()?.to_string())),
         }
     }
