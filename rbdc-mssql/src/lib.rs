db0e4a2b34aaa917e5c6bd773817796134f6e640
zhuxiujia <zhuxiujia@qq.com>
2022-07-31 07:21:00 +0800

    impl mssql

--- rbdc-mssql/src/lib.rs
+++ rbdc-mssql/src/lib.rs
@@ -2,10 +2,11 @@ pub mod encode;
 pub mod decode;
 pub mod driver;
 
+use std::any::Any;
 use std::sync::Arc;
 use futures_core::future::BoxFuture;
 use futures_util::StreamExt;
-use rbdc::db::{Connection, MetaData, Row};
+use rbdc::db::{Connection, ConnectOptions, MetaData, Row};
 use tiberius::{Client, Config, AuthMethod, Column, QueryStream, Query, ColumnData};
 use rbdc::{block_on, Error};
 use rbs::Value;
@@ -21,6 +22,44 @@ use crate::encode::Encode;
 pub struct MssqlConnection {
     inner: Client<Compat<TcpStream>>,
 }
+
+impl MssqlConnection {
+    /// let cfg = Config::from_jdbc_string(url).map_err(|e| Error::from(e.to_owned()))?;
+    pub async fn establish(cfg: &Config) -> Result<Self, Error> {
+        // let cfg = Config::from_jdbc_string(url).map_err(|e| Error::from(e.to_owned()))?;
+        let tcp = TcpStream::connect(cfg.get_addr()).await.map_err(|e| Error::from(e.to_string()))?;
+        tcp.set_nodelay(true).unwrap();
+        let c = Client::connect(cfg.clone(), tcp.compat_write()).await.map_err(|e| Error::from(e.to_string()))?;
+        Ok(Self {
+            inner: c,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct MssqlConnectOptions(Config);
+
+impl ConnectOptions for MssqlConnectOptions{
+    fn connect(&self) -> BoxFuture<Result<Box<dyn Connection>, Error>> {
+        Box::pin(async move {
+            let v = MssqlConnection::establish(&self.0)
+                .await
+                .map_err(|e| Error::from(e.to_string()))?;
+            Ok(Box::new(v) as Box<dyn Connection>)
+        })
+    }
+
+    fn set_uri(&mut self, url: &str) -> Result<(), Error> {
+        *self = MssqlConnectOptions(Config::from_jdbc_string(url).map_err(|e| Error::from(e.to_string()))?);
+        Ok(())
+    }
+
+    fn uppercase_self(&self) -> &(dyn Any + Send + Sync) {
+        self
+    }
+}
+
+
 
 #[derive(Debug)]
 pub struct MssqlRow {
@@ -55,7 +94,7 @@ impl Row for MssqlRow {
     }
 
     fn get(&mut self, i: usize) -> Option<Value> {
-        Some(Value::decode(&self.inner,i,self.inner.columns()[i].column_type()))
+        Some(Value::decode(&self.inner, i, self.inner.columns()[i].column_type()))
     }
 }
 
@@ -64,7 +103,7 @@ impl Connection for MssqlConnection {
     fn get_rows(&mut self, sql: &str, params: Vec<Value>) -> BoxFuture<Result<Vec<Box<dyn Row>>, rbdc::Error>> {
         let sql = sql.to_string();
         Box::pin(async move {
-            let mut q =Query::new(sql);
+            let mut q = Query::new(sql);
             for x in params {
                 x.encode(&mut q)?;
             }
