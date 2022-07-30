069f3368b4e7a5c33f5910053f2f39f0c8b09620
zhuxiujia <zhuxiujia@qq.com>
2022-07-31 03:10:32 +0800

    add impl uppercase

--- rbdc/src/pool/mobc.rs
+++ rbdc/src/pool/mobc.rs
@@ -1,31 +1,30 @@
-
 use mobc::{async_trait, Manager, Pool};
-use crate::block_on;
+use crate::{block_on, Error};
 use crate::db::{ConnectOptions, Driver};
 
-pub struct RBDCManager{
-    driver:Box<dyn Driver>,
-    opt:Box<dyn ConnectOptions>,
+pub struct RBDCManager {
+    driver: Box<dyn Driver>,
+    opt: Box<dyn ConnectOptions>,
 }
 
 #[async_trait]
 impl Manager for RBDCManager {
     type Connection = Box<dyn crate::db::Connection>;
-    type Error = crate::error::Error;
+    type Error = Error;
 
     async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        // self.driver.connect()
-        todo!()
+        self.driver.connect_opt(self.opt.as_ref()).await
     }
 
-    async fn check(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
+    async fn check(&self, mut conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
+        conn.ping().await?;
         Ok(conn)
     }
 }
 
 #[test]
-fn test_pool(){
-    let f=async move{
+fn test_pool() {
+    let f = async move {
         // let foo=RBDCManager{
         //     driver: Box::new(),
         //     opt: Box::new(())
