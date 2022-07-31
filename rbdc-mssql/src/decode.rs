f5c2436275a148600fbab6cc69de6df76474fc34
zhuxiujia <zhuxiujia@qq.com>
2022-07-31 14:53:45 +0800

    impl mssql

--- rbdc-mssql/src/decode.rs
+++ rbdc-mssql/src/decode.rs
@@ -13,7 +13,7 @@ pub trait Decode {
 
 impl Decode for Value {
     fn decode(row: &tiberius::Row, i: usize, t: ColumnType) -> Value {
-        if (i + 1) > row.len() {
+        if (i + 1) >= row.len() {
             return Value::Null;
         }
         match t {
