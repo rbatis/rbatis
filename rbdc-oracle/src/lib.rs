938b2212c75f25fdd8604b596a9d28964e5fe22a
zhuxiujia <zhuxiujia@qq.com>
2022-07-31 16:13:13 +0800

    add oracle

--- rbdc-oracle/src/lib.rs
+++ rbdc-oracle/src/lib.rs
@@ -6,8 +6,10 @@ pub mod decode;
 pub mod encode;
 
 pub struct OracleConnection {
-    sender: flume::Sender<Value>,
-    receiver: flume::Receiver<Value>,
+    sender_arg: flume::Sender<Value>,
+    receiver_arg: flume::Receiver<Value>,
+    sender_result: flume::Sender<Result<Value,Error>>,
+    receiver_result: flume::Receiver<Result<Value,Error>>,
 }
 
 impl OracleConnection{
@@ -19,6 +21,7 @@ impl OracleConnection{
 
 #[cfg(test)]
 mod tests {
+    use flume::SendError;
     use rbdc::block_on;
     use rbdc::rt::tokio;
     use rbs::Value;
@@ -31,22 +34,33 @@ mod tests {
             // Connect to a database.
             let conn = Connection::connect("scott", "tiger", "//localhost/XE").unwrap();
             let sql = "select ename, sal, comm from emp where deptno = :1";
-
             let (s, r) = flume::unbounded();
-            let s_clone = s.clone();
-            let r_clone = r.clone();
+            let (s_r, r_r) = flume::unbounded();
+            let conn = OracleConnection {
+                sender_arg: s,
+                receiver_arg: r,
+                sender_result: s_r,
+                receiver_result: r_r
+            };
+            let s = conn.sender_result.clone();
+            let r = conn.receiver_arg.clone();
             std::thread::spawn(move || {
                 loop {
-                    if let Ok(v) = r_clone.recv() {
-                        s_clone.send(v);
+                    if let Ok(v) = r.recv() {
+                        let result = Ok(Value::Null);
+
+                        match s.send(result) {
+                            Ok(_) => {
+
+                            }
+                            Err(_) => {
+                                //disconnected
+                            }
+                        }
                     }
                 }
             });
-            let conn = OracleConnection {
-                sender: s,
-                receiver: r,
-            };
-            conn.sender.send_async(Value::Null).await;
+            conn.sender_arg.send_async(Value::Null).await;
         };
         block_on!(f);
     }
