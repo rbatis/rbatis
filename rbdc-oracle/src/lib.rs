5786719107b4736aeacc9af93afd234a99fac0e3
zhuxiujia <zhuxiujia@qq.com>
2022-07-31 15:52:51 +0800

    add oracle

--- rbdc-oracle/src/lib.rs
+++ rbdc-oracle/src/lib.rs
@@ -1,9 +1,12 @@
+use rbs::Value;
+
 pub mod driver;
 pub mod decode;
 pub mod encode;
 
 pub struct OracleConnection {
-
+    sender: flume::Sender<Value>,
+    receiver: flume::Receiver<Value>,
 }
 
 
@@ -11,14 +14,32 @@ pub struct OracleConnection {
 mod tests {
     use rbdc::block_on;
     use rbdc::rt::tokio;
+    use rbs::Value;
+    use crate::OracleConnection;
 
     #[test]
     fn test_oracle_pool() {
-        let f=async move{
+        let f = async move {
             use oracle::{Connection, Error};
             // Connect to a database.
             let conn = Connection::connect("scott", "tiger", "//localhost/XE").unwrap();
             let sql = "select ename, sal, comm from emp where deptno = :1";
+
+            let (s, r) = flume::unbounded();
+            let s_clone = s.clone();
+            let r_clone = r.clone();
+            std::thread::spawn(move || {
+                loop {
+                    if let Ok(v) = r_clone.recv() {
+                        s_clone.send(v);
+                    }
+                }
+            });
+            let conn = OracleConnection {
+                sender: s,
+                receiver: r,
+            };
+            conn.sender.send_async(Value::Null).await;
         };
         block_on!(f);
     }
