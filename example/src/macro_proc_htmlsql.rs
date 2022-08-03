ff52654785e2c737bf7577479178adb6dc94826e
zxj <zhuxiujia@qq.com>
2022-08-03 16:50:13 +0800

    remove dead code

--- example/src/macro_proc_htmlsql.rs
+++ example/src/macro_proc_htmlsql.rs
@@ -1,38 +1,36 @@
-#[cfg(test)]
-mod test {
-    use std::fs::File;
-    use std::io::Read;
+use std::fs::File;
+use std::io::Read;
 
-    use rbatis::executor::RbatisExecutor;
-    use rbatis::sql::page::{Page, PageRequest};
-    use rbatis::rbatis::Rbatis;
+use rbatis::executor::RbatisExecutor;
+use rbatis::sql::page::{Page, PageRequest};
+use rbatis::rbatis::Rbatis;
 
-    use crate::{init_sqlite, BizActivity};
+use crate::{init_sqlite, BizActivity};
 
-    ///select page must have  '?:&PageRequest' arg and return 'Page<?>'
-    #[html_sql("example/example.html")]
-    async fn select_by_condition(
-        mut rb: RbatisExecutor<'_>,
-        page_req: &PageRequest,
-        name: &str,
-        dt: &rbatis::DateTimeNative,
-    ) -> Page<BizActivity> {
-        impled!()
-    }
+///select page must have  '?:&PageRequest' arg and return 'Page<?>'
+#[html_sql("example/example.html")]
+async fn select_by_condition(
+    mut rb: RbatisExecutor<'_>,
+    page_req: &PageRequest,
+    name: &str,
+    dt: &rbatis::DateTimeNative,
+) -> Page<BizActivity> {
+    impled!()
+}
 
-    #[tokio::test]
-    pub async fn test_select_by_condition() {
-        fast_log::init(fast_log::config::Config::new().console());
-        //use static ref
-        let rb = init_sqlite().await;
-        let a = select_by_condition(
-            rb.as_executor(),
-            &PageRequest::new(1, 10),
-            "test",
-            &rbatis::DateTimeNative::now(),
-        )
+#[tokio::main]
+pub async fn main() {
+    fast_log::init(fast_log::config::Config::new().console());
+    //use static ref
+    let rb = init_sqlite().await;
+    let a = select_by_condition(
+        rb.as_executor(),
+        &PageRequest::new(1, 10),
+        "test",
+        &rbatis::DateTimeNative::now(),
+    )
         .await
         .unwrap();
-        println!("{:?}", a);
-    }
+    println!("{:?}", a);
 }
+
