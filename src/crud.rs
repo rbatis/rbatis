7826a91ddb671c008a34eef81a729893bde4f2d8
zxj <zhuxiujia@qq.com>
2022-08-04 17:08:17 +0800

    add test

--- src/crud.rs
+++ src/crud.rs
@@ -38,3 +38,32 @@ async fn do_save(mut rb: $crate::executor::RbatisExecutor<'_>,table: &$table,tab
         }
     };
 }
+
+
+///gen sql => SELECT (column1,column2,column3,...) FROM table_name (column1,column2,column3,...)  *** WHERE ***
+///
+/// example:
+/// pub struct BizActivity{}
+///
+/// impl_insert!(BizActivity,"biz_activity");
+///
+/// let table = BizActivity{}
+/// BizActivity::select()
+///
+#[macro_export]
+macro_rules! impl_select_all {
+    ($table:ty) => {
+        $crate::impl_select_all!($table,$crate::utils::string_util::to_snake_name(stringify!($table)));
+    };
+    ($table:ty,$table_name:expr) => {
+        impl $table{
+            pub async fn select_all(mut rb: $crate::executor::RbatisExecutor<'_>)->Result<Vec<$table>,rbdc::Error>{
+                #[py_sql(
+"select * from ${table_name}")]
+async fn do_select_all(mut rb: $crate::executor::RbatisExecutor<'_>,table_name:String) -> Result<Vec<$table>,rbdc::Error> {impled!()}
+            let table_name = $table_name.to_string();
+            do_select_all(rb,table_name).await
+            }
+        }
+    };
+}
