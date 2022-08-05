d490d17e59702f239bb1dac764fd53355ee0f918
zhuxiujia <zhuxiujia@qq.com>
2022-08-05 22:18:21 +0800

    impl iter

--- tests/mod.rs
+++ tests/mod.rs
@@ -3,23 +3,27 @@ extern crate rbatis;
 
 
 #[cfg(test)]
-mod test{
+mod test {
     use rbs::Value;
     use rbs::value::map::ValueMap;
 
     #[test]
-    fn test_value_iter(){
-        let v=Value::Array(vec![Value::I32(1)]);
-        for (k,v) in v {
-            println!("{},{}",k,v);
+    fn test_value_iter() {
+        let v = Value::Array(vec![Value::I32(1)]);
+        for (k, v) in &v {
+            println!("{},{}", k, v);
         }
-        let v=Value::Array(vec![Value::I32(1)]);
-        for (k,v) in &v {
-            println!("{},{}",k,v);
+        for (k, v) in v {
+            println!("{},{}", k, v);
         }
-        let v=Value::Map(ValueMap::new());
-        for (k,v) in v {
-            println!("{},{}",k,v);
+        let mut m = ValueMap::new();
+        m.insert(1.into(), 1.into());
+        let v = Value::Map(m);
+        for (k, v) in &v {
+            println!("{},{}", k, v);
+        }
+        for (k, v) in v {
+            println!("{},{}", k, v);
         }
     }
 }
