f52df71574c98d2c13d07f5e18951fb6cb5777f0
zxj <zhuxiujia@qq.com>
2022-08-05 13:35:01 +0800

    clear dead code

--- tests/plugin_page_test.rs
+++ tests/plugin_page_test.rs
@@ -1,39 +1,4 @@
 #[cfg(test)]
 mod test {
-    use rbatis::{
-        PagePlugin, PageRequest, RbatisPackPagePlugin, RbatisPagePlugin, RbatisReplacePagePlugin,
-    };
-    use rbatis_core::db::DriverType;
-
-    #[test]
-    fn test_page() {
-        let pack_page = RbatisReplacePagePlugin {};
-        let sql = "select
-        b.name,
-        count (*) as total
-    from
-        (select * from demands where deleted_at is null) a
-    left join
-        (select * from demand_kinds where deleted_at is null) b
-    on
-        a.demand_kind_id = b.id
-    group by
-        b.name
-    order by
-        total desc";
-        let (count, select) = pack_page
-            .make_page_sql(
-                &DriverType::Postgres,
-                &sql,
-                &vec![],
-                &PageRequest::new(1, 10),
-            )
-            .unwrap();
-        println!("{}", count);
-        println!("/////////////////");
-        println!("{}", select);
-    }
-
-    #[test]
-    fn test_all() {}
+   
 }
