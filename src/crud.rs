aa334b36fff814f0d02191953f38d35bb2530921
zhuxiujia <zhuxiujia@qq.com>
2022-08-06 02:37:17 +0800

    fix bugs

--- src/crud.rs
+++ src/crud.rs
@@ -215,7 +215,7 @@ macro_rules! impl_select_page {
                     async fn do_select_page(rb: &mut dyn $crate::executor::Executor,table_name: &str,page_no:u64,page_size:u64,$($param_key:$param_type,)+) -> Result<Vec<$table>, rbdc::Error> {impled!()}
                     records = do_select_page(rb,&table_name,page_req.page_no, page_req.page_size,$($param_key,)+).await?;
                 }else{
-                    #[py_sql("select * from ${table_name} ",$where_sql,"limit ${page_no},${page_size}")]
+                    #[py_sql("select * from ${table_name} ",$where_sql," limit ${page_no},${page_size}")]
                     async fn do_select_page(rb: &mut dyn $crate::executor::Executor,table_name: &str,page_no:u64,page_size:u64,$($param_key:$param_type,)+) -> Result<Vec<$table>, rbdc::Error> {impled!()}
                     records = do_select_page(rb,&table_name,page_req.page_no, page_req.page_size,$($param_key,)+).await?;
                 }
