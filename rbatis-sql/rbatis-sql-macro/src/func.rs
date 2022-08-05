83011783189babb1f1db9db37f5c83ae4421270b
zxj <zhuxiujia@qq.com>
2022-08-05 13:10:23 +0800

    change version

--- rbatis-sql/rbatis-sql-macro/src/func.rs
+++ rbatis-sql/rbatis-sql-macro/src/func.rs
@@ -6,21 +6,6 @@ use syn::{BinOp, Expr, ItemFn, Lit, Member};
 
 use crate::proc_macro::TokenStream;
 
-// fn is_name_char(arg: char) -> bool {
-//     match arg {
-//         '.' |
-//         '_' |
-//         '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' |
-//         'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' |
-//         'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
-//         => {
-//             return true;
-//         }
-//         _ => {}
-//     }
-//     return false;
-// }
-
 fn is_param_char(arg: char) -> bool {
     match arg {
         'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' |
