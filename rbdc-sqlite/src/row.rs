2551b78f5fec1861277ddfbd5c4e0c187f4caffc
zhuxiujia <zhuxiujia@qq.com>
2022-08-13 22:53:16 +0800

    fix decode

--- rbdc-sqlite/src/row.rs
+++ rbdc-sqlite/src/row.rs
@@ -83,7 +83,7 @@ impl Row for SqliteRow {
     fn get(&mut self, i: usize) -> Result<Value,Error> {
         match self.try_take(i) {
             Err(e) => {
-                Err(Error::from(format!("get error:{}", e)))
+                Err(Error::from(format!("get error index:{},error:{}",i, e)))
             }
             Ok(v) => Value::decode(v),
         }
