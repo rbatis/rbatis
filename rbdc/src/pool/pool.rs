ce4dfe71ebf522667a13a61b009e8cd8d98d11d9
zxj <zhuxiujia@qq.com>
2022-08-15 13:34:02 +0800

    pool auto call close() on drop

--- rbdc/src/pool/pool.rs
+++ rbdc/src/pool/pool.rs
@@ -4,13 +4,25 @@ use futures_core::future::BoxFuture;
 use mobc::{async_trait, Builder, Manager};
 use rbs::Value;
 use std::fmt::{Debug, Formatter};
+use std::future::Future;
 use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 
 /// RBDC pool
 pub struct Pool {
-    pub manager: Arc<RBDCManager>,
+    pub manager: ManagerPorxy,
     pub inner: mobc::Pool<ManagerPorxy>,
+}
+
+impl Pool {
+    pub fn name(&self) -> &str {
+        self.manager.name()
+    }
+
+    /// spawn task on runtime
+    pub fn spawn_task<T>(&self, task: T) where T: Future + Send + 'static, T::Output: Send + 'static {
+        self.manager.spawn_task(task);
+    }
 }
 
 impl Debug for Pool {
@@ -21,7 +33,7 @@ impl Debug for Pool {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct ManagerPorxy {
     pub inner: Arc<RBDCManager>,
 }
@@ -40,25 +52,51 @@ impl Deref for ManagerPorxy {
     }
 }
 
-impl Pool {
-    pub fn name(&self) -> &str {
-        self.manager.name()
-    }
-}
-
 #[derive(Debug)]
 pub struct RBDCManager {
     pub driver: Box<dyn Driver>,
     pub option: Box<dyn ConnectOptions>,
 }
 
+pub struct DropBox {
+    pub proxy: ManagerPorxy,
+    pub inner: Option<Box<dyn Connection>>,
+}
+
+impl Deref for DropBox {
+    type Target = Box<dyn Connection>;
+
+    fn deref(&self) -> &Self::Target {
+        self.inner.as_ref().unwrap()
+    }
+}
+
+impl DerefMut for DropBox {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.as_mut().unwrap()
+    }
+}
+
+impl Drop for DropBox {
+    fn drop(&mut self) {
+        if let Some(mut conn) = self.inner.take() {
+            self.proxy.spawn_task(async move {
+                let _ = conn.close().await;
+            });
+        }
+    }
+}
+
 #[async_trait]
 impl Manager for ManagerPorxy {
-    type Connection = Box<dyn crate::db::Connection>;
+    type Connection = DropBox;
     type Error = Error;
 
     async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        self.driver.connect_opt(self.option.as_ref()).await
+        Ok(DropBox {
+            proxy: self.clone(),
+            inner: Some(self.driver.connect_opt(self.option.as_ref()).await?),
+        })
     }
 
     async fn check(&self, mut conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
@@ -114,8 +152,8 @@ impl Pool {
         d: Driver,
         url: &str,
     ) -> Result<Self, Error> {
-        let manager = Arc::new(RBDCManager::new(d, url)?);
-        let p = mobc::Pool::new(ManagerPorxy::from(manager.clone()));
+        let manager = ManagerPorxy::from(Arc::new(RBDCManager::new(d, url)?));
+        let p = mobc::Pool::new(manager.clone());
         let pool = Pool {
             manager: manager,
             inner: p,
@@ -126,8 +164,8 @@ impl Pool {
         d: Driver,
         o: ConnectOptions,
     ) -> Self {
-        let manager = Arc::new(RBDCManager::new_opt(d, o));
-        let p = mobc::Pool::new(ManagerPorxy::from(manager.clone()));
+        let manager = ManagerPorxy::from(Arc::new(RBDCManager::new_opt(d, o)));
+        let p = mobc::Pool::new(manager.clone());
         let pool = Pool {
             manager: manager,
             inner: p,
@@ -136,8 +174,8 @@ impl Pool {
     }
 
     pub fn new_box(d: Box<dyn Driver>, o: Box<dyn ConnectOptions>) -> Self {
-        let manager = Arc::new(RBDCManager::new_opt_box(d, o));
-        let p = mobc::Pool::new(ManagerPorxy::from(manager.clone()));
+        let manager = ManagerPorxy::from(Arc::new(RBDCManager::new_opt_box(d, o)));
+        let p = mobc::Pool::new(manager.clone());
         let pool = Pool {
             manager: manager,
             inner: p,
@@ -150,8 +188,8 @@ impl Pool {
         d: Box<dyn Driver>,
         o: Box<dyn ConnectOptions>,
     ) -> Self {
-        let manager = Arc::new(RBDCManager::new_opt_box(d, o));
-        let p = builder.build(ManagerPorxy::from(manager.clone()));
+        let manager = ManagerPorxy::from(Arc::new(RBDCManager::new_opt_box(d, o)));
+        let p = builder.build(manager.clone());
         let pool = Pool {
             manager: manager,
             inner: p,
