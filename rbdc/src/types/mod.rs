1ec4a22ad27e54d5dc4fe1fb35ff6f513d6c4788
zxj <zhuxiujia@qq.com>
2022-08-17 16:33:30 +0800

    add doc

--- rbdc/src/types/mod.rs
+++ rbdc/src/types/mod.rs
@@ -1,8 +1,8 @@
+///this mod support Some common types, the actual type may need to use the type provided by the driver
+///
 pub mod date;
 pub mod datetime;
 pub mod decimal;
-///this mod support Some common types, the actual type may need to use the type provided by the driver
-///
 pub mod json;
 pub mod time;
 pub mod timestamp;
