f744bf9af614f249658198f89dea06e4eab9e87b
zxj <zhuxiujia@qq.com>
2022-08-17 16:27:26 +0800

    add from_str

--- rbdc/src/types/uuid.rs
+++ rbdc/src/types/uuid.rs
@@ -1,5 +1,7 @@
 use rbs::Value;
 use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+use crate::Error;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
 #[serde(rename = "Uuid")]
@@ -14,5 +16,13 @@ impl Display for Uuid {
 impl From<Uuid> for Value {
     fn from(arg: Uuid) -> Self {
         Value::Ext("Uuid", Box::new(Value::String(arg.0)))
+    }
+}
+
+impl FromStr for Uuid {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Uuid(s.to_string()))
     }
 }
