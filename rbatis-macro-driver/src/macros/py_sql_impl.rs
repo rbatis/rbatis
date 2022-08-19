050e36923b64b8b72b9f646d17ee93914de45348
zxj <zhuxiujia@qq.com>
2022-08-19 15:14:28 +0800

    remove dead code

--- rbatis-macro-driver/src/macros/py_sql_impl.rs
+++ rbatis-macro-driver/src/macros/py_sql_impl.rs
@@ -82,13 +82,12 @@ pub(crate) fn impl_macro_py_sql(target_fn: &ItemFn, args: &AttributeArgs) -> Tok
         call_method = quote! {
              use rbatis::executor::{Executor};
              let r=#rbatis_ident.fetch(&sql,rb_args).await?;
-             Ok(rbatis::decode::decode(r)?)
+             rbatis::decode::decode(r)
         };
     } else {
         call_method = quote! {
              use rbatis::executor::{Executor};
-             let r=#rbatis_ident.exec(&sql,rb_args).await?;
-             Ok(r)
+             #rbatis_ident.exec(&sql,rb_args).await
         };
     }
     let gen_target_method = quote! {
