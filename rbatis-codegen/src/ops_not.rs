d5f0d4ee133f9d0e74006f73b2a4c0ddef5f5d99
zxj <zhuxiujia@qq.com>
2022-09-06 15:00:56 +0800

    fix codegen

--- rbatis-codegen/src/ops_not.rs
+++ rbatis-codegen/src/ops_not.rs
@@ -2,33 +2,41 @@ use crate::ops::Not;
 
 use rbs::Value;
 
+fn op_not(left: Value) -> Value {
+    match left {
+        Value::I32(b) => {
+            Value::I32(!b)
+        }
+        Value::Bool(b) => {
+            Value::Bool(!b)
+        }
+        Value::Ext(_, e) => {
+            op_not(*e)
+        }
+        _ => {
+            Value::Null
+        }
+    }
+}
+
 impl Not for Value {
-    type Output = bool;
+    type Output = Value;
 
     fn op_not(self) -> Self::Output {
-        match self {
-            Value::Bool(b) => !b,
-            _ => true,
-        }
+        op_not(self)
     }
 }
 
 impl Not for &Value {
-    type Output = bool;
+    type Output = Value;
     fn op_not(self) -> Self::Output {
-        match self {
-            Value::Bool(b) => !*b,
-            _ => true,
-        }
+        op_not(self.to_owned())
     }
 }
 
 impl Not for &mut Value {
-    type Output = bool;
+    type Output = Value;
     fn op_not(self) -> Self::Output {
-        match self {
-            Value::Bool(b) => !*b,
-            _ => true,
-        }
+        op_not(self.to_owned())
     }
 }
