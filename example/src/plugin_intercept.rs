38e3798484990b7f5c465bd763bc566dabeda9f1
zxj <zhuxiujia@qq.com>
2022-10-17 10:20:10 +0800

    add example intercept

--- example/src/plugin_intercept.rs
+++ example/src/plugin_intercept.rs
@@ -7,16 +7,19 @@ use rbatis::{crud, Error, Rbatis};
 use rbs::Value;
 use crate::model::{BizActivity, init_db};
 
+/// Logic delete： The deletion statement changes to the modification of flag, and the query statement filters flag with additional conditions
 pub struct LogicDeletePlugin {}
 
 impl SqlIntercept for LogicDeletePlugin {
     fn do_intercept(&self, _rb: &Rbatis, sql: &mut String, _args: &mut Vec<Value>, _is_prepared_sql: bool) -> Result<(), Error> {
-        if sql.contains("delete from "){
+        if sql.contains("delete from ") {
             let table_name = sql[sql.find("from").unwrap_or(0) + 4..sql.find("where").unwrap_or(0)].trim();
-            println!("table_name => {}",table_name);
-            println!("befor => {}",sql);
-            *sql = sql.replace(&format!("delete from {}", table_name), &format!("update {} set delete_flag = 0 ", table_name));
-            println!("after => {}",sql);
+            println!("table_name => {}", table_name);
+            println!("befor => {}", sql);
+            *sql = sql.replace(&format!("delete from {}", table_name), &format!("update {} set delete_flag = 1 ", table_name));
+            println!("after => {}", sql);
+        } else if sql.contains("select ") && sql.contains(" where ") {
+            sql.push_str(" and delete_flag = 0 ");
         }
         Ok(())
     }
@@ -31,5 +34,7 @@ pub async fn main() {
     rb.set_sql_intercepts(vec![Box::new(LogicDeletePlugin {})]);
     let r = BizActivity::delete_by_column(&mut rb.clone(), "id", "1").await;
     println!("{:?}", r);
+    let record = BizActivity::select_by_column(&mut rb.clone(), "id", "1").await;
+    println!("{:?}", record);
     sleep(Duration::from_secs(1));
 }
