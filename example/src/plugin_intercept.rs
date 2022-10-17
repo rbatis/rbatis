c436d170a2901140cc3aaa7a80b40f7b0c9f9b4a
zxj <zhuxiujia@qq.com>
2022-10-17 17:44:01 +0800

    add example intercept

--- example/src/plugin_intercept.rs
+++ example/src/plugin_intercept.rs
@@ -14,12 +14,13 @@ impl SqlIntercept for LogicDeletePlugin {
     fn do_intercept(&self, _rb: &Rbatis, sql: &mut String, _args: &mut Vec<Value>, _is_prepared_sql: bool) -> Result<(), Error> {
         if sql.contains("delete from ") {
             let table_name = sql[sql.find("from").unwrap_or(0) + 4..sql.find("where").unwrap_or(0)].trim();
-            println!("table_name => {}", table_name);
-            println!("befor => {}", sql);
+            println!("[LogicDeletePlugin] before=> {}", sql);
             *sql = sql.replace(&format!("delete from {}", table_name), &format!("update {} set delete_flag = 1 ", table_name));
-            println!("after => {}", sql);
+            println!("[LogicDeletePlugin] after=> {}", sql);
         } else if sql.contains("select ") && sql.contains(" where ") {
+            println!("[LogicDeletePlugin] before=> {}", sql);
             sql.push_str(" and delete_flag = 0 ");
+            println!("[LogicDeletePlugin] after=> {}", sql);
         }
         Ok(())
     }
