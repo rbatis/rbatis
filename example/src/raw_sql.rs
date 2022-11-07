f7bf3c8b5289f9aded09d82fb24a4ee220e33cb1
zxj <zhuxiujia@qq.com>
2022-11-07 16:15:34 +0800

    edit example

--- example/src/raw_sql.rs
+++ example/src/raw_sql.rs
@@ -9,20 +9,17 @@ use tokio::time::sleep;
 pub async fn main() {
     fast_log::init(fast_log::Config::new().console()).expect("rbatis init fail");
     let rb = init_db().await;
+    //fetch
     let table: Option<BizActivity> = rb
         .fetch_decode("select * from biz_activity limit ?", vec![to_value!(1)])
         .await
         .unwrap();
-    let count: u64 = rb
-        .fetch_decode("select count(1) as count from biz_activity", vec![])
-        .await
-        .unwrap();
+    //exec
     let result = rb
         .exec("update biz_activity set status = 0 where id > 0", vec![])
         .await
         .unwrap();
     sleep(Duration::from_secs(1)).await;
     println!(">>>>> table={:?}", table);
-    println!(">>>>> count={}", count);
     println!(">>>>> exec={}", result);
 }
