f9b8dffe2f5085533973c136dea837f6b8fd0b1e
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-12-30 16:37:24 +0800

    edit SqliteTableSync

--- example/src/plugin_table_sync.rs
+++ example/src/plugin_table_sync.rs
@@ -1,46 +1,35 @@
 use rbatis::rbatis::Rbatis;
 use rbatis::rbdc::datetime::FastDateTime;
-use rbatis::table_sync::{RbatisTableSync, SqliteTableSync};
+use rbatis::table_sync::{SqliteTableSync, TableSync};
 use rbdc_sqlite::driver::SqliteDriver;
+use rbs::to_value;
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub struct BizActivity {
-    pub id: Option<String>,
+pub struct RBUser {
+    pub id: i32,
     pub name: Option<String>,
-    pub pc_link: Option<String>,
-    pub h5_link: Option<String>,
-    pub pc_banner_img: Option<String>,
-    pub h5_banner_img: Option<String>,
-    pub sort: Option<String>,
-    pub status: Option<i32>,
     pub remark: Option<String>,
     pub create_time: Option<FastDateTime>,
     pub version: Option<i64>,
     pub delete_flag: Option<i32>,
 }
+
 #[tokio::main]
 pub async fn main() {
     fast_log::init(fast_log::Config::new().console()).expect("rbatis init fail");
     let rb = Rbatis::new();
     rb.init(SqliteDriver {}, &format!("sqlite://target/sqlite.db"))
         .unwrap();
-    let mut s = RbatisTableSync::new();
-    s.insert("sqlite".to_string(), Box::new(SqliteTableSync::default()));
-    let t = BizActivity {
-        id: Some("".to_string()),
+    let mut s = SqliteTableSync::default();
+    s.sql_id = " PRIMARY KEY AUTOINCREMENT NOT NULL ".to_string();
+    s.sync(rb.acquire().await.unwrap(), to_value!(RBUser {
+        id: 0,
         name: Some("".to_string()),
-        pc_link: Some("".to_string()),
-        h5_link: Some("".to_string()),
-        pc_banner_img: Some("".to_string()),
-        h5_banner_img: Some("".to_string()),
-        sort: Some("".to_string()),
-        status: Some(1),
         remark: Some("".to_string()),
         create_time: Some(FastDateTime::utc()),
         version: Some(1),
         delete_flag: Some(1),
-    };
-    s.sync("sqlite", rb.acquire().await.unwrap(), t)
+    }), "rb_user")
         .await
         .unwrap();
 }
--- src/plugin/table_sync/mod.rs
+++ src/plugin/table_sync/mod.rs
@@ -1,5 +1,4 @@
 pub mod sqlite_table_sync;
-
 pub use sqlite_table_sync::*;
 
 use crate::executor::RBatisConnExecutor;
@@ -23,64 +22,4 @@ pub trait TableSync {
         table: Value,
         name: &str,
     ) -> BoxFuture<Result<(), Error>>;
-}
-
-pub struct RbatisTableSync {
-    pub plugins: HashMap<String, Box<dyn TableSync>>,
-}
-
-impl Deref for RbatisTableSync {
-    type Target = HashMap<String, Box<dyn TableSync>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.plugins
-    }
-}
-
-impl DerefMut for RbatisTableSync {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.plugins
-    }
-}
-
-impl RbatisTableSync {
-    pub fn new() -> Self {
-        Self {
-            plugins: Default::default(),
-        }
-    }
-    pub async fn sync<Table: Serialize + Any>(
-        &self,
-        driver_type: &str,
-        rb: RBatisConnExecutor,
-        table: Table,
-    ) -> Result<(), Error> {
-        let plugin = self.plugins.get(driver_type);
-        match plugin {
-            None => Err(Error::from("not support or load plugin!")),
-            Some(plugin) => {
-                let name = std::any::type_name::<Table>();
-                let struct_name = name.split("::").last().unwrap_or_default();
-                let struct_name = to_snake_name(struct_name);
-                log::info!("sync table_name:{}", struct_name);
-                plugin.sync(rb, to_value!(table), &struct_name).await
-            }
-        }
-    }
-    pub async fn sync_with_table_name<Table: Serialize + Any>(
-        &self,
-        driver_type: &str,
-        rb: RBatisConnExecutor,
-        table: Table,
-        table_name: &str,
-    ) -> Result<(), Error> {
-        let plugin = self.plugins.get(driver_type);
-        match plugin {
-            None => Err(Error::from("not support or load plugin!")),
-            Some(plugin) => {
-                log::info!("sync table_name:{}", table_name);
-                plugin.sync(rb, to_value!(table), table_name).await
-            }
-        }
-    }
 }
