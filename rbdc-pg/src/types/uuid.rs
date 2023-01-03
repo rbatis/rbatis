63156c7dede7dad89b4eae36b55093aa58119e4e
zxj <zhuxiujia@qq.com>
2023-01-03 10:38:41 +0800

    fix Uuid

--- rbdc-pg/src/types/uuid.rs
+++ rbdc-pg/src/types/uuid.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::arguments::PgArgumentBuffer;
 use crate::types::decode::Decode;
 use crate::types::encode::{Encode, IsNull};
@@ -7,7 +8,8 @@ use rbdc::Error;
 
 impl Encode for Uuid {
     fn encode(self, buf: &mut PgArgumentBuffer) -> Result<IsNull, Error> {
-        buf.extend_from_slice(self.0.as_bytes());
+        let uuid = uuid::Uuid::from_str(&self.0).map_err(|e|Error::from(e.to_string()))?;
+        buf.extend_from_slice(uuid.as_bytes());
         Ok(IsNull::No)
     }
 }
