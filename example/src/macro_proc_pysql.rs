330d04f4936b2bf1fa13baa9b1d98bf6540b2587
zxj <zhuxiujia@qq.com>
2023-01-03 17:42:50 +0800

    add example

--- example/src/macro_proc_pysql.rs
+++ example/src/macro_proc_pysql.rs
@@ -7,21 +7,25 @@ use crate::{init_db, BizActivity};
 use model::*;
 use rbatis::executor::Executor;
 use rbatis::Error;
-
+use crate::rbatis::sql::IntoSql;
 #[py_sql(
     "`select * from biz_activity where delete_flag = 0`
                   if name != '':
-                    ` and name=#{name}`"
+                    ` and name=#{name}`
+                  if !ids.is_empty():
+                    ` and id in `
+                    ${ids.sql()}"
 )]
-async fn py_select(rb: &mut dyn Executor, name: &str) -> Result<Vec<BizActivity>, Error> {
+async fn py_select(rb: &mut dyn Executor, name: &str,ids: &[i32]) -> Result<Vec<BizActivity>, Error> {
     impled!()
 }
 
 #[tokio::main]
 pub async fn main() {
-    fast_log::init(fast_log::Config::new().console()).expect("rbatis init fail");
+    let l =fast_log::init(fast_log::Config::new().console()).expect("rbatis init fail");
     //use static ref
     let rb = init_db().await;
-    let a = py_select(&mut rb.clone(), "").await.unwrap();
+    let a = py_select(&mut rb.clone(), "",&[1,2,3]).await.unwrap();
     println!(">>>>>>>>>>>> {:?}", a);
+    l.wait();
 }
