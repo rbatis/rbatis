398484dad0963a19b87c943929fc884cfb7a0511
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-01-08 22:49:48 +0800

    fix line \n

--- tests/parser_html.rs
+++ tests/parser_html.rs
@@ -1,6 +1,38 @@
 #[cfg(test)]
 mod test {
-    use rbatis_codegen::codegen::parser_html::load_mapper_map;
+    use rbatis_codegen::codegen::parser_html::{load_mapper_map, parse_html};
+
+    #[test]
+    fn test_parse_line_feed() {
+        let mut ig=vec![];
+       let token= parse_html(r#"
+       <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "https://raw.githubusercontent.com/rbatis/rbatis/master/rbatis-codegen/mybatis-3-mapper.dtd">
+        <mapper>
+            <select id="select_by_condition">
+        `select * `
+        ` from biz_activity`
+         </select>
+        </mapper>"#,"select_by_condition",&mut ig);
+        let code=token.to_string();
+        println!("{}",token);
+        assert_eq!(code.contains(r#"select * \n from biz_activity"#),true);
+    }
+
+    #[test]
+    fn test_parse_line_feed2() {
+        let mut ig=vec![];
+        let token= parse_html(r#"
+       <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "https://raw.githubusercontent.com/rbatis/rbatis/master/rbatis-codegen/mybatis-3-mapper.dtd">
+        <mapper>
+            <select id="select_by_condition">
+        `select * from biz_activity
+         where`
+         </select>
+        </mapper>"#,"select_by_condition",&mut ig);
+        let code=token.to_string();
+        println!("{}",token);
+        assert_eq!(code.contains(r#"select * from biz_activity\nwhere"#),true);
+    }
 
     #[test]
     fn test_load_html_include_replace() {
