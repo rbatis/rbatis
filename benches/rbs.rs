208db3b13b937baa63c2c138969c280889212375
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-03-19 12:32:24 +0800

    support string ops

--- benches/rbs.rs
+++ benches/rbs.rs
@@ -7,7 +7,7 @@ use rbs::value::util::extract_number;
 //test bench_extract_number ... bench:           3 ns/iter (+/- 0)
 #[bench]
 fn bench_extract_number(b: &mut Bencher) {
-    let v ="1.111";
+    let v ="dsafasdfasdfasdfasdf1.111gdfsgdsfgsdfg";
     b.iter(|| {
        extract_number(v);
     });
