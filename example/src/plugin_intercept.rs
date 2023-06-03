cd752cf78a7f05b756d513dc4eb56637e88a36d5
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-06-04 02:59:13 +0800

    update doc

--- example/src/plugin_intercept.rs
+++ example/src/plugin_intercept.rs
@@ -1,40 +1,36 @@
 pub mod init;
 use crate::init::init_db;
-use rbatis::intercept::{ResultType, SqlIntercept};
+use rbatis::intercept::{SqlIntercept};
 use rbatis::rbdc::datetime::DateTime;
 use rbatis::{crud, Error};
 use rbs::Value;
 use serde_json::json;
 use rbatis::executor::Executor;
-use rbatis::rbdc::db::ExecResult;
 
 /// Logic delete： The deletion statement changes to the modification of flag, and the query statement filters flag with additional conditions
 pub struct LogicDeletePlugin {}
 
 impl SqlIntercept for LogicDeletePlugin {
-    fn do_intercept(
+    fn before(
         &self,
         _task_id: i64,
         _rb: &dyn Executor,
         sql: &mut String,
         _args: &mut Vec<Value>,
-        result: Option<Result<ResultType<&ExecResult, &Vec<Value>>, &Error>>,
     ) -> Result<(), Error> {
-        if let None = result{
-            if sql.contains("delete from ") {
-                let table_name =
-                    sql[sql.find("from").unwrap_or(0) + 4..sql.find("where").unwrap_or(0)].trim();
-                println!("[LogicDeletePlugin] before=> {}", sql);
-                *sql = sql.replace(
-                    &format!("delete from {}", table_name),
-                    &format!("update {} set delete_flag = 1 ", table_name),
-                );
-                println!("[LogicDeletePlugin] after=> {}", sql);
-            } else if sql.contains("select ") && sql.contains(" where ") {
-                println!("[LogicDeletePlugin] before=> {}", sql);
-                sql.push_str(" and delete_flag = 0 ");
-                println!("[LogicDeletePlugin] after=> {}", sql);
-            }
+        if sql.contains("delete from ") {
+            let table_name =
+                sql[sql.find("from").unwrap_or(0) + 4..sql.find("where").unwrap_or(0)].trim();
+            println!("[LogicDeletePlugin] before=> {}", sql);
+            *sql = sql.replace(
+                &format!("delete from {}", table_name),
+                &format!("update {} set delete_flag = 1 ", table_name),
+            );
+            println!("[LogicDeletePlugin] after=> {}", sql);
+        } else if sql.contains("select ") && sql.contains(" where ") {
+            println!("[LogicDeletePlugin] before=> {}", sql);
+            sql.push_str(" and delete_flag = 0 ");
+            println!("[LogicDeletePlugin] after=> {}", sql);
         }
         Ok(())
     }
