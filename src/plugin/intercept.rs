7957e25275ae5d2da7df0484c7d7e5b0f3f24c75
zxj <zhuxiujia@qq.com>
2023-06-05 12:08:26 +0800

    add test

--- src/plugin/intercept.rs
+++ src/plugin/intercept.rs
@@ -20,6 +20,7 @@ pub trait Intercept: Send + Sync + Debug {
 
     /// task_id maybe is conn_id or tx_id,
     /// is_prepared_sql = !args.is_empty(),
+    #[inline]
     fn before(
         &self,
         _task_id: i64,
@@ -32,6 +33,7 @@ pub trait Intercept: Send + Sync + Debug {
 
     /// task_id maybe is conn_id or tx_id,
     /// is_prepared_sql = !args.is_empty(),
+    #[inline]
     fn after(
         &self,
         _task_id: i64,
---
+++ tests/intercept_test.rs
@@ -0,0 +1,174 @@
+#[cfg(test)]
+mod test {
+    use std::any::Any;
+    use std::sync::OnceLock;
+    use futures_core::future::BoxFuture;
+    use log::{ LevelFilter, Log, Metadata, Record};
+    use rbatis::{Error, RBatis};
+    use rbdc::db::{Connection, ConnectOptions, Driver, ExecResult, MetaData, Row};
+    use rbs::Value;
+    use rbdc::rt::block_on;
+
+    pub struct Logger {}
+
+    impl Log for Logger {
+        fn enabled(&self, _metadata: &Metadata) -> bool {
+            return true;
+        }
+
+        fn log(&self, record: &Record) {
+            println!("[{}]{}",record.module_path_static().unwrap(),record.args())
+        }
+
+        fn flush(&self) {}
+    }
+
+    pub static LOGGER: OnceLock<Logger> = OnceLock::new();
+
+
+    #[derive(Debug, Clone)]
+    struct MockDriver {}
+
+    impl Driver for MockDriver {
+        fn name(&self) -> &str {
+            "test"
+        }
+
+        fn connect(&self, _url: &str) -> BoxFuture<Result<Box<dyn Connection>, Error>> {
+            Box::pin(async { Ok(Box::new(MockConnection {}) as Box<dyn Connection>) })
+        }
+
+        fn connect_opt<'a>(
+            &'a self,
+            _opt: &'a dyn ConnectOptions,
+        ) -> BoxFuture<Result<Box<dyn Connection>, Error>> {
+            Box::pin(async { Ok(Box::new(MockConnection {}) as Box<dyn Connection>) })
+        }
+
+        fn default_option(&self) -> Box<dyn ConnectOptions> {
+            Box::new(MockConnectOptions {})
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    struct MockRowMetaData {
+        sql: String,
+    }
+
+    impl MetaData for MockRowMetaData {
+        fn column_len(&self) -> usize {
+            if self.sql.contains("select count") {
+                1
+            } else {
+                2
+            }
+        }
+
+        fn column_name(&self, i: usize) -> String {
+            if self.sql.contains("select count") {
+                "count".to_string()
+            } else {
+                if i == 0 {
+                    "sql".to_string()
+                } else {
+                    "count".to_string()
+                }
+            }
+        }
+
+        fn column_type(&self, _i: usize) -> String {
+            "String".to_string()
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    struct MockRow {
+        pub sql: String,
+        pub count: u64,
+    }
+
+    impl Row for MockRow {
+        fn meta_data(&self) -> Box<dyn MetaData> {
+            Box::new(MockRowMetaData {
+                sql: self.sql.clone(),
+            }) as Box<dyn MetaData>
+        }
+
+        fn get(&mut self, i: usize) -> Result<Value, Error> {
+            if self.sql.contains("select count") {
+                Ok(Value::U64(self.count))
+            } else {
+                if i == 0 {
+                    Ok(Value::String(self.sql.clone()))
+                } else {
+                    Ok(Value::U64(self.count.clone()))
+                }
+            }
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    struct MockConnection {}
+
+    impl Connection for MockConnection {
+        fn get_rows(
+            &mut self,
+            sql: &str,
+            _params: Vec<Value>,
+        ) -> BoxFuture<Result<Vec<Box<dyn Row>>, Error>> {
+            let sql = sql.to_string();
+            Box::pin(async move {
+                let data = Box::new(MockRow { sql: sql, count: 1 }) as Box<dyn Row>;
+                Ok(vec![data])
+            })
+        }
+
+        fn exec(&mut self, _sql: &str, _params: Vec<Value>) -> BoxFuture<Result<ExecResult, Error>> {
+            Box::pin(async move {
+                Ok(ExecResult {
+                    rows_affected: 0,
+                    last_insert_id: Value::Null,
+                })
+            })
+        }
+
+        fn close(&mut self) -> BoxFuture<Result<(), Error>> {
+            Box::pin(async { Ok(()) })
+        }
+
+        fn ping(&mut self) -> BoxFuture<Result<(), Error>> {
+            Box::pin(async { Ok(()) })
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    struct MockConnectOptions {}
+
+    impl ConnectOptions for MockConnectOptions {
+        fn connect(&self) -> BoxFuture<Result<Box<dyn Connection>, Error>> {
+            Box::pin(async { Ok(Box::new(MockConnection {}) as Box<dyn Connection>) })
+        }
+
+        fn set_uri(&mut self, _uri: &str) -> Result<(), Error> {
+            Ok(())
+        }
+
+        fn uppercase_self(&self) -> &(dyn Any + Send + Sync) {
+            self
+        }
+    }
+
+    #[test]
+    fn test_to_snake_name() {
+        _ = LOGGER.set(Logger {});
+        log::set_logger(LOGGER.get().unwrap())
+            .map(|()| log::set_max_level(LevelFilter::Info)).unwrap();
+        let rb = RBatis::new();
+
+        rb.init(MockDriver {}, "test").unwrap();
+        let f = async move {
+            _=rb.exec("select * from table", vec![]).await;
+        };
+        block_on(f);
+    }
+}
