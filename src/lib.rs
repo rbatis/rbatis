d0a7419049eb79920101d6d6f703e1ad7f68462f
zxj <zhuxiujia@qq.com>
2023-11-02 14:29:17 +0800

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub extern crate rbatis_codegen;
 extern crate rbatis_macro_driver;
 pub extern crate rbdc;
 
-pub use rbatis_macro_driver::{html_sql, py_sql, sql, string_fn};
+pub use rbatis_macro_driver::{html_sql, py_sql, sql};
 
 pub mod plugin;
 
