f061e3d67dfd532296b32167dacf8d8a64ae2010
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-11-10 23:25:34 +0800

    fix try_acquire

--- rbdc-pool-mobc/src/lib.rs
+++ rbdc-pool-mobc/src/lib.rs
@@ -54,7 +54,15 @@ impl Pool for MobcPool {
         Ok(Box::new(proxy))
     }
 
-    async fn get_timeout(&self, d: Duration) -> Result<Box<dyn Connection>, Error> {
+    async fn get_timeout(&self, mut d: Duration) -> Result<Box<dyn Connection>, Error> {
+        if d.is_zero() {
+            let state = self.inner.state().await;
+            if state.in_use < state.max_open {
+                d = Duration::from_secs(10);
+            } else {
+                return Err(Error::from("get timeout"));
+            }
+        }
         let v = self.inner.get_timeout(d).await.map_err(|e| Error::from(e.to_string()))?;
         let proxy = ConnManagerProxy {
             inner: v.manager_proxy.clone(),
