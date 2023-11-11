22f382136a63054371075abe004d8cd9445e6a8b
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-11-11 16:34:01 +0800

    clear dead code

--- tests/rbatis_sql_page_test.rs
+++ tests/rbatis_sql_page_test.rs
@@ -14,18 +14,18 @@ mod tests {
         let data = make_pages();
         let page_req = PageRequest::default();
         for i in 0..data.len() {
-            let (page_no, page_size, total, search_count) = (
+            let (page_no, page_size, total, do_count) = (
                 data[i][0] as u64,
                 data[i][1] as u64,
                 data[i][2] as u64,
                 data[i][3] != 0,
             );
             let mut pr = PageRequest::new_total(page_no, page_size, total);
-            pr = pr.set_do_count(search_count);
+            pr = pr.set_do_count(do_count);
             assert_eq!(pr.page_size(), page_size);
             assert_eq!(pr.page_no(), if page_no < 1 { 1 } else { page_no });
             assert_eq!(pr.total(), total);
-            assert_eq!(pr.do_count(), search_count);
+            assert_eq!(pr.do_count(), do_count);
 
             let mut pr = PageRequest::new_option(
                 if page_no == 0 { None } else { Some(page_no) },
@@ -35,22 +35,22 @@ mod tests {
                     Some(page_size)
                 },
             );
-            pr = pr.set_do_count(search_count);
+            pr = pr.set_do_count(do_count);
             assert_eq!(pr.page_size(), page_size);
             assert_eq!(pr.page_no(), if page_no < 1 { 1 } else { page_no });
-            assert_eq!(pr.do_count(), search_count);
+            assert_eq!(pr.do_count(), do_count);
 
             let mut pr = page_req
                 .clone()
                 .set_page_no(page_no)
                 .set_page_size(page_size)
                 .set_total(total)
-                .set_do_count(search_count);
-            pr = pr.set_do_count(search_count);
+                .set_do_count(do_count);
+            pr = pr.set_do_count(do_count);
             assert_eq!(pr.page_size(), page_size);
             assert_eq!(pr.page_no(), if page_no < 1 { 1 } else { page_no });
             assert_eq!(pr.total(), total);
-            assert_eq!(pr.do_count(), search_count);
+            assert_eq!(pr.do_count(), do_count);
         }
     }
 }
