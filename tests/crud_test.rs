294738178f53a1661ddfc561e3f111d92fd6d5e5
zxj <zhuxiujia@qq.com>
2023-12-26 16:13:14 +0800

    add rbdc-pool-fast

--- tests/crud_test.rs
+++ tests/crud_test.rs
@@ -16,13 +16,12 @@ mod test {
     use rbatis::executor::{Executor, RBatisConnExecutor};
     use rbatis::intercept::{Intercept, ResultType};
     use rbatis::sql::PageRequest;
-    use rbatis::{Error, RBatis};
+    use rbatis::{DefaultPool, Error, RBatis};
     use rbdc::datetime::DateTime;
     use rbdc::db::{ConnectOptions, Connection, Driver, ExecResult, MetaData, Row};
     use rbdc::pool::conn_manager::ConnManager;
     use rbdc::pool::Pool;
     use rbdc::rt::block_on;
-    use rbdc_pool_mobc::MobcPool;
     use rbs::{from_value, to_value, Value};
     use std::any::Any;
     use std::collections::HashMap;
@@ -267,7 +266,7 @@ mod test {
             let mut rb = RBatis::new();
             let mut opts = MockConnectOptions {};
             opts.set_uri("test");
-            rb.init_option::<MockDriver, MockConnectOptions, MobcPool>(MockDriver {}, opts)
+            rb.init_option::<MockDriver, MockConnectOptions, DefaultPool>(MockDriver {}, opts)
                 .unwrap();
             rb.acquire().await.unwrap();
         };
@@ -280,7 +279,7 @@ mod test {
             let mut rb = RBatis::new();
             let mut opts = MockConnectOptions {};
             opts.set_uri("test");
-            let pool: MobcPool = MobcPool::new(ConnManager::new_opt_box(
+            let pool = DefaultPool::new(ConnManager::new_opt_box(
                 Box::new(MockDriver {}),
                 Box::new(opts),
             ))
