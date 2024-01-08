5cbf0144df9c747ac525b206295ede926e8a1f75
zhuxiujia@qq.com <zhuxiujia@qq.com>
2024-01-08 20:06:09 +0800

    array save to json type

--- rbdc-mysql/src/types/json.rs
+++ rbdc-mysql/src/types/json.rs
@@ -3,6 +3,7 @@ use crate::types::{Decode, Encode};
 use crate::value::MySqlValue;
 use rbdc::json::Json;
 use rbdc::Error;
+use rbs::Value;
 
 impl Encode for Json {
     fn encode(self, buf: &mut Vec<u8>) -> Result<usize, Error> {
@@ -16,4 +17,16 @@ impl Decode for Json {
     fn decode(value: MySqlValue) -> Result<Self, Error> {
         Ok(Self(value.as_str().unwrap_or("null").to_string()))
     }
+}
+
+pub fn encode_json(arg:Value,buf: &mut Vec<u8>) -> Result<usize, Error> {
+    let bytes = arg.to_string().into_bytes();
+    let len = bytes.len();
+    buf.put_bytes_lenenc(bytes);
+    Ok(len)
+}
+
+pub fn decode_json(value: MySqlValue) -> Result<Value, Error> {
+    let v= value.as_str().unwrap_or("null").to_string();
+    Ok(serde_json::from_str(&v).map_err(|e|Error::from(e.to_string()))?)
 }
