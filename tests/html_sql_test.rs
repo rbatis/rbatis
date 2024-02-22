47f9b182cd25f673e4c7c7974464e7f38b416512
zxj <zhuxiujia@qq.com>
2024-02-22 10:06:30 +0800

    add test

--- tests/html_sql_test.rs
+++ tests/html_sql_test.rs
@@ -564,4 +564,36 @@ mod test {
         };
         block_on(f);
     }
+
+    #[test]
+    fn test_include() {
+        let f = async move {
+            let mut rb = RBatis::new();
+            rb.init(MockDriver {}, "test").unwrap();
+            let queue = Arc::new(SyncVec::new());
+            rb.set_intercepts(vec![Arc::new(MockIntercept::new(queue.clone()))]);
+            htmlsql!(test_include(rb: &RBatis)  -> Result<Value, Error> => "tests/test.html");
+            let r = test_include(&rb).await.unwrap();
+            let (sql, args) = queue.pop().unwrap();
+            assert_eq!(sql, "1");
+            assert_eq!(args, vec![]);
+        };
+        block_on(f);
+    }
+
+    #[test]
+    fn test_set() {
+        let f = async move {
+            let mut rb = RBatis::new();
+            rb.init(MockDriver {}, "test").unwrap();
+            let queue = Arc::new(SyncVec::new());
+            rb.set_intercepts(vec![Arc::new(MockIntercept::new(queue.clone()))]);
+            htmlsql!(test_set(rb: &RBatis)  -> Result<Value, Error> => "tests/test.html");
+            let r = test_set(&rb).await.unwrap();
+            let (sql, args) = queue.pop().unwrap();
+            assert_eq!(sql, " set 1 ");
+            assert_eq!(args, vec![]);
+        };
+        block_on(f);
+    }
 }
