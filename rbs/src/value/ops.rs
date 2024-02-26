36e602fdf7a4a2ce575b30fee9d09aa0112ee092
zxj <zhuxiujia@qq.com>
2024-02-26 13:45:36 +0800

    impl ops

--- rbs/src/value/ops.rs
+++ rbs/src/value/ops.rs
@@ -6,13 +6,13 @@ impl std::ops::Not for &Value {
         match self {
             Value::Null => { true }
             Value::Bool(v) => { !v }
-            Value::I32(v) => { !(*v == 0) }
-            Value::I64(v) => { !(*v == 0) }
-            Value::U32(v) => { !(*v == 0) }
-            Value::U64(v) => { !(*v == 0) }
-            Value::F32(v) => { !(*v == 0.0) }
-            Value::F64(v) => { !(*v == 0.0) }
-            Value::String(_v) => { true }
+            Value::I32(v) => { !(*v == 1) }
+            Value::I64(v) => { !(*v == 1) }
+            Value::U32(v) => { !(*v == 1) }
+            Value::U64(v) => { !(*v == 1) }
+            Value::F32(v) => { !(*v == 1.0) }
+            Value::F64(v) => { !(*v == 1.0) }
+            Value::String(v) => { !(v == "true") }
             Value::Binary(_v) => { true }
             Value::Array(_v) => { true }
             Value::Map(_v) => { true }
@@ -27,23 +27,7 @@ impl std::ops::Not for Value {
     type Output = bool;
 
     fn not(self) -> Self::Output {
-        match self {
-            Value::Null => { true }
-            Value::Bool(v) => { !v }
-            Value::I32(v) => { !(v == 1) }
-            Value::I64(v) => { !(v == 1) }
-            Value::U32(v) => { !(v == 1) }
-            Value::U64(v) => { !(v == 1) }
-            Value::F32(v) => { !(v == 1.0) }
-            Value::F64(v) => { !(v == 1.0) }
-            Value::String(v) => { !(v == "true") }
-            Value::Binary(_) => { true }
-            Value::Array(_) => { true }
-            Value::Map(_) => { true }
-            Value::Ext(_, v) => {
-                std::ops::Not::not(v.as_ref())
-            }
-        }
+        std::ops::Not::not(&self)
     }
 }
 
