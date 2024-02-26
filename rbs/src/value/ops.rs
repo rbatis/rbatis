a11d83b54ffdcdb596ce201e9397d8fdbd0882a1
zxj <zhuxiujia@qq.com>
2024-02-26 13:44:04 +0800

    impl ops

---
+++ rbs/src/value/ops.rs
@@ -0,0 +1,59 @@
+use crate::Value;
+
+impl std::ops::Not for &Value {
+    type Output = bool;
+    fn not(self) -> Self::Output {
+        match self {
+            Value::Null => { true }
+            Value::Bool(v) => { !v }
+            Value::I32(v) => { !(*v == 0) }
+            Value::I64(v) => { !(*v == 0) }
+            Value::U32(v) => { !(*v == 0) }
+            Value::U64(v) => { !(*v == 0) }
+            Value::F32(v) => { !(*v == 0.0) }
+            Value::F64(v) => { !(*v == 0.0) }
+            Value::String(_v) => { true }
+            Value::Binary(_v) => { true }
+            Value::Array(_v) => { true }
+            Value::Map(_v) => { true }
+            Value::Ext(_, v) => {
+                std::ops::Not::not(v.as_ref())
+            }
+        }
+    }
+}
+
+impl std::ops::Not for Value {
+    type Output = bool;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Value::Null => { true }
+            Value::Bool(v) => { !v }
+            Value::I32(v) => { !(v == 1) }
+            Value::I64(v) => { !(v == 1) }
+            Value::U32(v) => { !(v == 1) }
+            Value::U64(v) => { !(v == 1) }
+            Value::F32(v) => { !(v == 1.0) }
+            Value::F64(v) => { !(v == 1.0) }
+            Value::String(v) => { !(v == "true") }
+            Value::Binary(_) => { true }
+            Value::Array(_) => { true }
+            Value::Map(_) => { true }
+            Value::Ext(_, v) => {
+                std::ops::Not::not(v.as_ref())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Value;
+
+    #[test]
+    fn trestnot() {
+        let v = Value::Null;
+        assert_eq!(!v,true);
+    }
+}
