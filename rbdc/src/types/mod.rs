53d817d6d92836c57b4f2b66b39390172b7cd581
zxj <zhuxiujia@qq.com>
2024-04-19 10:01:53 +0800

    add doc

--- rbdc/src/types/mod.rs
+++ rbdc/src/types/mod.rs
@@ -1,10 +1,18 @@
 pub mod bytes;
+//'2024-12-12'
 pub mod date;
+///`2024-04-19T09:59:39.016756+08:00`
 pub mod datetime;
+/// 123456
 pub mod decimal;
+/// `{"a":"b"}`
+/// `[{"a":"b"}]`
 pub mod json;
+/// `00:00:00.000000`
 pub mod time;
+/// 1713491896
 pub mod timestamp;
+/// `00000000-0000-0000-0000-000000000000`
 pub mod uuid;
 
 pub use self::bytes::*;
