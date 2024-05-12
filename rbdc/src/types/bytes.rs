315b4cfef49cab73a4121d0df6e5e2bce5df3a3e
zxj <zhuxiujia@qq.com>
2024-05-12 20:30:21 +0800

    add BytesSize

--- rbdc/src/types/bytes.rs
+++ rbdc/src/types/bytes.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::cmp::Ordering;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::hash::{Hash, Hasher};
+use std::io;
+use std::io::ErrorKind;
 use std::ops::{Deref, DerefMut};
+use std::str::FromStr;
 use std::vec::IntoIter;
 
 /// rbatis bytes use serde_bytes
@@ -43,8 +46,8 @@ impl Debug for Bytes {
 }
 
 impl<Rhs> PartialEq<Rhs> for Bytes
-where
-    Rhs: ?Sized + AsRef<[u8]>,
+    where
+        Rhs: ?Sized + AsRef<[u8]>,
 {
     fn eq(&self, other: &Rhs) -> bool {
         self.as_ref().eq(other.as_ref())
@@ -52,8 +55,8 @@ where
 }
 
 impl<Rhs> PartialOrd<Rhs> for Bytes
-where
-    Rhs: ?Sized + AsRef<[u8]>,
+    where
+        Rhs: ?Sized + AsRef<[u8]>,
 {
     fn partial_cmp(&self, other: &Rhs) -> Option<Ordering> {
         self.as_ref().partial_cmp(other.as_ref())
@@ -143,8 +146,8 @@ impl<'a> IntoIterator for &'a mut Bytes {
 
 impl Serialize for Bytes {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
+        where
+            S: Serializer,
     {
         let inner = serde_bytes::Bytes::new(&self.inner);
         inner.serialize(serializer)
@@ -153,12 +156,128 @@ impl Serialize for Bytes {
 
 impl<'de> Deserialize<'de> for Bytes {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
+        where
+            D: Deserializer<'de>,
     {
         let buf = serde_bytes::ByteBuf::deserialize(deserializer)?;
         Ok(Self {
             inner: buf.into_vec(),
         })
+    }
+}
+
+
+#[derive(Debug, Eq, PartialEq, PartialOrd)]
+pub struct BytesSize(pub i64);
+
+pub const EB: BytesSize = BytesSize(1024 * 1024 * 1024 * 1024 * 1024);
+pub const TB: BytesSize = BytesSize(1024 * 1024 * 1024 * 1024);
+pub const GB: BytesSize = BytesSize(1024 * 1024 * 1024);
+pub const MB: BytesSize = BytesSize(1024 * 1024);
+pub const KB: BytesSize = BytesSize(1024);
+pub const B: BytesSize = BytesSize(1);
+
+impl BytesSize {
+    pub fn display(&self) -> String {
+        format!("{}", self)
+    }
+
+    pub fn into_inner(self) -> i64 {
+        self.0
+    }
+}
+
+impl Serialize for BytesSize {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for BytesSize {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        let v = i64::deserialize(deserializer)?;
+        Ok(BytesSize(v))
+    }
+}
+
+impl Deref for BytesSize {
+    type Target = i64;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for BytesSize {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl From<i64> for BytesSize {
+    fn from(value: i64) -> Self {
+        Self(value)
+    }
+}
+
+impl Default for BytesSize {
+    fn default() -> Self {
+        BytesSize(0)
+    }
+}
+
+
+impl Display for BytesSize {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let s = self.0;
+        if s < KB.0 {
+            write!(f, "{:.2}B", s as f64 / B.0 as f64)
+        } else if s < MB.0 {
+            write!(f, "{:.2}KB", s as f64 / KB.0 as f64)
+        } else if s < GB.0 {
+            write!(f, "{:.2}MB", s as f64 / MB.0 as f64)
+        } else if s < TB.0 {
+            write!(f, "{:.2}GB", s as f64 / GB.0 as f64)
+        } else if s < EB.0 {
+            write!(f, "{:.2}TB", s as f64 / TB.0 as f64)
+        } else {
+            write!(f, "{:.2}EB", s as f64 / EB.0 as f64)
+        }
+    }
+}
+
+impl FromStr for BytesSize {
+    type Err = io::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.ends_with("EB") {
+            let v: f64 = s.trim_end_matches("EB").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * EB.0 as f64) as i64))
+        } else if s.ends_with("TB") {
+            let v: f64 = s.trim_end_matches("TB").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * TB.0 as f64) as i64))
+        } else if s.ends_with("GB") {
+            let v: f64 = s.trim_end_matches("GB").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * GB.0 as f64) as i64))
+        } else if s.ends_with("MB") {
+            let v: f64 = s.trim_end_matches("MB").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * MB.0 as f64) as i64))
+        } else if s.ends_with("KB") {
+            let v: f64 = s.trim_end_matches("KB").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * KB.0 as f64) as i64))
+        } else {
+            let v: f64 = s.trim_end_matches("B").parse().map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+            Ok(BytesSize((v * B.0 as f64) as i64))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::bytes::GB;
+
+    #[test]
+    fn test_display() {
+        assert_eq!(format!("{}", GB), "1.00GB");
     }
 }
