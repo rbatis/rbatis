68d8f76f5aa248cb17e93213bfeefe1d18fb717d
zxj <zhuxiujia@qq.com>
2024-06-01 00:27:59 +0800

    rename table_util.rs

--- src/utils/table_util.rs
+++ src/utils/table_util.rs
@@ -6,13 +6,13 @@
 /// }
 ///
 /// //step2: make struct
-/// let activity = rbatis::make_table!(MockTable{
+/// let activity = rbatis::table!(MockTable{
 ///             id : "12312".to_string(),
 ///             delete_flag : 1,
 ///             name:  None,
 ///             });
 #[macro_export]
-macro_rules! make_table {
+macro_rules! table {
         ($t:path{ $($key:ident:$value:expr$(,)?)+ }) => {
            {
             let mut temp_table_data = <$t>::default();
@@ -30,13 +30,13 @@ macro_rules! make_table {
 ///         pub role_id:String
 ///      }
 ///      let user_roles: Vec<SysUserRole>;
-///      let role_ids:Vec<String> = make_table_field_vec!(&user_roles,role_id);
+///      let role_ids:Vec<String> = table_field_vec!(&user_roles,role_id);
 ///
 ///
 ///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! make_table_field_vec {
+macro_rules! table_field_vec {
     ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
         let mut ids = std::vec::Vec::with_capacity($vec_ref.len());
         for item in $vec_ref {
@@ -60,13 +60,13 @@ macro_rules! make_table_field_vec {
 ///         pub role_id:String
 ///      }
 ///      let user_roles: Vec<SysUserRole>;
-///      let role_ids:HashSet<String> = make_table_field_vec!(&user_roles,role_id);
+///      let role_ids:HashSet<String> = table_field_vec!(&user_roles,role_id);
 ///
 ///
 ///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! make_table_field_set {
+macro_rules! table_field_set {
     ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
         let mut ids = std::collections::HashSet::with_capacity($vec_ref.len());
         for item in $vec_ref {
@@ -90,13 +90,13 @@ macro_rules! make_table_field_set {
 ///         pub role_id:String
 ///      }
 ///      let user_roles: Vec<SysUserRole>;
-///      let role_ids: HashMap<String,SysUserRole> = make_table_field_map!(&user_roles,role_id);
+///      let role_ids: HashMap<String,SysUserRole> = table_field_map!(&user_roles,role_id);
 ///
 ///
 ///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! make_table_field_map {
+macro_rules! table_field_map {
     ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
         let mut ids = std::collections::HashMap::with_capacity($vec_ref.len());
         for item in $vec_ref {
@@ -120,13 +120,13 @@ macro_rules! make_table_field_map {
 ///         pub role_id:String
 ///      }
 ///      let user_roles: Vec<SysUserRole>;
-///      let role_ids = make_table_field_map_btree!(&user_roles,role_id); // role_ids: HashMap<String,SysUserRole>
+///      let role_ids = table_field_map_btree!(&user_roles,role_id); // role_ids: HashMap<String,SysUserRole>
 ///
 ///
 ///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! make_table_field_map_btree {
+macro_rules! table_field_map_btree {
     ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
         let mut ids = std::collections::BTreeMap::new();
         for item in $vec_ref {
@@ -195,4 +195,72 @@ macro_rules! field_key {
         }
         stringify!($field3).trim_start_matches("r#")
     }};
+}
+
+
+
+
+#[deprecated(note = "please use table_field_map_btree!")]
+#[macro_export]
+macro_rules! make_table_field_map_btree {
+    ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
+        let mut ids = std::collections::BTreeMap::new();
+        for item in $vec_ref {
+             match &item $(.$field_name)+ {
+                std::option::Option::Some(v) => {
+                    ids.insert(v.clone(), item.clone());
+                }
+                _ => {}
+            }
+        }
+        ids
+    }};
+}
+
+#[deprecated(note = "please use table_field_map!")]
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! make_table_field_map {
+    ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
+        let mut ids = std::collections::HashMap::with_capacity($vec_ref.len());
+        for item in $vec_ref {
+              match &item $(.$field_name)+ {
+                std::option::Option::Some(v) => {
+                    ids.insert(v.clone(), item.clone());
+                }
+                _ => {}
+            }
+        }
+        ids
+    }};
+}
+
+#[deprecated(note = "please use table_field_vec!")]
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! make_table_field_vec {
+    ($vec_ref:expr,$($field_name:ident$(.)?)+) => {{
+        let mut ids = std::vec::Vec::with_capacity($vec_ref.len());
+        for item in $vec_ref {
+            match &item $(.$field_name)+ {
+                std::option::Option::Some(v) => {
+                    ids.push(v.clone());
+                }
+                _ => {}
+            }
+        }
+        ids
+    }};
+}
+
+#[deprecated(note = "please use table!")]
+#[macro_export]
+macro_rules! make_table {
+        ($t:path{ $($key:ident:$value:expr$(,)?)+ }) => {
+           {
+            let mut temp_table_data = <$t>::default();
+            $(temp_table_data.$key = $value.into();)+
+            temp_table_data
+           }
+        }
 }
