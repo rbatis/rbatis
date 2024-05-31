b79db2ff6f6991a9579eebcf993f8acfa6b8eb81
zxj <zhuxiujia@qq.com>
2024-06-01 03:51:22 +0800

    add util example

---
+++ example/src/table_util.rs
@@ -0,0 +1,43 @@
+use rbatis::rbdc::DateTime;
+use rbatis::{table, table_field_btree, table_field_map, table_field_vec};
+
+#[derive(serde::Serialize, serde::Deserialize,Default,Debug)]
+pub struct Activity {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub pc_link: Option<String>,
+    pub h5_link: Option<String>,
+    pub pc_banner_img: Option<String>,
+    pub h5_banner_img: Option<String>,
+    pub sort: Option<String>,
+    pub status: Option<i32>,
+    pub remark: Option<String>,
+    pub create_time: Option<DateTime>,
+    pub version: Option<i64>,
+    pub delete_flag: Option<i32>,
+}
+
+fn main() {
+    let tables: Vec<Activity> = vec![
+        table!(Activity {
+            id: Some(3),
+            name: Some("3".to_string()),
+        }),
+        table!(Activity {
+            id: Some(2),
+            name: Some("2".to_string()),
+        }),
+        table!(Activity {
+            id: Some(1),
+            name: Some("1".to_string()),
+        })];
+    let hash = table_field_map!(&tables,id);
+    println!("---hash={}", rbs::to_value!(hash));
+    //auto sort by id
+    let btree = table_field_btree!(&tables,id);
+    println!("---btree={}", rbs::to_value!(btree));
+    let ids= table_field_vec!(&tables,id);
+    println!("---ids={}", rbs::to_value!(ids));
+    let ids= table_field_vec!(tables,id);
+    println!("---ids owned={:?}", ids);
+}
