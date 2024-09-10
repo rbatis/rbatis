9ff393dae6f5ceb4ffdb6a926a98d79fe4a45faa
zxj <zhuxiujia@qq.com>
2024-09-11 01:12:30 +0800

    fix Snowflake race generate

--- src/plugin/snowflake.rs
+++ src/plugin/snowflake.rs
@@ -123,7 +123,7 @@ impl Snowflake {
         let sequence = self.sequence.fetch_add(1, Ordering::SeqCst);
         // Shift and combine the components to generate the final ID.
         let timestamp_shifted = now << 22;
-        let worker_id_shifted = self.worker_id << 22;
+        let worker_id_shifted = self.worker_id << 14;
         let id = timestamp_shifted | worker_id_shifted | sequence;
         id
     }
