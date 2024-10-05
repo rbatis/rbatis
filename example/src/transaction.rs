ae1ee7c1fc1dc4fe5c144e42e5bb75cfaebf2674
zxj <zhuxiujia@qq.com>
2024-10-05 22:01:29 +0800

    update example

--- example/src/transaction.rs
+++ example/src/transaction.rs
@@ -32,11 +32,7 @@ pub async fn main() {
     // rb.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:123456@localhost:3306/test").unwrap();
     // rb.init(rbdc_pg::driver::PgDriver {}, "postgres://postgres:123456@localhost:5432/postgres").unwrap();
     // rb.init(rbdc_mssql::driver::MssqlDriver {}, "mssql://SA:TestPass!123456@localhost:1433/test").unwrap();
-    rb.init(
-        rbdc_sqlite::driver::SqliteDriver {},
-        "sqlite://target/sqlite.db",
-    )
-    .unwrap();
+    rb.init(rbdc_sqlite::driver::SqliteDriver {},"sqlite://target/sqlite.db").unwrap();
     // table sync done
     fast_log::logger().set_level(LevelFilter::Off);
     _ = RBatis::sync(
