437a00412f129cfff12a0d60e10c1905cf21f9aa
zxj <zhuxiujia@qq.com>
2024-10-24 19:45:24 +0800

    reset code

--- src/plugin/table_sync/mod.rs
+++ src/plugin/table_sync/mod.rs
@@ -119,28 +119,7 @@ pub fn sync<'a>(
                                 if k.eq("id") || v.as_str().unwrap_or_default() == "id" {
                                     id_key = &PRIMARY_KEY;
                                 }
-                                let mut column_type = mapper.get_column(k, &v);
-                                if id_key == PRIMARY_KEY && v.as_str().unwrap_or_default() == "id" && column_type == "id" {
-                                    let mut v = v.clone();
-                                    match &mut v {
-                                        Value::Null => {}
-                                        Value::Bool(_) => {}
-                                        Value::I32(_) => {}
-                                        Value::I64(_) => {}
-                                        Value::U32(_) => {}
-                                        Value::U64(_) => {}
-                                        Value::F32(_) => {}
-                                        Value::F64(_) => {}
-                                        Value::String(s) => {
-                                            s.clear();
-                                        }
-                                        Value::Binary(_) => {}
-                                        Value::Array(_) => {}
-                                        Value::Map(_) => {}
-                                        Value::Ext(_, _) => {}
-                                    }
-                                    column_type = mapper.get_column(k, &v);
-                                }
+                                let column_type = mapper.get_column(k, &v);
                                 match executor
                                     .exec(
                                         &format!(
