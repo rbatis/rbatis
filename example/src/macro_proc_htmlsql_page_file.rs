9cde57099516a14a661541fa9fffd380fb4dc32f
zxj <zhuxiujia@qq.com>
2024-12-01 19:52:09 +0800

    fix example

--- example/src/macro_proc_htmlsql_page_file.rs
+++ example/src/macro_proc_htmlsql_page_file.rs
@@ -41,8 +41,8 @@ pub async fn main() {
     // ------------choose database driver------------
     // rb.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:123456@localhost:3306/test").unwrap();
     // rb.init(rbdc_pg::driver::PgDriver {}, "postgres://postgres:123456@localhost:5432/postgres").unwrap();
-    // rb.init(rbdc_mssql::driver::MssqlDriver {}, "mssql://SA:TestPass!123456@localhost:1433/test").unwrap();
-    rb.init(rbdc_sqlite::driver::SqliteDriver {}, "sqlite://target/sqlite.db").unwrap();
+    rb.init(rbdc_mssql::driver::MssqlDriver {}, "jdbc:sqlserver://localhost:1433;User=SA;Password={TestPass!123456};Database=master;").unwrap();
+    //rb.init(rbdc_sqlite::driver::SqliteDriver {}, "sqlite://target/sqlite.db").unwrap();
     // table sync done
     fast_log::logger().set_level(LevelFilter::Off);
     _ = RBatis::sync(
