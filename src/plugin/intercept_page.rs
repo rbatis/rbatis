867787f83268460a12fc66a45d03a0665046a2f2
zxj <zhuxiujia@qq.com>
2024-12-04 20:29:53 +0800

    fix #567

--- src/plugin/intercept_page.rs
+++ src/plugin/intercept_page.rs
@@ -5,6 +5,7 @@ use async_trait::async_trait;
 use dark_std::sync::SyncHashMap;
 use rbdc::db::ExecResult;
 use rbs::Value;
+use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 
 /// make count sql remove `limit`
@@ -59,43 +60,46 @@ impl Intercept for PageIntercept {
         }
         if self.count_ids.contains_key(&executor.id()) {
             self.count_ids.remove(&executor.id());
-            if !(sql.contains("select ") && sql.contains(" from ")) {
-                return Err(Error::from(
-                    "PageIntercept sql must have select and from ",
-                ));
-            }
-            let start = sql.find("select ").unwrap_or(0) + "select ".len();
-            let end = sql.find(" from ").unwrap_or(0);
-            let v = &sql[start..end];
-            *sql = sql.replace(v, "count(1) as count").to_string();
-            if let Some(idx) = sql.rfind(" limit ") {
-                *sql = (&sql[..idx]).to_string();
+            if !(sql.trim_start().starts_with("select ") && sql.contains(" from ")) {
+                let start = sql.find("select ").unwrap_or(0) + "select ".len();
+                let end = sql.find(" from ").unwrap_or(0);
+                let v = &sql[start..end];
+                *sql = sql.replace(v, "count(1) as count").to_string();
+                if let Some(idx) = sql.rfind(" limit ") {
+                    *sql = (&sql[..idx]).to_string();
+                }
+            } else {
+                if executor.rb_ref().is_debug_mode() {
+                    log::debug!("PageIntercept unsupported sql: {}", sql);
+                }
             }
         }
         if self.select_ids.contains_key(&executor.id()) {
             let req = self.select_ids.remove(&executor.id());
-            if !(sql.contains("select ") && sql.contains(" from ")) {
-                return Err(Error::from(
-                    "PageIntercept sql must have select and from ",
-                ));
-            }
-            let driver_type = executor.driver_type().unwrap_or_default();
-            let mut templete = " limit ${page_no},${page_size} ".to_string();
-            if driver_type == "pg" || driver_type == "postgres" {
-                //postgres use `limit x offset x`
-                templete = " limit ${page_size} offset ${page_no}".to_string();
-            } else if driver_type == "mssql" {
-                //mssql must have `order by`, if you not add on sql.we will add this
-                if !sql.contains(" order by ") {
-                    sql.push_str(" order by id desc ");
+            if !(sql.trim_start().starts_with("select ") && sql.contains(" from ")) {
+                let driver_type = executor.driver_type().unwrap_or_default();
+                let mut templete = " limit ${page_no},${page_size} ".to_string();
+                if driver_type == "pg" || driver_type == "postgres" {
+                    //postgres use `limit x offset x`
+                    templete = " limit ${page_size} offset ${page_no}".to_string();
+                } else if driver_type == "mssql" {
+                    //mssql must have `order by`, if you not add on sql.we will add this
+                    if !sql.contains(" order by ") {
+                        sql.push_str(" order by id desc ");
+                    }
+                    templete =
+                        " offset ${page_no} rows fetch next ${page_size} rows only ".to_string();
                 }
-                templete = " offset ${page_no} rows fetch next ${page_size} rows only ".to_string();
-            }
-            if !sql.contains("limit") {
-                if let Some(req) = req {
-                    templete = templete.replace("${page_no}", &req.offset().to_string());
-                    templete = templete.replace("${page_size}", &req.page_size().to_string());
-                    sql.push_str(&templete);
+                if !sql.contains("limit") {
+                    if let Some(req) = req {
+                        templete = templete.replace("${page_no}", &req.offset().to_string());
+                        templete = templete.replace("${page_size}", &req.page_size().to_string());
+                        sql.push_str(&templete);
+                    }
+                }
+            } else {
+                if executor.rb_ref().is_debug_mode() {
+                    log::debug!("PageIntercept unsupported sql: {}", sql);
                 }
             }
         }
