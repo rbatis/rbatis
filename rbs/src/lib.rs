22693deacf4bc10c92bc08f65ee9945092286df2
zxj <zhuxiujia@qq.com>
2025-05-18 23:20:10 +0800

    fix tests

--- rbs/src/lib.rs
+++ rbs/src/lib.rs
@@ -7,6 +7,7 @@ pub mod value;
 
 mod value_serde;
 mod error;
+mod macros;
 
 pub use crate::error::Error;
 pub use value_serde::{from_value, from_value_ref};
@@ -59,91 +60,6 @@ impl Value {
     }
 }
 
-/// to_value macro
-///
-/// to_value! map
-///```rust
-/// let v=  rbs::to_value! {"1":"1",};
-///```
-/// to_value! expr
-///```rust
-/// let arg="1";
-/// let v =  rbs::to_value!(arg);
-///```
-/// 
-/// 支持任意层级的嵌套结构（嵌套JSON示例）:
-/// ```ignore
-/// // 这是一个嵌套JSON示例，支持任意层级:
-/// let v = rbs::to_value! {
-///     "id": 1, 
-///     "user": {
-///         "name": "Alice",
-///         "address": {
-///             "city": "Beijing",
-///             "street": {
-///                 "number": 123
-///             }
-///         }
-///     }
-/// };
-/// ```
-#[macro_export]
-macro_rules! to_value {
-    // object inner {}
-    ({$($k:tt: {$($ik:tt: $iv:tt),* $(,)*}),* $(,)*}) => {
-        {
-            let mut map = $crate::value::map::ValueMap::new();
-            $(
-                let inner_value = $crate::to_value!({$($ik: $iv),*});
-                map.insert($crate::to_value!($k), inner_value);
-            )*
-            $crate::Value::Map(map)
-        }
-    };
-    
-    // object inner {}
-    {$($k:tt: {$($ik:tt: $iv:tt),* $(,)*}),* $(,)*} => {
-        {
-            let mut map = $crate::value::map::ValueMap::new();
-            $(
-                let inner_value = $crate::to_value!({$($ik: $iv),*});
-                map.insert($crate::to_value!($k), inner_value);
-            )*
-            $crate::Value::Map(map)
-        }
-    };
-    
-    // object
-    ({$($k:tt: $v:tt),* $(,)*}) => {
-        {
-            let mut map = $crate::value::map::ValueMap::new();
-            $(
-                map.insert($crate::to_value!($k), $crate::to_value!($v));
-            )*
-            $crate::Value::Map(map)
-        }
-    };
-    
-    // k-v
-    ($($k:tt: $v:expr),* $(,)?) => {
-        {
-            let mut map = $crate::value::map::ValueMap::new();
-            $(
-                map.insert($crate::to_value!($k), $crate::to_value!($v));
-            )*
-            $crate::Value::Map(map)
-        }
-    };
-    
-    // expr/ident
-    ($arg:expr) => {
-        $crate::to_value($arg).unwrap_or_default()
-    };
-    
-    [$($arg:tt),*] => {
-        $crate::to_value([$($arg),*]).unwrap_or_default()
-    };
-}
 
 /// is debug mode
 pub fn is_debug_mode() -> bool {
---
+++ rbs/src/macros.rs
@@ -0,0 +1,73 @@
+/// to_value macro
+///
+/// to_value! map
+///```rust
+/// let v=  rbs::to_value! {"1":"1",};
+///```
+/// to_value! expr
+///```rust
+/// let arg="1";
+/// let v =  rbs::to_value!(arg);
+///```
+/// 
+/// JSON example:
+/// ```ignore
+/// let v = rbs::to_value! {
+///     "id": 1, 
+///     "user": {
+///         "name": "Alice",
+///         "address": {
+///             "city": "Beijing",
+///             "street": {
+///                 "number": 123
+///             }
+///         }
+///     }
+/// };
+/// ```
+#[macro_export]
+macro_rules! to_value {
+
+    // object inner {}
+    ($($k:tt: {$($ik:tt: $iv:tt),* $(,)*}),* $(,)*) => {
+        {
+            let mut map = $crate::value::map::ValueMap::new();
+            $(
+                let inner_value = $crate::to_value!({$($ik: $iv),*});
+                map.insert($crate::to_value!($k), inner_value);
+            )*
+            $crate::Value::Map(map)
+        }
+    };
+    
+    // object
+    ({$($k:tt: $v:tt),* $(,)*}) => {
+        {
+            let mut map = $crate::value::map::ValueMap::new();
+            $(
+                map.insert($crate::to_value!($k), $crate::to_value!($v));
+            )*
+            $crate::Value::Map(map)
+        }
+    };
+    
+    // k-v
+    ($($k:tt: $v:expr),* $(,)?) => {
+        {
+            let mut map = $crate::value::map::ValueMap::new();
+            $(
+                map.insert($crate::to_value!($k), $crate::to_value!($v));
+            )*
+            $crate::Value::Map(map)
+        }
+    };
+    
+    // expr/ident
+    ($arg:expr) => {
+        $crate::to_value($arg).unwrap_or_default()
+    };
+    
+    [$($arg:tt),*] => {
+        $crate::to_value([$($arg),*]).unwrap_or_default()
+    };
+}
