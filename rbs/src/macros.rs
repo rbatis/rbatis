c02b9a59b95dc9fb9313f64269394c81251d626c
zxj <zhuxiujia@qq.com>
2025-05-18 23:32:33 +0800

    fix tests

--- rbs/src/macros.rs
+++ rbs/src/macros.rs
@@ -27,31 +27,41 @@
 /// ```
 #[macro_export]
 macro_rules! to_value {
-
-    // object inner {}
-    ($($k:tt: {$($ik:tt: $iv:tt),* $(,)*}),* $(,)*) => {
+    // Handle empty object case
+    ({}) => {
+        $crate::Value::Map($crate::value::map::ValueMap::new())
+    };
+    
+    // Handle empty input
+    () => {
+        $crate::Value::Map($crate::value::map::ValueMap::new())
+    };
+    
+    // Handle nested objects with brace syntax {"key": {nested object}}
+    // This is a general rule for handling objects with nested objects
+    // Note: This rewrites the handling method, focusing on the internal block {}
+    {$($k:tt: $v:tt),* $(,)*} => {
         {
             let mut map = $crate::value::map::ValueMap::new();
             $(
-                let inner_value = $crate::to_value!({$($ik: $iv),*});
-                map.insert($crate::to_value!($k), inner_value);
+                $crate::to_value!(@map_entry map $k $v);
             )*
             $crate::Value::Map(map)
         }
     };
     
-    // object
+    // Handle object form with parentheses: to_value!({k:v})
     ({$($k:tt: $v:tt),* $(,)*}) => {
         {
             let mut map = $crate::value::map::ValueMap::new();
             $(
-                map.insert($crate::to_value!($k), $crate::to_value!($v));
+                $crate::to_value!(@map_entry map $k $v);
             )*
             $crate::Value::Map(map)
         }
     };
     
-    // k-v
+    // Handle key-value pairs: to_value!(k:v)
     ($($k:tt: $v:expr),* $(,)?) => {
         {
             let mut map = $crate::value::map::ValueMap::new();
@@ -62,13 +72,28 @@ macro_rules! to_value {
         }
     };
     
-    // expr/ident
-    ($arg:expr) => {
-        $crate::to_value($arg).unwrap_or_default()
+    // Array syntax: to_value![a, b, c]
+    [$($v:expr),* $(,)*] => {
+        {
+            // Use to_value function directly to handle arrays, avoiding recursive expansion
+            $crate::to_value(vec![$($v),*]).unwrap_or_default()
+        }
     };
     
-    // array [*,*]
-    [$($arg:tt),*] => {
-        $crate::to_value([$($arg),*]).unwrap_or_default()
+    // Internal helper rule: handle key-value pairs in a map
+    (@map_entry $map:ident $k:tt {$($ik:tt: $iv:tt),* $(,)*}) => {
+        // Process nested object
+        let inner_map = $crate::to_value!({$($ik: $iv),*});
+        $map.insert($crate::to_value!($k), inner_map);
+    };
+    
+    // Handle regular key-value pairs
+    (@map_entry $map:ident $k:tt $v:tt) => {
+        $map.insert($crate::to_value!($k), $crate::to_value!($v));
+    };
+    
+    // Handle single expression
+    ($arg:expr) => {
+        $crate::to_value($arg).unwrap_or_default()
     };
 }
