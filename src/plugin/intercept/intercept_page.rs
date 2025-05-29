3cf7bf3ddb6b38083996f09cf9faa05d08f62ea6
zxj <zhuxiujia@qq.com>
2025-05-29 15:20:19 +0800

    PageIntercept remove order by when count sql

--- src/plugin/intercept/intercept_page.rs
+++ src/plugin/intercept/intercept_page.rs
@@ -72,6 +72,9 @@ impl Intercept for PageIntercept {
                 if let Some(idx) = sql.rfind(" limit ") {
                     *sql = (&sql[..idx]).to_string();
                 }
+                if let Some(idx) = sql.rfind(" order by ") {
+                    *sql = (&sql[..idx]).to_string();
+                }
             }
         }
         if self.select_ids.contains_key(&executor.id()) {
