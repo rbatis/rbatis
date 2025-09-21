a1afbd6e8b833d1ac9f2c8a79b456cd992e858ce
zxj <zhuxiujia@qq.com>
2025-09-22 02:18:05 +0800

    remove example

--- example/example.py
+++
@@ -1,6 +0,0 @@
-`delete from activity where delete_flag = 0`
-                  if name != '':
-                    ` and name=#{name}`
-                  if !ids.is_empty():
-                    ` and id in `
-                    ${ids.sql()}
