b9924b817661a2eae28b86bb9e215ecc3ce0fadd
zxj <zhuxiujia@qq.com>
2025-10-12 23:42:50 +0800

    edit doc

--- example/src/crud_update_skip.rs
+++ example/src/crud_update_skip.rs
@@ -71,7 +71,7 @@ pub async fn main() {
     fast_log::logger().set_level(LevelFilter::Debug);
 
     // Create a sample activity record
-    let mut activity = Activity {
+    let activity = Activity {
         id: Some("123".into()),
         name: Some("Original Activity".into()),
         pc_link: Some("https://example.com/pc".into()),
