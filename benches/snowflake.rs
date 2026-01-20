6087e1aaee799a632979d804219a2d2357223dc8
zxj <zhuxiujia@qq.com>
2026-01-20 23:44:15 +0800

    fix bench

--- benches/snowflake.rs
+++ benches/snowflake.rs
@@ -7,11 +7,11 @@ use test::Bencher;
 #[bench]
 fn bench_snowflake_id(b: &mut Bencher) {
     let snow = Snowflake::new(1, 1, 0);
-    b.iter(|| snow.generate());
+    b.iter(|| snow.generate_id());
 }
 
 #[bench]
 fn bench_snowflake_mode1(b: &mut Bencher) {
     let snow = Snowflake::new(1, 1, 1);
-    b.iter(|| snow.generate());
+    b.iter(|| snow.generate_id());
 }
