1c2a235cec27cbbd49d11e10b1c4ad5e0e95f800
zxj <zhuxiujia@qq.com>
2026-05-04 02:56:35 +0800

    fix mapper

--- src/plugin/table_sync/rbdc_mapper.rs
+++ src/plugin/table_sync/rbdc_mapper.rs
@@ -10,18 +10,14 @@ impl ColumnMapper for RBatis {
         let pool = self.get_pool().expect("uninit pool");
         let driver = pool.driver();
         let column_type = driver.column_type(v);
-        let driver_name = driver.name();
         match v {
             Value::String(v) => {
                 if !v.is_empty() {
                     v.to_string()
+                } else if column.eq("id") || column.ends_with("_id") || column.starts_with("id_") {
+                    "VARCHAR(50)".to_string()
                 } else {
-                    if (driver_name == "mysql" || driver_name == "postgres" || driver_name == "pg")
-                        && (column.eq("id") || column.ends_with("_id") || column.starts_with("id_"))
-                    {
-                        return "VARCHAR(50)".to_string();
-                    }
-                    "TEXT".to_string()
+                    column_type
                 }
             }
             _ => column_type,
